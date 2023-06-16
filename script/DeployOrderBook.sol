2e70be5e0d528e9aebce3b3f3aa53354981a70a6
thedavidmeister <thedavidmeister@gmail.com>
2023-06-16 14:54:53 +0400

    deploy ob script

---
+++ script/DeployOrderBook.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.18;
+
+import "forge-std/Script.sol";
+
+contract DeployOrderBook is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_KEY");
+        // @todo pull this from subgraph.
+        // hardcoded from CI https://github.com/rainprotocol/rain-protocol/actions/runs/5039345251/jobs/9037426821
+        address i9rDeployer = 0xB20DFEdC1b12AA6afA308064998A28531a18C714;
+
+        string[] memory buildMeta = new string(11);
+        buildMeta[0] = "rain";
+        buildMeta[1] = "meta";
+        buildMeta[2] = "build";
+        buildMeta[3] = "--magic";
+        buildMeta[4] = "interpreter-caller-v1";
+        buildMeta[5] = "--input-path";
+        buildMeta[6] = "meta/OrderBook.meta.json";
+        buildMeta[7] = "--content-type";
+        buildMeta[8] = "json";
+        buildMeta[9] = "--content-encoding";
+        buildMeta[10] = "deflate";
+
+        bytes memory meta = vm.ffi(buildMeta);
+        vm.startBroadcast(deployerPrivateKey);
+
+        OrderBook orderbook = new OrderBook(DeployerDiscoverableMetaV1ConstructionConfig (
+            i9rDeployer,
+            meta
+        ));
+
+        vm.stopBroadcast();
+    }
+}
