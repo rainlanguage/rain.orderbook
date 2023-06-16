3a35e45334eae53a1bd8ad23b4ad12305a649b29
thedavidmeister <thedavidmeister@gmail.com>
2023-06-16 15:57:01 +0400

    content language for meta

--- script/DeployOrderBook.sol
+++ script/DeployOrderBook.sol
@@ -11,7 +11,7 @@ contract DeployOrderBook is Script {
         // hardcoded from CI https://github.com/rainprotocol/rain-protocol/actions/runs/5039345251/jobs/9037426821
         address i9rDeployer = 0xB20DFEdC1b12AA6afA308064998A28531a18C714;
 
-        string[] memory buildMeta = new string[](11);
+        string[] memory buildMeta = new string[](13);
         buildMeta[0] = "rain";
         buildMeta[1] = "meta";
         buildMeta[2] = "build";
@@ -23,6 +23,8 @@ contract DeployOrderBook is Script {
         buildMeta[8] = "json";
         buildMeta[9] = "--content-encoding";
         buildMeta[10] = "deflate";
+        buildMeta[11] = "--content-language";
+        buildMeta[12] = "en";
 
         bytes memory meta = vm.ffi(buildMeta);
         vm.startBroadcast(deployerPrivateKey);
