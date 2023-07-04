eb313eefb932fb890722ca5be0532a8d10a9f148
thedavidmeister <thedavidmeister@gmail.com>
2023-07-04 12:44:22 +0400

    snapshot

--- test/concrete/OrderBook.deposit.t.sol
+++ test/concrete/OrderBook.deposit.t.sol
@@ -9,15 +9,34 @@ import "test/util/abstract/OrderBookTest.sol";
 /// Tests depositing to an order book without any trades.
 contract OrderBookDepositTest is OrderBookTest {
     /// Tests that we can deposit some amount and view the new vault balance.
-    function testDeposit(address depositor, uint256 vaultId, uint256 amount) external {
+    function testDepositSimple(address depositor, uint256 vaultId, uint256 amount) external {
         vm.prank(depositor);
         vm.mockCall(
             address(token0),
             abi.encodeWithSelector(IERC20.transferFrom.selector, depositor, address(orderbook), amount),
             abi.encode(true)
         );
+
+        vm.record();
         orderbook.deposit(DepositConfig(address(token0), vaultId, amount));
+        (bytes32[] memory reads, bytes32[] memory writes) = vm.accesses((address(orderbook)));
+        // assertEq(reads.length, 0);
+        // assertEq(writes.length, 1);
+        // assertEq(writes[0], bytes32(uint256(1)));
+
         assertEq(orderbook.vaultBalance(depositor, address(token0), vaultId), amount);
+    }
+
+    function testDepositGas00() external {
+        vm.pauseGasMetering();
+        vm.mockCall(
+            address(token0),
+            abi.encodeWithSelector(IERC20.transferFrom.selector, address(this), address(orderbook), 1),
+            abi.encode(true)
+        );
+        vm.resumeGasMetering();
+
+        orderbook.deposit(DepositConfig(address(token0), 0, 1));
     }
 
     /// Any failure in the deposit should revert the entire transaction.
@@ -54,5 +73,19 @@ contract OrderBookDepositTest is OrderBookTest {
             orderbook.deposit(DepositConfig(address(token0), vaultId, amounts[i]));
             assertEq(orderbook.vaultBalance(depositor, address(token0), vaultId), totalAmount);
         }
+    }
+
+    /// Depositing should emit an event with the sender and all deposit details.
+    function testDepositEvent(address depositor, DepositConfig memory config) external {
+        config.token = address(token0);
+        vm.prank(depositor);
+        vm.mockCall(
+            address(token0),
+            abi.encodeWithSelector(IERC20.transferFrom.selector, depositor, address(orderbook), config.amount),
+            abi.encode(true)
+        );
+        vm.expectEmit(false, false, false, true);
+        emit Deposit(depositor, config);
+        orderbook.deposit(config);
     }
 }
--- test/util/abstract/OrderBookTest.sol
+++ test/util/abstract/OrderBookTest.sol
@@ -19,13 +19,15 @@ import "src/concrete/OrderBook.sol";
 /// - Deploys a mockable deployer contract for a DISpair.
 ///
 /// Inherits from Test so that it can be used as a base contract for other tests.
-abstract contract OrderBookTest is Test {
+/// Implements IOrderBookV2 so that it has access to all the relevant events.
+abstract contract OrderBookTest is Test, IOrderBookV2 {
     IExpressionDeployerV1 immutable deployer;
     IOrderBookV2 immutable orderbook;
     IERC20 immutable token0;
     IERC20 immutable token1;
 
     constructor() {
+        vm.pauseGasMetering();
         deployer = IExpressionDeployerV1(address(uint160(uint256(keccak256("deployer.rain.test")))));
         // All non-mocked calls will revert.
         vm.etch(address(deployer), REVERTING_MOCK_BYTECODE);
@@ -43,5 +45,59 @@ abstract contract OrderBookTest is Test {
         vm.etch(address(token0), REVERTING_MOCK_BYTECODE);
         token1 = IERC20(address(uint160(uint256(keccak256("token1.rain.test")))));
         vm.etch(address(token1), REVERTING_MOCK_BYTECODE);
+        vm.resumeGasMetering();
+    }
+
+    /// @inheritdoc IOrderBookV2
+    function addOrder(OrderConfig calldata) external pure {
+        revert("addOrder");
+    }
+
+    /// @inheritdoc IOrderBookV2
+    function clear(Order memory, Order memory, ClearConfig calldata, SignedContextV1[] memory, SignedContextV1[] memory)
+        external
+        pure
+    {
+        revert("clear");
+    }
+
+    /// @inheritdoc IOrderBookV2
+    function deposit(DepositConfig calldata) external pure {
+        revert("deposit");
+    }
+
+    /// @inheritdoc IERC3156FlashLender
+    function flashLoan(IERC3156FlashBorrower, address, uint256, bytes calldata) external pure returns (bool) {
+        revert("flashLoan");
+    }
+
+    /// @inheritdoc IERC3156FlashLender
+    function flashFee(address, uint256) external pure returns (uint256) {
+        revert("flashFee");
+    }
+
+    /// @inheritdoc IERC3156FlashLender
+    function maxFlashLoan(address) external pure returns (uint256) {
+        revert("maxFlashLoan");
+    }
+
+    /// @inheritdoc IOrderBookV2
+    function removeOrder(Order calldata) external pure {
+        revert("removeOrder");
+    }
+
+    /// @inheritdoc IOrderBookV2
+    function takeOrders(TakeOrdersConfig calldata) external pure returns (uint256, uint256) {
+        revert("takeOrders");
+    }
+
+    /// @inheritdoc IOrderBookV2
+    function vaultBalance(address, address, uint256) external pure returns (uint256) {
+        revert("vaultBalance");
+    }
+
+    /// @inheritdoc IOrderBookV2
+    function withdraw(WithdrawConfig calldata) external pure {
+        revert("withdraw");
     }
 }
