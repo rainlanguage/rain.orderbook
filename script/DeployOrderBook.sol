4fce0df74704e65e0fc4ea215b262d1bb66c226a
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-07-05 11:47:39 +0530

    script updated

--- script/DeployOrderBook.sol
+++ script/DeployOrderBook.sol
@@ -8,8 +8,8 @@ contract DeployOrderBook is Script {
     function run(bytes memory meta) external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
         // @todo pull this from subgraph.
-        // hardcoded from CI https://github.com/rainprotocol/rain-protocol/actions/runs/5039345251/jobs/9037426821
-        address i9rDeployer = 0xB20DFEdC1b12AA6afA308064998A28531a18C714;
+        // hardcoded from CI https://github.com/rainprotocol/rain-protocol/actions/runs/5365826502/jobs/9934721206
+        address i9rDeployer = 0x12CC9A83C200354bc35e19e6ad18a0F444aB5c86;
 
         console2.log("meta hash:");
         console2.logBytes32(keccak256(meta));
