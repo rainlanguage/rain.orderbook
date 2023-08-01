ce960c3b13fae491dd0c33140e1329fd3073cd14
vishal <vishalkale151071@gmail.com>
2023-08-01 19:48:43 +0530

    Schema issue resolved by update schema.json

--- cli/src/subgraph/orders/mod.rs
+++ cli/src/subgraph/orders/mod.rs
@@ -6,8 +6,9 @@ use serde_bytes::ByteBuf as Bytes;
 
 use once_cell::sync::Lazy;
 
-static BASE_URL: Lazy<Url> =
-    Lazy::new(|| Url::parse("https://api.thegraph.com/subgraphs/name/siddharth2207/nhstestbot").unwrap());
+static BASE_URL: Lazy<Url> = Lazy::new(|| {
+    Url::parse("https://api.thegraph.com/subgraphs/name/siddharth2207/nhstestbot").unwrap()
+});
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -21,7 +22,11 @@ pub async fn query() -> anyhow::Result<()> {
     let variables = orders_query::Variables {};
     let request_body = OrdersQuery::build_query(variables);
     let client = reqwest::Client::new();
-    let res = client.post((*BASE_URL).clone()).json(&request_body).send().await?;
+    let res = client
+        .post((*BASE_URL).clone())
+        .json(&request_body)
+        .send()
+        .await?;
     let response_body: Response<orders_query::ResponseData> = res.json().await?;
     match response_body {
         Response {
@@ -29,8 +34,10 @@ pub async fn query() -> anyhow::Result<()> {
             ..
         } => {
             dbg!(&orders);
-        },
-        _ => { tracing::warn!("Failed to get orders"); }
+        }
+        _ => {
+            tracing::warn!("Failed to get orders");
+        }
     }
     Ok(())
 }
