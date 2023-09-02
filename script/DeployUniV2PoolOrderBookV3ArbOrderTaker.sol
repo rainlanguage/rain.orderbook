e3a1fd2778244d9ae6ad848a724ac4d9e12e24d1
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-09-02 12:08:52 +0530

    fmt

--- script/DeployUniV2PoolOrderBookV3ArbOrderTaker.sol
+++ script/DeployUniV2PoolOrderBookV3ArbOrderTaker.sol
@@ -7,8 +7,8 @@ import "src/concrete/UniV2PoolOrderBookV3ArbOrderTaker.sol";
 /// @title DeployGenericPoolOrderBookV3FlashBorrower
 /// @notice A script that deploys a `GenericPoolOrderBookV3FlashBorrower`.
 contract DeployUniV2PoolOrderBookV3ArbOrderTaker is Script {
-    function run(bytes memory meta) external { 
-        uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY"); 
+    function run(bytes memory meta) external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
         // @todo pull this from subgraph.
         //https://github.com/rainprotocol/rain.interpreter/actions/runs/6000630847/job/16273099850
         address i9rDeployer = 0xCA0Ef6E0d9cd47d44aF5d87098f8482669303b06;
