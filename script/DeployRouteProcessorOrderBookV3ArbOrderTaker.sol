320ffa98891788b10a711ea062e630daeb4d4235
Siddharth2207 <siddharthbhoite22@gmail.com>
2023-09-06 15:48:41 +0530

    added router deploy scripts

--- script/DeployRouteProcessorOrderBookV3ArbOrderTaker.sol
+++ script/DeployRouteProcessorOrderBookV3ArbOrderTaker.sol
@@ -2,27 +2,28 @@
 pragma solidity =0.8.19;
 
 import "forge-std/Script.sol";
-import "src/concrete/GenericPoolOrderBookV3FlashBorrower.sol";
+import "src/concrete/RouteProcessorOrderBookV3ArbOrderTaker.sol";
 
-/// @title DeployGenericPoolOrderBookV3FlashBorrower
-/// @notice A script that deploys a `GenericPoolOrderBookV3FlashBorrower`. This
+/// @title DeployRouteProcessorOrderBookV3ArbOrderTaker
+/// @notice A script that deploys a `RouteProcessorOrderBookV3ArbOrderTaker`. This
 /// is intended to be run on every commit by CI to a testnet such as mumbai, then
 /// cross chain deployed to whatever mainnet is required, by users.
-contract DeployGenericPoolOrderBookV3FlashBorrower is Script {
+contract DeployRouteProcessorOrderBookV3ArbOrderTaker is Script {
     /// We are avoiding using ffi here, instead forcing the script runner to
     /// provide the built metadata. On CI this is achieved by using the rain cli.
-    function run(bytes memory meta) external {
+    function run(bytes memory meta) external { 
+
         uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
         // @todo pull this from subgraph.
         // hardcoded from CI https://github.com/rainprotocol/rain.interpreter/actions/runs/6062778321/job/16449363038
         address i9rDeployer = 0xCDAd930d648C562B570490901EC6d01D368B3b63;
 
-        console2.log("DeployGenericPoolOrderBookFlashBorrower meta hash:");
+        console2.log("RouteProcessorOrderBookV3ArbOrderTaker meta hash:");
         console2.logBytes32(keccak256(meta));
 
         vm.startBroadcast(deployerPrivateKey);
-        GenericPoolOrderBookV3FlashBorrower deployed =
-        new GenericPoolOrderBookV3FlashBorrower(DeployerDiscoverableMetaV2ConstructionConfig (
+        RouteProcessorOrderBookV3ArbOrderTaker deployed =
+        new RouteProcessorOrderBookV3ArbOrderTaker(DeployerDiscoverableMetaV2ConstructionConfig (
             i9rDeployer,
             meta
         ));
