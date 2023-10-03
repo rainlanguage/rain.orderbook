3613d9e28a387c316a0016c32fe241f303cb7bcf
NanezX <victor.nanezj@gmail.com>
2023-10-02 22:13:45 -0400

    wip nix flake tooling

--- subgraph/flake.nix
+++ subgraph/flake.nix
@@ -1,35 +1,24 @@
+# TODO: Improve tooling here
 {
-  description = "Flake for development workflows.";
+  description = "Flake for development orderbook subgraph workflows.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rain_subgraph_cli.url = "github:rainprotocol/rain.subgraph-cli";
+    subgraph-cli.url = "github:rainprotocol/rain.subgraph-cli";
     flake-utils.url = "github:numtide/flake-utils";
-
   };
 
-  outputs = {self, nixpkgs, rain_subgraph_cli, flake-utils }:
+  outputs = {self, nixpkgs, subgraph-cli, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        rain-subgraph-cli = "${rain_subgraph_cli.defaultPackage.${system}}/bin/rain_subgraph_cli";
+        rain-sg-cli = "${subgraph-cli.defaultPackage.${system}}/bin/rain_subgraph_cli";
 
       in rec {
         packages = rec {
-          check-build = "${rain-subgraph-cli}";
-          # check-build = pkgs.writeShellScriptBin "check-build" ("echo ${rain-subgraph-cli}");
+          install = pkgs.writeShellScriptBin "install" (''${rain-sg-cli} install'');
 
-
-          checkxd = pkgs.writeShellScriptBin "checkxd" ("echo lol");
-
-          default = check-build;
-
-
-          # build-meta-cmd = contract: ''
-          #   ${rain-cli} meta build \
-          #     -i <(${rain-cli} meta solc artifact -c abi -i out/${contract}.sol/${contract}.json) -m solidity-abi-v2 -t json -e deflate -l en \
-          #     -i src/concrete/${contract}.meta.json -m interpreter-caller-meta-v1 -t json -e deflate -l en \
-          # '';
+          default = install;
         };
       }
     );
