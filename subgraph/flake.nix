b9373ca0ba511188729a077e663d3b07650fe48e
NanezX <victor.nanezj@gmail.com>
2023-11-03 02:55:20 -0400

    ci debug

--- subgraph/flake.nix
+++ subgraph/flake.nix
@@ -82,15 +82,12 @@
             kill -9 $(lsof -t -i :8545)
           '');
 
-          ci-test = pkgs.writeShellScriptBin "ci-test" (''
-            # This build is for generate the schema.json.
-            # This in case the subgraph schema.graphql have changes when running tests
-            ${gen-sg-schema}
+          gen-subgraph-schema  = pkgs.writeShellScriptBin "gen-subgraph-schema" (gen-sg-schema)
 
-            echo "Print root"
-            ls .
-            echo "Print query folder"
-            ls tests/subgraph/query
+          ci-test = pkgs.writeShellScriptBin "ci-test" (''
+            # # This build is for generate the schema.json.
+            # # This in case the subgraph schema.graphql have changes when running tests
+            # ${gen-sg-schema}
 
             # Run tests in single thread
             cargo test -- --test-threads=1 --nocapture;
