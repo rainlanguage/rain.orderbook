dfe3796cfd0b8f7fc402ec25fb35071d3e77f8a0
NanezX <victor.nanezj@gmail.com>
2023-11-09 18:46:37 -0400

    using specific commit for nix pkgs in flake nix file

--- subgraph/flake.nix
+++ subgraph/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for development orderbook subgraph workflows.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/ec750fd01963ab6b20ee1f0cb488754e8036d89d";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
