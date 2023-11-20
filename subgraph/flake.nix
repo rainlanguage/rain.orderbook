4ea3d252343c22569e4f17f90aa4424809644325
NanezX <victor.nanezj@gmail.com>
2023-11-20 04:36:11 -0400

    update flake nix to use rain.subgraph_cli

--- subgraph/flake.nix
+++ subgraph/flake.nix
@@ -4,14 +4,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/ec750fd01963ab6b20ee1f0cb488754e8036d89d";
     flake-utils.url = "github:numtide/flake-utils";
+    rain.url = "github:rainprotocol/rain.cli/9f5d4bf65bee767f80871d9952665c654b01bdfb";
+
   };
 
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, rain, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         jq = "${pkgs.jq}/bin/jq";
+        rain-cli = "${rain.defaultPackage.${system}}/bin/rain";
 
       in rec {
         packages = rec {
@@ -40,7 +43,11 @@
             + (remove-duplicate-component)
           );
 
-          default = init-setup;
+          rain_cli = pkgs.writeShellScriptBin "rain_cli" (''
+            ${rain-cli} $@
+          '');
+
+          default = rain_cli;
         };
       }
     );
