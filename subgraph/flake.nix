4e00d8edbd88af141d6dbe3734860b0f5dfa0a27
NanezX <victor.nanezj@gmail.com>
2023-11-20 04:17:06 -0400

    tests

--- subgraph/flake.nix
+++ subgraph/flake.nix
@@ -13,35 +13,11 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         jq = "${pkgs.jq}/bin/jq";
-        rain-cli = "${rain.defaultPackage.${system}}/bin/rain";
+        rain = "${rain.defaultPackage.${system}}/bin/rain";
 
       in rec {
         packages = rec {
-          concrete-contracts = ["OrderBook" "RainterpreterExpressionDeployerNP" "RainterpreterNP" "RainterpreterStore"];
-
-          copy-abis = contract: ''
-            cp ../out/${contract}.sol/${contract}.json ./tests/generated/
-          '';
-
-          remove-duplicate-component = ''
-            # Remove a component duplicated on RainterpreterExpressionDeployerNP abi that conflict with abigen
-            contract_path="tests/generated/RainterpreterExpressionDeployerNP.json"
-            ${jq} '.abi |= map(select(.name != "StackUnderflow"))' $contract_path > updated_contract.json
-            mv updated_contract.json $contract_path
-          '';
-
-          init-setup =  pkgs.writeShellScriptBin "init-setup" (''
-            forge build --root ../
-
-            rm -rf ./abis ./tests/generated
-            mkdir ./abis ./tests/generated
-
-            cp ../out/OrderBook.sol/OrderBook.json ./abis/
-            cp ../out/ERC20.sol/ERC20.json ./abis/ERC20.json
-            '' + pkgs.lib.concatStrings (map copy-abis concrete-contracts)
-            + (remove-duplicate-component)
-          );
-          rain_cli = pkgs.writeShellScriptBin "rain_cli" rain-cli;
+          init-setup =  pkgs.writeShellScriptBin "init-setup" (''echo init'');
 
           default = init-setup;
         };
