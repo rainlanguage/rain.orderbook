ad1437d0c0824a58419605ecd1157afb563f8c3e
thedavidmeister <thedavidmeister@gmail.com>
2023-11-22 15:33:54 +0400

    update deployer

--- script/DeployOrderBook.sol
+++ script/DeployOrderBook.sol
@@ -8,8 +8,8 @@ contract DeployOrderBook is Script {
     function run(bytes memory meta) external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
         // @todo pull this from subgraph.
-        // hardcoded from CI https://github.com/rainprotocol/rain.interpreter/actions/runs/6101787278/job/16558857505
-        address i9rDeployer = 0xAb0A13cC2654CbaDABabC9952a090928F4ff569A;
+        // hardcoded from CI https://github.com/rainprotocol/rain.interpreter/actions/runs/6953107467/job/18917750124
+        address i9rDeployer = 0xa5aDC3F2A7A8Cf7b5172D76d8b26c3d49272297B;
 
         console2.log("DeployOrderBook meta hash:");
         console2.logBytes32(keccak256(meta));
