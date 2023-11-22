7348b875f9b3a6942e83fc2ecbc6f892213d1aa5
NanezX <victor.nanezj@gmail.com>
2023-11-21 20:04:14 -0400

    update rain url on flake

--- subgraph/flake.nix
+++ subgraph/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/ec750fd01963ab6b20ee1f0cb488754e8036d89d";
     flake-utils.url = "github:numtide/flake-utils";
-    rain.url = "github:rainprotocol/rain.cli/9f5d4bf65bee767f80871d9952665c654b01bdfb";
+    rain.url = "github:rainprotocol/rain.cli";
 
   };
 
