b8c63a7aad3ea4bc525fb28890fe7a91b5a8bb0f
NanezX <victor.nanezj@gmail.com>
2023-12-03 03:03:04 -0400

    updated ci workflow

--- subgraph/tests/entities.rs
+++ subgraph/tests/entities.rs
@@ -1,2 +1,14 @@
 mod generated;
 mod utils;
+
+use utils::deploy::get_orderbook;
+
+#[tokio::main]
+#[test]
+async fn test_orderbook_entity() -> anyhow::Result<()> {
+    let orderbook = get_orderbook().await?;
+
+    println!("orderbook: {:?}", orderbook.address());
+
+    Ok(())
+}
