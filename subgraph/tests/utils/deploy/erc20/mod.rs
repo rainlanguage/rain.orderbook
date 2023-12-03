e60643d95cc9e798b3f15c4879ee61169b10233b
NanezX <victor.nanezj@gmail.com>
2023-12-03 02:11:31 -0400

    deploy erc20 contract

---
+++ subgraph/tests/utils/deploy/erc20/mod.rs
@@ -0,0 +1,45 @@
+use crate::{
+    generated::ERC20Test,
+    utils::{get_provider, get_wallet},
+};
+use ethers::{
+    core::k256::ecdsa::SigningKey,
+    prelude::SignerMiddleware,
+    providers::{Http, Middleware, Provider},
+    signers::{Signer, Wallet},
+};
+use std::sync::Arc;
+
+pub async fn deploy_erc20(
+    wallet: Option<Wallet<SigningKey>>,
+) -> anyhow::Result<ERC20Test<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
+    let wallet = wallet.unwrap_or(get_wallet(0)?);
+    let provider = get_provider().await?;
+    let chain_id = provider.get_chainid().await?;
+
+    let client = Arc::new(SignerMiddleware::new(
+        provider.clone(),
+        wallet.with_chain_id(chain_id.as_u64()),
+    ));
+
+    let contract = ERC20Test::deploy(client, ())?.send().await?;
+
+    Ok(contract)
+}
+
+impl ERC20Test<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
+    pub async fn connect(
+        &self,
+        wallet: &Wallet<SigningKey>,
+    ) -> ERC20Test<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
+        let provider = get_provider().await.expect("cannot get provider");
+        let chain_id = provider.get_chainid().await.expect("cannot get chain id");
+
+        let client = Arc::new(SignerMiddleware::new(
+            provider.clone(),
+            wallet.clone().with_chain_id(chain_id.as_u64()),
+        ));
+
+        ERC20Test::new(self.address(), client)
+    }
+}
--- subgraph/tests/utils/deploy/mod.rs
+++ subgraph/tests/utils/deploy/mod.rs
@@ -1,7 +1,9 @@
+mod erc20;
 mod meta_getter;
 mod registry1820;
 mod touch_deployer;
 
+pub use erc20::deploy_erc20;
 pub use meta_getter::get_authoring_meta;
 pub use registry1820::deploy1820;
 pub use touch_deployer::touch_deployer;
