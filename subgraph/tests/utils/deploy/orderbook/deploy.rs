c0d6f448f7325d7c1f427313f86fa52e6788aa1d
NanezX <victor.nanezj@gmail.com>
2023-12-05 22:46:16 -0400

    ob with reduced code when deploy

--- subgraph/tests/utils/deploy/orderbook/deploy.rs
+++ subgraph/tests/utils/deploy/orderbook/deploy.rs
@@ -1,46 +1,32 @@
 use crate::{
     generated::{Orderbook, ORDERBOOK_ABI, ORDERBOOK_BYTECODE},
-    utils::{deploy::touch_deployer, get_provider, get_wallet},
+    utils::{deploy::touch_deployer, get_client},
 };
 use ethers::{
     abi::Token,
     contract::ContractFactory,
     core::k256::ecdsa::SigningKey,
     prelude::SignerMiddleware,
-    providers::{Http, Middleware, Provider},
-    signers::{Signer, Wallet},
+    providers::{Http, Provider},
+    signers::Wallet,
 };
-use std::sync::Arc;
 
 pub async fn deploy_orderbook(
 ) -> anyhow::Result<Orderbook<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
-    let wallet = get_wallet(0)?;
-    let provider = get_provider().await?;
-    let chain_id = provider.get_chainid().await?;
-
-    let client = Arc::new(SignerMiddleware::new(
-        provider.clone(),
-        wallet.clone().with_chain_id(chain_id.as_u64()),
-    ));
-
-    // Deploying DISpair
     let expression_deployer = touch_deployer().await?;
 
-    // Obtaining OB Meta bytes
     let meta = std::fs::read("../meta/OrderBook.rain.meta")?;
-
     let args = vec![Token::Tuple(vec![
         Token::Address(expression_deployer.address()),
         Token::Bytes(meta),
     ])];
 
-    // Obtaining OB deploy transaction
+    let client = get_client(None).await?;
     let deploy_transaction = ContractFactory::new(
         ORDERBOOK_ABI.clone(),
         ORDERBOOK_BYTECODE.clone(),
         client.clone(),
     );
-
     let contract = deploy_transaction.deploy_tokens(args)?.send().await?;
 
     Ok(Orderbook::new(contract.address(), client))
@@ -51,14 +37,7 @@ impl Orderbook<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
         &self,
         wallet: &Wallet<SigningKey>,
     ) -> anyhow::Result<Orderbook<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
-        let provider = get_provider().await?;
-        let chain_id = provider.get_chainid().await?;
-
-        let client = Arc::new(SignerMiddleware::new(
-            provider.clone(),
-            wallet.clone().with_chain_id(chain_id.as_u64()),
-        ));
-
+        let client = get_client(Some(wallet.to_owned())).await?;
         Ok(Orderbook::new(self.address(), client))
     }
 }
