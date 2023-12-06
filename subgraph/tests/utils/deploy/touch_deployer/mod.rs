7006898a37c55b021cf6e17d2374ba088ff7be85
NanezX <victor.nanezj@gmail.com>
2023-12-06 04:46:14 -0400

    deploy dispair with less code

--- subgraph/tests/utils/deploy/touch_deployer/mod.rs
+++ subgraph/tests/utils/deploy/touch_deployer/mod.rs
@@ -4,7 +4,7 @@ use crate::{
         Rainterpreter, RainterpreterExpressionDeployer, RainterpreterStore,
         RAINTERPRETEREXPRESSIONDEPLOYER_ABI, RAINTERPRETEREXPRESSIONDEPLOYER_BYTECODE,
     },
-    utils::{get_provider, get_wallet},
+    utils::get_client,
 };
 use anyhow::Result;
 use ethers::{
@@ -12,18 +12,15 @@ use ethers::{
     contract::ContractFactory,
     core::k256::ecdsa::SigningKey,
     prelude::SignerMiddleware,
-    providers::{Http, Middleware, Provider},
-    signers::{Signer, Wallet},
+    providers::{Http, Provider},
+    signers::Wallet,
     types::H160,
 };
-use std::sync::Arc;
 
 pub async fn touch_deployer(
 ) -> Result<RainterpreterExpressionDeployer<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
     let rainterpreter = rainterpreter_deploy().await?;
-
     let store = rainterpreter_store_deploy().await?;
-
     let expression_deployer =
         rainterpreter_expression_deployer_deploy(rainterpreter.address(), store.address()).await?;
 
@@ -32,66 +29,38 @@ pub async fn touch_deployer(
 
 pub async fn rainterpreter_deploy(
 ) -> Result<Rainterpreter<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
-    let wallet = get_wallet(0)?;
-    let provider = get_provider().await?;
-    let chain_id = provider.get_chainid().await?;
-
-    let deployer = Arc::new(SignerMiddleware::new(
-        provider.clone(),
-        wallet.with_chain_id(chain_id.as_u64()),
-    ));
-
-    let interpreter = Rainterpreter::deploy(deployer, ())?.send().await?;
-
-    Ok(interpreter)
+    let deployer = get_client(None).await?;
+    Ok(Rainterpreter::deploy(deployer, ())?.send().await?)
 }
 
 pub async fn rainterpreter_store_deploy(
 ) -> Result<RainterpreterStore<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
-    let wallet = get_wallet(0)?;
-    let provider = get_provider().await?;
-    let chain_id = provider.get_chainid().await?;
-
-    let deployer = Arc::new(SignerMiddleware::new(
-        provider.clone(),
-        wallet.with_chain_id(chain_id.as_u64()),
-    ));
-
-    let store = RainterpreterStore::deploy(deployer, ())?.send().await?;
-
-    Ok(store)
+    let deployer = get_client(None).await?;
+    Ok(RainterpreterStore::deploy(deployer, ())?.send().await?)
 }
 
 pub async fn rainterpreter_expression_deployer_deploy(
     rainiterpreter_address: H160,
     store_address: H160,
 ) -> Result<RainterpreterExpressionDeployer<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
-    let wallet = get_wallet(0)?;
-    let provider = get_provider().await?;
-    let chain_id = provider.get_chainid().await?;
-
-    let client = Arc::new(SignerMiddleware::new(
-        provider.clone(),
-        wallet.with_chain_id(chain_id.as_u64()),
-    ));
-
     let meta_bytes = get_authoring_meta().await?.to_vec();
-
     let args = vec![Token::Tuple(vec![
         Token::Address(rainiterpreter_address),
         Token::Address(store_address),
         Token::Bytes(meta_bytes),
     ])];
 
+    let deployer = get_client(None).await?;
+
     let deploy_transaction = ContractFactory::new(
         RAINTERPRETEREXPRESSIONDEPLOYER_ABI.clone(),
         RAINTERPRETEREXPRESSIONDEPLOYER_BYTECODE.clone(),
-        client.clone(),
+        deployer.clone(),
     );
-
     let contract = deploy_transaction.deploy_tokens(args)?.send().await?;
 
-    let deployer = RainterpreterExpressionDeployer::new(contract.address(), client);
-
-    return Ok(deployer);
+    Ok(RainterpreterExpressionDeployer::new(
+        contract.address(),
+        deployer,
+    ))
 }
