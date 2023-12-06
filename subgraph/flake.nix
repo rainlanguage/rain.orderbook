bc0cb9ddc9fac173225903be53bace5fafef0508
NanezX <victor.nanezj@gmail.com>
2023-12-06 05:29:58 -0400

    updated commit hashes for cli subgraph tooling

--- subgraph/flake.nix
+++ subgraph/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/ec750fd01963ab6b20ee1f0cb488754e8036d89d";
     flake-utils.url = "github:numtide/flake-utils";
-    rain.url = "github:rainlanguage/rain.cli/b702505ddd2a9bb714837a62b09d2ba08001c335";
+    rain.url = "github:rainlanguage/rain.cli/192d5e1921ee37ba933b5ed42c9edf6f45ae6a93";
   };
 
 
