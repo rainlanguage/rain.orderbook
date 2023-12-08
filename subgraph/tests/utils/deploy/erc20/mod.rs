684497e65b97db25eb2cf7e66202cf0af1bb99c2
NanezX <victor.nanezj@gmail.com>
2023-12-08 11:34:17 -0400

    deploy erc20 using crate for sg utils

--- subgraph/tests/utils/deploy/erc20/mod.rs
+++ subgraph/tests/utils/deploy/erc20/mod.rs
@@ -1,4 +1,4 @@
-use crate::{generated::ERC20Test, utils::get_client};
+use crate::{generated::ERC20Test, utils::setup::get_wallets_handler};
 use ethers::{
     core::k256::ecdsa::SigningKey,
     prelude::SignerMiddleware,
@@ -9,16 +9,16 @@ use ethers::{
 pub async fn deploy_erc20(
     wallet: Option<Wallet<SigningKey>>,
 ) -> anyhow::Result<ERC20Test<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
-    let client = get_client(wallet).await?;
+    let client = get_wallets_handler().get_client(wallet).await?;
     Ok(ERC20Test::deploy(client, ())?.send().await?)
 }
 
 impl ERC20Test<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
     pub async fn connect(
         &self,
-        wallet: &Wallet<SigningKey>,
+        wallet: Wallet<SigningKey>,
     ) -> anyhow::Result<ERC20Test<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>> {
-        let client = get_client(Some(wallet.to_owned())).await?;
+        let client = get_wallets_handler().get_client(wallet).await?;
         Ok(ERC20Test::new(self.address(), client))
     }
 }
