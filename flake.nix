f96d457d51a5d54f3eca1f697226629a69169f6f
thedavidmeister <thedavidmeister@gmail.com>
2024-01-07 20:12:47 +0400

    rainix workflow

--- flake.nix
+++ flake.nix
@@ -2,23 +2,23 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/ec750fd01963ab6b20ee1f0cb488754e8036d89d";
     rain.url = "github:rainprotocol/rain.cli/6a912680be6d967fd6114aafab793ebe8503d27b";
+    rainix.url = "github:rainprotocol/rainix/9e945f33349606c12f71559f947473766ed3f922";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {self, nixpkgs, rain, flake-utils }:
+  outputs = {self, flake-utils, rainix, rain }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        rain-cli = "${rain.defaultPackage.${system}}/bin/rain";
+        pkgs = rainix.pkgs.${system};
+        rain-bin = "${rain.defaultPackage.${system}}/bin/rain";
 
       in rec {
         packages = rec {
           concrete-contracts = ["OrderBook" "GenericPoolOrderBookV3FlashBorrower" "GenericPoolOrderBookV3ArbOrderTaker" "RouteProcessorOrderBookV3ArbOrderTaker"];
           build-meta-cmd = contract: ''
-            ${rain-cli} meta build \
-              -i <(${rain-cli} meta solc artifact -c abi -i out/${contract}.sol/${contract}.json) -m solidity-abi-v2 -t json -e deflate -l en \
+            ${rain-bin} meta build \
+              -i <(${rain-bin} meta solc artifact -c abi -i out/${contract}.sol/${contract}.json) -m solidity-abi-v2 -t json -e deflate -l en \
               -i src/concrete/${contract}.meta.json -m interpreter-caller-meta-v1 -t json -e deflate -l en \
           '';
           build-single-meta = contract: ''
@@ -42,7 +42,10 @@
           '' + pkgs.lib.concatStrings (map deploy-single-contract concrete-contracts));
 
           default = build-meta;
-        };
+          ci-prep = build-meta;
+        } // rainix.packages.${system};
+
+        devShells = rainix.devShells.${system};
       }
     );
 
