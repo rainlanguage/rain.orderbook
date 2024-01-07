375109c4ede9adc695a31d8075f0654b186cf797
thedavidmeister <thedavidmeister@gmail.com>
2024-01-07 21:12:21 +0400

    fix ci prep

--- flake.nix
+++ flake.nix
@@ -2,33 +2,32 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    rain.url = "github:rainprotocol/rain.cli/6a912680be6d967fd6114aafab793ebe8503d27b";
-    rainix.url = "github:rainprotocol/rainix/9e945f33349606c12f71559f947473766ed3f922";
+    rainix.url = "github:rainprotocol/rainix/49f820b308eda51ea5d1f9697e85012004f1cf05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {self, flake-utils, rainix, rain }:
+  outputs = {self, flake-utils, rainix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = rainix.pkgs.${system};
-        rain-bin = "${rain.defaultPackage.${system}}/bin/rain";
 
       in rec {
         packages = rec {
           concrete-contracts = ["OrderBook" "GenericPoolOrderBookV3FlashBorrower" "GenericPoolOrderBookV3ArbOrderTaker" "RouteProcessorOrderBookV3ArbOrderTaker"];
           build-meta-cmd = contract: ''
-            ${rain-bin} meta build \
-              -i <(${rain-bin} meta solc artifact -c abi -i out/${contract}.sol/${contract}.json) -m solidity-abi-v2 -t json -e deflate -l en \
+            rain meta build \
+              -i <(rain meta solc artifact -c abi -i out/${contract}.sol/${contract}.json) -m solidity-abi-v2 -t json -e deflate -l en \
               -i src/concrete/${contract}.meta.json -m interpreter-caller-meta-v1 -t json -e deflate -l en \
           '';
           build-single-meta = contract: ''
             ${(build-meta-cmd contract)} -o meta/${contract}.rain.meta;
           '';
-          build-meta = pkgs.writeShellScriptBin "build-meta" (''
+
+          build-meta = rainix.mkTask.${system} { name = "build-meta"; body = (''
           set -x;
           mkdir -p meta;
           forge build --force;
-          '' + pkgs.lib.concatStrings (map build-single-meta concrete-contracts));
+          '' + pkgs.lib.concatStrings (map build-single-meta concrete-contracts)); };
 
           deploy-single-contract = contract: ''
             forge script script/Deploy${contract}.sol:Deploy${contract} --legacy --verify --broadcast --rpc-url "''${CI_DEPLOY_RPC_URL}" --etherscan-api-key "''${EXPLORER_VERIFICATION_KEY}" \
