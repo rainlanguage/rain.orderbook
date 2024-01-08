12e2afb40f172facf32999285832b72d408cb50b
thedavidmeister <thedavidmeister@gmail.com>
2024-01-08 11:49:30 +0400

    fix compile

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -2,6 +2,10 @@
 pragma solidity =0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {OrderBook} from "src/concrete/OrderBook.sol";
+import {GenericPoolOrderBookV3ArbOrderTaker} from "src/concrete/GenericPoolOrderBookV3ArbOrderTaker.sol";
+import {RouteProcessorOrderBookV3ArbOrderTaker} from "src/concrete/RouteProcessorOrderBookV3ArbOrderTaker.sol";
+import {GenericPoolOrderBookV3FlashBorrower} from "src/concrete/GenericPoolOrderBookV3FlashBorrower.sol";
 
 // hardcoded from CI https://github.com/rainprotocol/rain.interpreter/actions/runs/6953107467/job/18917750124
 address constant I9R_DEPLOYER = 0xa5aDC3F2A7A8Cf7b5172D76d8b26c3d49272297B;
