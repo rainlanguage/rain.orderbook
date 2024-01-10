f0bde25c33d88fc3284d3558a3b5e6f55eba229b
Matt Gabrenya <matt@buildyourweb.app>
2024-01-10 13:39:46 -0700

    build(tauri/ui): add tailwind, flowbite

--- tauri-app/src/routes/+page.svelte
+++ tauri-app/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<Alert color="green">
+	<h1 class="text-2xl font-medium">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</Alert>
