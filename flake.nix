dcea54525e80bff538af7e2cc2f86baa6c52982d
thedavidmeister <thedavidmeister@gmail.com>
2024-01-11 17:58:54 +0400

    rollback flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    rainix.url = "github:rainprotocol/rainix";
+    rainix.url = "github:rainprotocol/rainix/ff671d13606dc12c096c9e2a70c8d26342bed5c1";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
