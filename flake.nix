1a7d8dceb1de2c92757b06366dc2b1e55628f31e
thedavidmeister <thedavidmeister@gmail.com>
2024-01-11 22:12:28 +0400

    update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    rainix.url = "github:rainprotocol/rainix/553ab725610bb381ea2b9f7440909fb3e2360e31";
+    rainix.url = "github:rainprotocol/rainix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
