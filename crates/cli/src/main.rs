012ac0abf172bc5ca08246f06a6e827834fa8e47
Matt Gabrenya <matt@buildyourweb.app>
2024-01-13 14:55:44 -0700

    feat(cli): modify tracing subscriber formatting & level

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use clap::Parser;
 use rain_orderbook_cli::Orderbook;
+use tracing_subscriber;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -8,9 +10,21 @@ struct Cli {
     #[command(subcommand)]
     orderbook: Orderbook,
 }
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing::subscriber::set_global_default(tracing_subscriber::fmt::Subscriber::new())?;
+    // Config tracing subscriber output
+    let filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env()?;
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_thread_names(false)
+        .with_thread_ids(false)
+        .with_target(false)
+        .without_time()
+        .compact()
+        .init();
 
     let cli = Cli::parse();
     cli.orderbook.execute().await
