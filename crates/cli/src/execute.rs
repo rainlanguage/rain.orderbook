a4b8d81e7014b146696df7929ba7bdb96e52fd8e
Matt Gabrenya <matt@buildyourweb.app>
2024-01-13 14:54:53 -0700

    feat(cli): tracing messages when awaiting connection to ledger and awaiting signature

--- crates/cli/src/execute.rs
+++ crates/cli/src/execute.rs
@@ -6,6 +6,7 @@ use clap::Args;
 use clap::FromArgMatches;
 use clap::Parser;
 use rain_orderbook_common::transaction::TransactionArgs;
+use tracing::info;
 
 #[derive(Parser)]
 pub struct ExecutableTransactionCall<T: FromArgMatches + Args> {
@@ -20,12 +21,15 @@ impl<T: FromArgMatches + Args> ExecutableTransactionCall<T> {
     pub async fn execute_transaction_call(self, call: impl SolCall) -> Result<()> {
         let tx_args: TransactionArgs = self.transaction_args.into();
         let request = tx_args.to_transaction_request_with_call(call).await?;
+
+        info!("Connecting to Ledger device");
         let ledger_client = tx_args.to_ledger_client().await?;
 
         let tx =
             ExecutableTransaction::from_alloy_transaction_request(request, ledger_client.client)
                 .await?;
 
+        info!("Awaiting signature from Ledger device");
         tx.execute().await.map_err(|e| anyhow::anyhow!(e))?;
         Ok(())
     }
