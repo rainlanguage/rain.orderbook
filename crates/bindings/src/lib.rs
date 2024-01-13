d1bd5d45122cc592f0e740449d50264c62fc1386
Matt Gabrenya <matt@buildyourweb.app>
2024-01-13 11:53:27 -0700

    feat(bindings): add erc20 bindings and basic tests

--- crates/bindings/src/lib.rs
+++ crates/bindings/src/lib.rs
@@ -4,3 +4,8 @@ sol!(
     #![sol(all_derives = true)]
     IOrderBookV3, "../../out/IOrderBookV3.sol/IOrderBookV3.json"
 );
+
+sol!(
+    #![sol(all_derives = true)]
+    IERC20, "../../out/IERC20.sol/IERC20.json"
+);
