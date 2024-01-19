1d3dea6c557193f61aa7f0650d1d16d49c0746f2
Matt Gabrenya <matt@buildyourweb.app>
2024-01-19 16:24:51 -0700

    refactor(common): remove deposit from common

--- crates/cli/src/commands/vault/deposit.rs
+++ crates/cli/src/commands/vault/deposit.rs
@@ -3,13 +3,48 @@ use crate::{
     transaction::{CliTransactionCommandArgs, ExecuteTransaction},
 };
 use alloy_ethers_typecast::ethers_address_to_alloy;
+use alloy_primitives::{Address, U256};
+use anyhow::Result;
 use anyhow::Result;
 use clap::Args;
 use rain_orderbook_bindings::{IOrderBookV3::depositCall, IERC20::approveCall};
-use rain_orderbook_common::deposit::DepositArgs;
+use std::convert::TryInto;
 use tracing::info;
 
-pub type Deposit = CliTransactionCommandArgs<CliDepositArgs>;
+#[derive(Args, Clone)]
+pub struct DepositArgs {
+    #[arg(short, long, help = "The token address in hex format")]
+    token: String,
+
+    #[arg(short, long, help = "The ID of the vault")]
+    vault_id: u64,
+
+    #[arg(short, long, help = "The amount to deposit")]
+    amount: u64,
+}
+
+impl TryInto<depositCall> for DepositArgs {
+    type Error = anyhow::Error;
+
+    fn try_into(self) -> Result<depositCall> {
+        Ok(depositCall {
+            token: self.token.parse::<Address>()?,
+            vaultId: U256::from(self.vault_id),
+            amount: U256::from(self.amount),
+        })
+    }
+}
+
+impl DepositArgs {
+    pub fn try_into_approve_call(self, spender: Address) -> Result<approveCall> {
+        Ok(approveCall {
+            spender,
+            amount: U256::from(self.amount),
+        })
+    }
+}
+
+pub type Deposit = CliTransactionCommandArgs<DepositArgs>;
 
 impl Execute for Deposit {
     async fn execute(&self) -> Result<()> {
@@ -33,24 +68,61 @@ impl Execute for Deposit {
     }
 }
 
-#[derive(Args, Clone)]
-pub struct CliDepositArgs {
-    #[arg(short, long, help = "The token address in hex format")]
-    token: String,
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy_primitives::{hex, Address};
 
-    #[arg(short, long, help = "The ID of the vault")]
-    vault_id: u64,
+    #[test]
+    fn test_deposit_args_try_into() {
+        let args = DepositArgs {
+            token: "0x1234567890abcdef1234567890abcdef12345678".to_string(),
+            vault_id: 42,
+            amount: 100,
+        };
 
-    #[arg(short, long, help = "The amount to deposit")]
-    amount: u64,
-}
+        let result: Result<depositCall, _> = args.try_into();
 
-impl From<CliDepositArgs> for DepositArgs {
-    fn from(val: CliDepositArgs) -> Self {
-        DepositArgs {
-            token: val.token,
-            vault_id: val.vault_id,
-            amount: val.amount,
+        match result {
+            Ok(_) => (),
+            Err(e) => panic!("Unexpected error: {}", e),
         }
+
+        assert!(result.is_ok());
+
+        let deposit_call = result.unwrap();
+        assert_eq!(
+            deposit_call.token,
+            "0x1234567890abcdef1234567890abcdef12345678"
+                .parse::<Address>()
+                .unwrap()
+        );
+        assert_eq!(deposit_call.vaultId, U256::from(42));
+        assert_eq!(deposit_call.amount, U256::from(100));
+    }
+
+    #[test]
+    fn test_deposit_args_try_into_approve_call() {
+        let args = DepositArgs {
+            token: "0x1234567890abcdef1234567890abcdef12345678".to_string(),
+            vault_id: 42,
+            amount: 100,
+        };
+        let spender_address = Address::repeat_byte(0x11);
+        let result: Result<approveCall, _> = args.try_into_approve_call(spender_address);
+
+        match result {
+            Ok(_) => (),
+            Err(e) => panic!("Unexpected error: {}", e),
+        }
+
+        assert!(result.is_ok());
+
+        let approve_call = result.unwrap();
+        assert_eq!(approve_call.amount, U256::from(100));
+        assert_eq!(
+            approve_call.spender,
+            hex!("1111111111111111111111111111111111111111")
+        );
     }
 }
