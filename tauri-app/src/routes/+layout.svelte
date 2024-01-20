b32eacb4840936222cfe4ec449b1824c5dd0b3b9
Matt Gabrenya <matt@buildyourweb.app>
2024-01-20 13:16:07 -0700

    chore(tauri/ui): short import path

--- tauri-app/src/routes/+layout.svelte
+++ tauri-app/src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import '@fontsource/dm-sans/400.css';
   import '@fontsource/dm-sans/600.css';
   import '@fontsource/dm-sans/800.css';
-  import Sidebar from '../lib/Sidebar.svelte';
+  import Sidebar from '$lib/Sidebar.svelte';
 </script>
 
 <div class="flex min-h-screen w-full justify-start bg-white p-2 dark:bg-gray-600">
