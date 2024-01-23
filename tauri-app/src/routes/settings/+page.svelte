6ab6ca15f95d8d2bddaf711cc27d9e49d2ed763d
Matt Gabrenya <matt@buildyourweb.app>
2024-01-22 19:55:54 -0700

    feat(tauri/ui): limit width of settings page content

--- tauri-app/src/routes/settings/+page.svelte
+++ tauri-app/src/routes/settings/+page.svelte
@@ -18,55 +18,62 @@
 
 <Heading tag="h1" class="mb-8 text-center text-4xl font-bold">Settings</Heading>
 
-{#if !$isSettingsDefinedAndValid}
-  <Alert color="red" class="m-8 text-lg">
-    Please fill in all the settings to use the Orderbook.
-  </Alert>
-{/if}
+<div class="flex w-full justify-center">
+  <div class="max-w-screen-lg">
+    {#if !$isSettingsDefinedAndValid}
+      <Alert color="red" class="m-8 text-lg">
+        Please fill in all the settings to use the Orderbook.
+      </Alert>
+    {/if}
 
-<div class="mb-8">
-  <Label class="bold mb-2 block text-xl">RPC URL</Label>
-  <Input label="RPC URL" name="rpcUrl" required bind:value={$rpcUrl} />
-  {#if !$isRpcUrlValid && $rpcUrl.length > 0}
-    <Helper class="mt-2 text-sm" color="red">Invalid URL</Helper>
-  {/if}
-  <Helper class="mt-2 text-sm">
-    The URL of the blockchain node RPC endpoint you will use to submit Orderbook transactions. You
-    can setup a hosted RPC account at <BadgeExternalLink href="https://infura.io" text="Infura" />
-    for a reliable hosted RPC service provider. Or visit
-    <BadgeExternalLink href="https://chainlist.org/" text="Chainlist" /> for find other publically available
-    RPC providerss.
-  </Helper>
-</div>
+    <div class="mb-8">
+      <Label class="bold mb-2 block text-xl">RPC URL</Label>
+      <Input label="RPC URL" name="rpcUrl" required bind:value={$rpcUrl} />
+      {#if !$isRpcUrlValid && $rpcUrl.length > 0}
+        <Helper class="mt-2 text-sm" color="red">Invalid URL</Helper>
+      {/if}
+      <Helper class="mt-2 text-sm">
+        The URL of the blockchain node RPC endpoint you will use to submit Orderbook transactions.
+        You can setup a hosted RPC account at <BadgeExternalLink
+          href="https://infura.io"
+          text="Infura"
+        />
+        for a reliable hosted RPC service provider. Or visit
+        <BadgeExternalLink href="https://chainlist.org/" text="Chainlist" /> for find other publically
+        available RPC providerss.
+      </Helper>
+    </div>
 
-<div class="mb-8">
-  <Label class="bold mb-2 block text-xl">Subgraph URL</Label>
-  <Input label="Subgraph URL" name="subgraphUrl" required bind:value={$subgraphUrl} />
-  {#if !$isSubgraphUrlValid && $subgraphUrl.length > 0}
-    <Helper class="mt-2 text-sm" color="red">Invalid URL</Helper>
-  {/if}
-  <Helper class="mt-2 text-sm">
-    The URL of the Subgraph you will use to query Orderbook data. Contact us for help setting up a
-    Subgraph for your Orderbook deployment.
-  </Helper>
-</div>
+    <div class="mb-8">
+      <Label class="bold mb-2 block text-xl">Subgraph URL</Label>
+      <Input label="Subgraph URL" name="subgraphUrl" required bind:value={$subgraphUrl} />
+      {#if !$isSubgraphUrlValid && $subgraphUrl.length > 0}
+        <Helper class="mt-2 text-sm" color="red">Invalid URL</Helper>
+      {/if}
+      <Helper class="mt-2 text-sm">
+        The URL of the Subgraph you will use to query Orderbook data. Contact us for help setting up
+        a Subgraph for your Orderbook deployment.
+      </Helper>
+    </div>
 
-<div class="mb-8">
-  <Label class="bold mb-2 block text-xl">Orderbook Address</Label>
-  <Input label="Subgraph URL" name="orderbookAddress" required bind:value={$orderbookAddress} />
-  {#if !$isOrderbookAddressValid && $orderbookAddress.length > 0}
-    <Helper class="mt-2 text-sm" color="red">Invalid Address</Helper>
-  {/if}
-  <Helper class="mt-2 text-sm">
-    The address of the deployed OrderbookV3 contract. Contact us for help setting up an Orderbook
-    deployment.
-  </Helper>
-</div>
+    <div class="mb-8">
+      <Label class="bold mb-2 block text-xl">Orderbook Address</Label>
+      <Input label="Subgraph URL" name="orderbookAddress" required bind:value={$orderbookAddress} />
+      {#if !$isOrderbookAddressValid && $orderbookAddress.length > 0}
+        <Helper class="mt-2 text-sm" color="red">Invalid Address</Helper>
+      {/if}
+      <Helper class="mt-2 text-sm">
+        The address of the deployed OrderbookV3 contract. Contact us for help setting up an
+        Orderbook deployment.
+      </Helper>
+    </div>
 
-<div class="mb-8">
-  <Label class="bold mb-2 block text-xl">Ledger Wallet</Label>
-  <InputLedgerWallet
-    bind:derivationIndex={$walletDerivationIndex}
-    bind:walletAddress={$walletAddress}
-  />
+    <div class="mb-8">
+      <Label class="bold mb-2 block text-xl">Ledger Wallet</Label>
+      <InputLedgerWallet
+        bind:derivationIndex={$walletDerivationIndex}
+        bind:walletAddress={$walletAddress}
+      />
+    </div>
+  </div>
 </div>
