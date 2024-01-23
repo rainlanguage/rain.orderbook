ed09e6e145f593e8baf24a6cf215f921c93fbf74
Matt Gabrenya <matt@buildyourweb.app>
2024-01-23 14:11:36 -0700

    feat(tauri/ui): include all chains to display names

--- tauri-app/src/lib/stores/chain.ts
+++ tauri-app/src/lib/stores/chain.ts
@@ -1,8 +1,6 @@
 import  find from 'lodash/find';
 import { derived, writable } from 'svelte/store';
-import { mainnet, polygon, flare } from 'viem/chains'
-
-const SUPPORTED_CHAINS = [mainnet, polygon, flare];
+import * as chains from 'viem/chains'
 
 export const chainId = writable(parseInt(localStorage.getItem("settings.chainId") || '1'))
 
@@ -10,8 +8,6 @@ chainId.subscribe(value => {
   localStorage.setItem("settings.chainId", (value || 0).toString());
 });
 
-export const isChainIdSupported = derived(chainId, (val) => SUPPORTED_CHAINS.map((c) => c.id).includes(val));
-
 export const activeChain = derived(chainId, (val) => {  
-  return find(SUPPORTED_CHAINS, (c) => c.id === val);
+  return find(Object.values(chains), (c) => c.id === val);
 })
