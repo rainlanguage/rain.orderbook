0e597b369d2b09e109a047fa793dc4014b7c3864
Matt Gabrenya <matt@buildyourweb.app>
2024-01-30 17:10:31 -0700

    refactor(tauri/ui): extract paginated store logic into generic paginated & cached store

--- tauri-app/src/lib/stores/ordersList.ts
+++ tauri-app/src/lib/stores/ordersList.ts
@@ -1,32 +1,7 @@
-import { derived, get, writable } from 'svelte/store';
+import { get } from 'svelte/store';
 import type { Order } from '$lib/typeshare/ordersList';
 import { invoke } from '@tauri-apps/api';
 import { subgraphUrl } from '$lib/stores/settings';
+import { usePaginatedCachedStore } from '$lib/stores/paginatedStore';
 
-function useOrdersPagesStore() {
-  const STORAGE_KEY = "orders.ordersList";
-
-  const ordersPages = writable<{[page: number]: Array<Order>}>(localStorage.getItem(STORAGE_KEY) ? JSON.parse(localStorage.getItem(STORAGE_KEY) as string) : []);
-
-  ordersPages.subscribe(value => {
-    if(value) {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(value));
-    } else {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
-    }
-  });
-
-  return ordersPages;
-}
-
-export const ordersPages = useOrdersPagesStore();
-
-export async function refetchOrdersPage(page: number = 1, pageSize: number = 10) {
-  const res: Array<Order> = await invoke("orders_list", {subgraphArgs: { url: get(subgraphUrl)}, paginationArgs: { page, page_size: pageSize } });
-  ordersPages.update((val) => {
-    val[page] = res;
-    return val;
-  });
-}
-
-export const ordersPage = derived(ordersPages, $ordersPages => (page: number) => $ordersPages[page] || []);
+export const ordersList = usePaginatedCachedStore<Order>('ordersList', (page, pageSize = 10) => invoke("orders_list", {subgraphArgs: { url: get(subgraphUrl)}, paginationArgs: { page, page_size: pageSize } }));
