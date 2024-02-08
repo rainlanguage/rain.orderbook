c0e66dc14ec1869b5b0a29c8cd53fa7dc0729780
Matt Gabrenya <matt@buildyourweb.app>
2024-02-08 09:12:45 -0700

    refactor(cli): instead of writing csv to file, print to stdout

--- tauri-app/src-tauri/src/commands/order.rs
+++ tauri-app/src-tauri/src/commands/order.rs
@@ -8,7 +8,7 @@ use rain_orderbook_common::{
 };
 use rain_orderbook_subgraph_client::{
     types::{flattened::{OrderFlattened, TryIntoFlattenedError},  order_detail, orders_list},
-    WriteCsv,
+    TryIntoCsv,
     PaginationArgs
 };
 use std::path::PathBuf;
