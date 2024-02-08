5fb410bac24a16e89cd1e1f986055b5a14cabc9b
Matt Gabrenya <matt@buildyourweb.app>
2024-02-07 22:12:32 -0700

    feat(tauri/ui): improve styling of vault link on order detail page

--- tauri-app/src/lib/utils/address.ts
+++
@@ -1,8 +0,0 @@
-import { getAddress, isAddress } from "viem";
-
-export function formatAddressShorthand(address: string, size=4) {
-  if(!isAddress(address)) throw Error("Must be a valid address");
-  const cased = getAddress(address);
-
-  return `${cased.slice(0, size+2)}...${cased.slice(cased.length-size, cased.length)}`;
-}
