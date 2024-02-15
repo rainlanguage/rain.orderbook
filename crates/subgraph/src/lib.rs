e6fc45d8b9b9f13d4e709733a29553bf55eccbc3
Matt Gabrenya <matt@buildyourweb.app>
2024-02-14 20:39:02 -0700

    chore(subgraph): file rename for clarity

--- crates/subgraph/src/lib.rs
+++ crates/subgraph/src/lib.rs
@@ -1,4 +1,4 @@
-mod client;
+mod orderbook_client;
 mod csv;
 mod cynic_client;
 mod pagination;
@@ -8,6 +8,6 @@ pub mod utils;
 #[cynic::schema("orderbook")]
 pub mod schema {}
 
-pub use client::{OrderbookSubgraphClient, OrderbookSubgraphClientError};
+pub use orderbook_client::{OrderbookSubgraphClient, OrderbookSubgraphClientError};
 pub use csv::{TryIntoCsv, TryIntoCsvError};
 pub use pagination::{PageQueryClient, PaginationArgs};
