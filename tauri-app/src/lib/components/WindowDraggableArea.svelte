bab0c19e54d9df99e474c186e45dbe6dfaa76882
Matt Gabrenya <matt@buildyourweb.app>
2024-02-17 14:03:31 -0700

    fix(tauri/ui): ensure draggable region is above modals

--- tauri-app/src/lib/components/WindowDraggableArea.svelte
+++ tauri-app/src/lib/components/WindowDraggableArea.svelte
@@ -12,5 +12,5 @@
 <!-- The titlebar is only hidden on macos -->
 {#if currentPlatform === 'darwin'}
   <!-- On this div the tauri window will be draggable -->
-  <div class="absolute w-full h-8 top-0 z-50" data-tauri-drag-region></div>
+  <div class="absolute w-full h-[28px] top-0 z-100" data-tauri-drag-region></div>
 {/if}
