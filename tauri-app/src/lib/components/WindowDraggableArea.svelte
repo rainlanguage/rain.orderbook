471641d59a414523b2abf0aa148aa7af30c76de5
Matt Gabrenya <matt@buildyourweb.app>
2024-02-16 17:25:07 -0700

    fix(tauri/ui): os platform name

--- tauri-app/src/lib/components/WindowDraggableArea.svelte
+++ tauri-app/src/lib/components/WindowDraggableArea.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <!-- The titlebar is only hidden on macos -->
-{#if currentPlatform === 'macos'}
+{#if currentPlatform === 'darwin'}
   <!-- On this div the tauri window will be draggable -->
   <div class="absolute w-full h-8 top-0 z-50" data-tauri-drag-region></div>
 {/if}
