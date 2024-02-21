bb54e204fc4f9f06e1e679372dc405818e1c4aeb
Matt Gabrenya <matt@buildyourweb.app>
2024-02-20 21:25:39 -0700

    revert: temporarily remove order clears feature, until it can be merged with takes

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -18,9 +18,6 @@ pub enum Orderbook {
     Vault(Box<Vault>),
 
     #[command(subcommand)]
-    OrderClear(OrderClear),
-
-    #[command(subcommand)]
     OrderTake(OrderTake),
 }
 
@@ -29,7 +26,6 @@ impl Orderbook {
         match self {
             Orderbook::Order(order) => order.execute().await,
             Orderbook::Vault(vault) => (*vault).execute().await,
-            Orderbook::OrderClear(order_clear) => (order_clear).execute().await,
             Orderbook::OrderTake(order_take) => (order_take).execute().await,
         }
     }
