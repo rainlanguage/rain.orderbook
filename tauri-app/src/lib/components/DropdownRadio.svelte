818f998ed27c54f2ec3219325deb7be667c856db
Matt Gabrenya <matt@buildyourweb.app>
2024-02-22 09:42:00 -0700

    feat(tauri/ui): minor styling improvements to dropdown

--- tauri-app/src/lib/components/DropdownRadio.svelte
+++ tauri-app/src/lib/components/DropdownRadio.svelte
@@ -15,9 +15,9 @@
   <ChevronDownSolid class="w-3 h-3 mx-2 text-black dark:text-white" />
 </Button>
 
-<Dropdown class="w-72 p-0" bind:open>
+<Dropdown class="py-0 w-72" bind:open>
   {#each options as option, index}
-    <Radio bind:group={value} value={index} class="w-full p-3 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+    <Radio bind:group={value} value={index} class="w-full p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600">
       <div class="ml-2">
         <slot name="option" {option} {index}></slot>
       </div>
