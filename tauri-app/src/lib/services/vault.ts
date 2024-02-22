1d2f09a77a5a34ba7529d859d88d465358744dbe
Matt Gabrenya <matt@buildyourweb.app>
2024-02-21 23:58:34 -0700

    revert(tauri/ui): uncessary loading of derives

--- tauri-app/src/lib/services/vault.ts
+++ tauri-app/src/lib/services/vault.ts
@@ -4,12 +4,6 @@ import { rpcUrl, orderbookAddress, chainId } from '$lib/stores/settings';
 import { walletDerivationIndex } from '$lib/stores/wallets';
 
 export async function vaultDeposit(vaultId: bigint, token: string, amount: bigint) {
-  const [rpc_url, orderbook_address, chain_id] = await Promise.all([
-    rpcUrl.load(),
-    orderbookAddress.load(),
-    chainId.load()
-  ]);
-
   await invoke("vault_deposit", {
     depositArgs: {
       vault_id: vaultId.toString(),
@@ -17,21 +11,15 @@ export async function vaultDeposit(vaultId: bigint, token: string, amount: bigin
       amount: amount.toString(),
     },
     transactionArgs: {
-      rpc_url,
-      orderbook_address,
+      rpc_url: get(rpcUrl),
+      orderbook_address: get(orderbookAddress),
       derivation_index: get(walletDerivationIndex),
-      chain_id
+      chain_id: get(chainId),
     }
   });
 }
 
 export async function vaultWithdraw(vaultId: bigint, token: string, targetAmount: bigint) {
-  const [rpc_url, orderbook_address, chain_id] = await Promise.all([
-    rpcUrl.load(),
-    orderbookAddress.load(),
-    chainId.load()
-  ]);
-
   await invoke("vault_withdraw", {
     withdrawArgs: {
       vault_id: vaultId.toString(),
@@ -39,10 +27,10 @@ export async function vaultWithdraw(vaultId: bigint, token: string, targetAmount
       target_amount: targetAmount.toString(),
     },
     transactionArgs: {
-      rpc_url,
-      orderbook_address,
+      rpc_url: get(rpcUrl),
+      orderbook_address: get(orderbookAddress),
       derivation_index: get(walletDerivationIndex),
-      chain_id,
+      chain_id: get(chainId)
     }
   });
 }
