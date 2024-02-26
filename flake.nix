f69302477aacd4f7deb065142c4484943bf169f3
thedavidmeister <thedavidmeister@gmail.com>
2024-02-26 07:13:05 +0400

    debug

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    rainix.url = "github:rainprotocol/rainix";
+    rainix.url = "github:rainprotocol/rainix/d91b87813b2ae7a98fa545ff8f063fd1c5784fb8";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
