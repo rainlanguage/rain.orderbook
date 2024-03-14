20817fe9334069bff5ab372449a3d18ea4b623d4
thedavidmeister <thedavidmeister@gmail.com>
2024-03-14 15:39:39 +0400

    standardise tauri prep

--- prep-tauri.sh
+++ prep-tauri.sh
@@ -5,17 +5,16 @@ nix develop --command rainix-sol-prelude
 nix develop --command rainix-rs-prelude
 
 # Run commands in lib/rain.interpreter
-pushd lib/rain.interpreter
+cd lib/rain.interpreter
 nix develop --command rainix-sol-prelude
 nix develop --command rainix-rs-prelude
 nix develop --command i9r-prelude
-popd
+cd -
 
-# Run commands in tauri-app
-pushd tauri-app
-nix develop .#tauri-shell --command cargo build --verbose
 nix develop .#tauri-shell --command ob-tauri-prelude
 nix develop .#tauri-shell --command ob-tauri-test
-# Use tauri dev instead of tauri build
-nix develop .#tauri-shell --command cargo tauri dev --verbose
-popd
+
+# Run commands in tauri-app
+cd tauri-app
+nix develop .#tauri-shell --command cargo build --verbose
+cd -
