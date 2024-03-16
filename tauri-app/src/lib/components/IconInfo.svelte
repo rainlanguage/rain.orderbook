3385413f2ae01321f59545db2f8b07a0d6829c6b
Matt Gabrenya <matt@buildyourweb.app>
2024-03-15 23:06:27 -0700

    refactor(tauri/ui): toasts have fixed height, scroll when overflowed, close button positioned at top-right

---
+++ tauri-app/src/lib/components/IconInfo.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import InfoCircleSolid from 'flowbite-svelte-icons/InfoCircleSolid.svelte';
+</script>
+
+<div class="rounded-lg inline-flex items-center justify-center shrink-0 text-blue-500 bg-blue-100 dark:bg-blue-800 dark:text-blue-200 w-8 h-8">
+  <InfoCircleSolid class="h-5 w-5" />
+</div>
