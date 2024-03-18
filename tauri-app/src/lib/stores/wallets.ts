2dcb886122cb8bafc1088ecdd26d27fc7d0a96ae
rouzwelt <rouzweltt@gmail.com>
2024-03-18 00:16:39 +0000

    fix action button show for owner

--- tauri-app/src/lib/stores/wallets.ts
+++ tauri-app/src/lib/stores/wallets.ts
@@ -2,9 +2,18 @@ import { cachedWritableInt } from "$lib/storesGeneric/cachedWritableStore";
 import { validatedStringStore } from "$lib/storesGeneric/settingStore";
 import { derived } from "svelte/store";
 import { isAddress } from "viem";
+import { walletconnectAccount } from "$lib/stores/walletconnect"
 
 export const walletAddress = validatedStringStore("settings.walletAddress", "", isAddress);
 export const walletDerivationIndex = cachedWritableInt("settings.walletDerivationIndex", 0);
-export const walletAddressMatchesOrBlank = derived(walletAddress, $walletAddress => {
-  return (otherAddress: string) => $walletAddress.value === otherAddress || $walletAddress.value === "";
-});
+export const walletAddressMatchesOrBlank = derived(
+  [walletAddress, walletconnectAccount],
+  ([$walletAddress, $walletconnectAccount]) => {
+    return (otherAddress: string) => {
+      const otherAddressLowercase = otherAddress.toLowerCase();
+      return $walletAddress.value.toLowerCase() === otherAddressLowercase
+        || $walletconnectAccount?.toLowerCase() === otherAddressLowercase
+        || $walletAddress.value === "";
+    }
+  }
+);
