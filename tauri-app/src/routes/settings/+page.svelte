1ce5e63d4b6bb75e300b28ba011e9b9410cf2b3c
rouzwelt <rouzweltt@gmail.com>
2024-03-18 21:24:04 +0000

    cleanup ui layout for chooing wallet

--- tauri-app/src/routes/settings/+page.svelte
+++ tauri-app/src/routes/settings/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-  import { Alert, Label, } from 'flowbite-svelte';
-  import {
-    hasRequiredSettings,
-    settingsText,
-  } from '$lib/stores/settings';
+  import { Alert, Button, Modal } from 'flowbite-svelte';
+  import { hasRequiredSettings, settingsText } from '$lib/stores/settings';
   import PageHeader from '$lib/components/PageHeader.svelte';
   import CodeMirrorConfigString from '$lib/components/CodeMirrorConfigString.svelte';
   import ButtonLoading from '$lib/components/ButtonLoading.svelte';
@@ -11,11 +8,25 @@
   import FileTextarea from '$lib/components/FileTextarea.svelte';
   import InputLedgerWallet from '$lib/components/InputLedgerWallet.svelte';
   import { ledgerWalletDerivationIndex, ledgerWalletAddress } from '$lib/stores/wallets';
-  import WalletConnect from '$lib/components/WalletConnect.svelte';
+  import { walletconnectModal, walletconnectAccount } from '$lib/stores/walletconnect';
+
+  let open = false;
+  let selectedLedger = false;
+  let selectedWalletconnect = false;
 
   function apply() {
     settingsText.set($settingsFile.text);
   };
+
+  $: walletconnectLabel = $walletconnectAccount
+    ? `${$walletconnectAccount.slice(0, 5)}...${$walletconnectAccount.slice(-5)}`
+    : "CONNECT"
+
+  function reset() {
+    open = false;
+    selectedLedger = false;
+    selectedWalletconnect = false;
+  }
 </script>
 
 <PageHeader title="Settings" />
@@ -47,15 +58,35 @@
   </svelte:fragment>
 </FileTextarea>
 
-<div class="my-8">
-  <Label class="mb-2">Ledger Wallet</Label>
-  <InputLedgerWallet
-    bind:derivationIndex={$ledgerWalletDerivationIndex}
-    bind:walletAddress={$ledgerWalletAddress.value}
-  />
+<div class="flex flex-col w-full w-full py-4">
+  <Button color="blue" on:click={() => open = true}>Connect to Wallet</Button>
 </div>
 
-<div class="my-8">
-  <Label class="mb-2">Wallet Connect</Label>
-  <WalletConnect/>
-</div>
+<Modal title="Connect to Wallet" bind:open={open} outsideclose size="sm" on:close={reset}>
+  {#if !selectedLedger && !selectedWalletconnect}
+    <div class="flex flex-col w-full justify-between space-y-2">
+      <Button on:click={() => selectedLedger = true}>Ledger Wallet</Button>
+      <Button on:click={() => selectedWalletconnect = true}>WalletConnect</Button>
+    </div>
+  {:else if selectedLedger}
+    <Button color="alternative" on:click={() => selectedLedger = false}>Back</Button>
+    <InputLedgerWallet
+      bind:derivationIndex={$ledgerWalletDerivationIndex}
+      bind:walletAddress={$ledgerWalletAddress.value}
+    />
+  {:else if selectedWalletconnect}
+    <Button color="alternative" on:click={() => selectedWalletconnect = false}>Back</Button>
+    <div class="text-lg">Note that only <b>mobile</b> wallets are supported.</div>
+    <div class="flex flex-col w-full justify-between space-y-2">
+      <Button
+        color="blue"
+        class="px-2 py-1"
+        size="lg"
+        pill
+        on:click={() => $walletconnectModal?.open()}
+      >
+      {walletconnectLabel}
+      </Button>
+    </div>
+  {/if}
+</Modal>
