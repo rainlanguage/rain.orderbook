edf6d4248ebbdcb9a53a5a74d109ab830ff289c1
rouzwelt <rouzweltt@gmail.com>
2024-03-25 22:12:54 +0000

    style

--- tauri-app/src/lib/components/ModalExecute.svelte
+++ tauri-app/src/lib/components/ModalExecute.svelte
@@ -44,7 +44,7 @@
       bind:derivationIndex={$ledgerWalletDerivationIndex}
       bind:walletAddress={$ledgerWalletAddress.value}
     />
-    <ButtonLoading on:click={() => executeLedger().finally(() => reset())} disabled={isSubmitting || !$ledgerWalletAddress || $ledgerWalletDerivationIndex == undefined || $ledgerWalletDerivationIndex == null} loading={isSubmitting}>
+    <ButtonLoading class="w-full" on:click={() => executeLedger().finally(() => reset())} disabled={isSubmitting || !$ledgerWalletAddress || $ledgerWalletDerivationIndex == undefined || $ledgerWalletDerivationIndex == null} loading={isSubmitting}>
       {execButtonLabel}
     </ButtonLoading>
   {:else if selectedWalletconnect}
