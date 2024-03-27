1ef6372942b2338a67e3d10bef09c8bb505a1bd4
Matt Gabrenya <matt@buildyourweb.app>
2024-03-26 18:50:52 -0700

    chore(tauri-e2e-test): clearer test names

--- tauri-e2e-test/tests/startup_test.rs
+++ tauri-e2e-test/tests/startup_test.rs
@@ -7,7 +7,7 @@ use common::WebdriverTestContext;
 
 #[test_context(WebdriverTestContext)]
 #[tokio::test]
-async fn loads_sidebar(ctx: &mut WebdriverTestContext) {
+async fn has_sidebar(ctx: &mut WebdriverTestContext) {
     let _ = ctx.driver.query(By::Css("body > aside"))
         .nowait()
         .exists()
@@ -17,7 +17,7 @@ async fn loads_sidebar(ctx: &mut WebdriverTestContext) {
 
 #[test_context(WebdriverTestContext)]
 #[tokio::test]
-async fn opens_settings_page_on_first_launch(ctx: &mut WebdriverTestContext) {
+async fn navigates_to_settings_page_on_first_launch(ctx: &mut WebdriverTestContext) {
     let breadcrumb_title_elem = ctx
         .driver
         .query(By::Css("span[data-testid=breadcrumb-page-title]"))
@@ -32,7 +32,7 @@ async fn opens_settings_page_on_first_launch(ctx: &mut WebdriverTestContext) {
 
 #[test_context(WebdriverTestContext)]
 #[tokio::test]
-async fn opens_orders_page_when_valid_network_and_orderbook_settings(ctx: &mut WebdriverTestContext) {
+async fn navigates_to_orders_page_when_valid_network_and_orderbook_settings(ctx: &mut WebdriverTestContext) {
     ctx.apply_settings(common::MIN_VALID_SETTINGS.to_string()).await;
 
     let breadcrumb_title_elem = ctx
