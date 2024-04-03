8f8a804f62fa56b44450f353d7367562e96624c2
highonhopium <jks.hardy@gmail.com>
2024-04-03 13:32:32 +0100

    adding single metric option to chart type in frontmatter

--- tauri-app/src/lib/components/Charts.svelte
+++ tauri-app/src/lib/components/Charts.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
   import ObservableChart from '$lib/components/ObservableChart.svelte';
   import type { ChartData } from '$lib/typeshare/config';
+  import { transformData } from '$lib/utils/chartData';
+  import MetricChart from './MetricChart.svelte';
   export let chartData: ChartData;
 </script>
 
 {#if chartData}
   <div class="flex flex-col items-center">
     {#each Object.entries(chartData.charts) as chart}
+      {@const data = transformData(chartData.scenarios_data[chart[1].scenario.name])}
       <div class="w-full">
         <div class="flex flex-col justify-center gap-y-4">
           <h2 class="text-2xl font-bold">{chart[0]}</h2>
           <div class="grid w-full grid-cols-2 gap-4">
-            {#each chart[1].plots as plot}
+            {#each chart[1]?.metrics || [] as metric}
               <div class="col-span-1 flex flex-col gap-y-4">
-                <ObservableChart
-                  {plot}
-                  scenarioData={chartData.scenarios_data[chart[1].scenario.name]}
-                />
+                <MetricChart {metric} {data} />
+              </div>
+            {/each}
+            {#each chart[1]?.plots || [] as plot}
+              <div class="col-span-1 flex flex-col gap-y-4">
+                <ObservableChart {plot} {data} />
               </div>
             {/each}
           </div>
