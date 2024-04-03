3267cd20bf9ed525e633d1bc7842122d6ad7f9c4
highonhopium <jks.hardy@gmail.com>
2024-04-03 19:32:00 +0100

    scenario debug tables and auto rainlang generation

---
+++ tauri-app/src/lib/components/ScenarioDebugTable.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type { ChartData } from '$lib/typeshare/config';
+  import { transformData } from '$lib/utils/chartData';
+  import {
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+  } from 'flowbite-svelte';
+
+  export let chartData: ChartData;
+</script>
+
+{#if chartData}
+  <div class="flex w-full gap-x-4 overflow-x-scroll">
+    {#each Object.values(chartData.scenarios_data) as scenario}
+      {@const data = transformData(scenario)}
+      <div class="flex flex-col gap-y-2">
+        <span>{scenario.scenario}</span>
+        <Table divClass="cursor-pointer rounded-lg overflow-hidden dark:border-none border">
+          <TableHead>
+            <TableHeadCell>Stack item</TableHeadCell>
+            <TableHeadCell>Value</TableHeadCell>
+            <TableHeadCell>Hex</TableHeadCell>
+          </TableHead>
+          <TableBody>
+            {#each Object.entries(data[0]) as [key, value]}
+              <TableBodyRow>
+                <TableBodyCell>{key}</TableBodyCell>
+                <TableBodyCell>{value[0]}</TableBodyCell>
+                <TableBodyCell>{value[1]}</TableBodyCell>
+              </TableBodyRow>
+            {/each}
+          </TableBody>
+        </Table>
+      </div>
+    {/each}
+  </div>
+{:else}
+  No scenario data
+{/if}
