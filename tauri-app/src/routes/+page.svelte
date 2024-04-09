fa5e5a617e1d6b7c2beda316f066cfdc42dc31f1
highonhopium <jks.hardy@gmail.com>
2024-04-09 13:20:00 +0100

    lint

--- tauri-app/src/routes/+page.svelte
+++ tauri-app/src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import IconTelegram from '$lib/components/IconTelegram.svelte';
-  import { Button, Heading } from 'flowbite-svelte';
+  import { Button } from 'flowbite-svelte';
 </script>
 
 <div class="mx-auto flex max-w-prose flex-col items-center gap-y-8 pt-12">
-  <img class="w-[400px]" src="/logo-light.svg" />
+  <img alt="Raindex logo" class="w-[400px]" src="/logo-light.svg" />
   <div class="mt-4 text-2xl">
     Raindex is an onchain orderbook contract that enables users to deploy complex, onchain
     strategies for managing token trades.
@@ -18,6 +18,7 @@
   </div>
   <div style="position: relative; padding-bottom: 64.63195691202873%; height: 0;" class="w-full">
     <iframe
+      title="Raindex Demo"
       src="https://www.loom.com/embed/fca750f31f0a43258891cea0ddacb588?sid=21583276-742b-49d5-a7db-b6e9e01ca418"
       frameborder="0"
       allowfullscreen
