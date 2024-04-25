da737f88e612cb367939d9913e96da247060b21a
Dan <dan.lewis@enspiral.com>
2024-04-25 22:29:41 +0200

    Refactor and add the Vault Balance

--- tauri-app/src/lib/components/ButtonVaultLink.svelte
+++ tauri-app/src/lib/components/ButtonVaultLink.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-  import type { TokenVault } from '$lib/typeshare/orderDetail';
+  import type { TokenVault } from '$lib/typeshare/vaultDetail';
   import { bigintStringToHex } from '$lib/utils/hex';
 
   export let tokenVault: TokenVault;
@@ -12,9 +12,16 @@
   class="hover:text-primary-600 cursor-pointer"
   on:click={() => goto(`/vaults/${tokenVault.id}`)}
 >
-  <div class="grid grid-cols-2 gap-x-2 break-normal">
-    <div>
-      {tokenVault.token.name} <span class="break-normal">({tokenVault.token.symbol})</span>
+  <div class="grid grid-cols-2 gap-x-2 break-normal leading-tight">
+    <div class="flex flex-col gap-y-5">
+      <div style="margin-top: 1px">
+        <span>{tokenVault.token.name} </span>
+        <span class="break-normal">({tokenVault.token.symbol})</span>
+      </div>
+      <div>
+        <p class="text-gray-500 dark:text-gray-400">Balance</p>
+        <span>{tokenVault.balance_display}</span>
+      </div>
     </div>
     <div>
       <span class="text-gray-500 dark:text-gray-400">ID</span>
