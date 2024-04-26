829008f7d8bb7f3e5e8fa5d61f23e66c7ffa8583
Dan <dan.lewis@enspiral.com>
2024-04-26 23:23:31 +0200

    init service wrapper for rust functions

--- tauri-app/src/lib/services/wallet.ts
+++ tauri-app/src/lib/services/wallet.ts
@@ -7,3 +7,33 @@ export const getAddressFromLedger = (derivationIndex: number): Promise<string> =
   chainId: get(chainId),
   rpcUrl: get(rpcUrl)
 });
+
+export const getBalanceFromLedger = async (derivationIndex: number): Promise<unknown> => {
+  try {
+    const balance = await invoke('get_balance_from_ledger', {
+      derivationIndex,
+      chainId: get(chainId),
+      rpcUrl: get(rpcUrl)
+    });
+    return balance;
+  } catch (error) {
+    //console.error('Error in getBalanceFromLedger:', error, 'derivationIndex', derivationIndex);
+  }
+};
+
+export const getBalanceFromWallet = async (address: string): Promise<string | undefined> => {
+  try {
+    const _rpcUrl = get(rpcUrl);
+    if (_rpcUrl) {
+      const balance = await invoke('get_balance_from_wallet', {
+        address,
+        rpcUrl: _rpcUrl
+      });
+      console.log('balance', balance);
+      return balance as string;
+    }
+    return undefined;
+  } catch (error) {
+    console.error({'Error in getBalance': error, 'address': address});
+  }
+}
