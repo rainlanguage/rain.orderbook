74e4509c1b8f9e11ce558aa56487c7a1b285d3e4
thedavidmeister <thedavidmeister@gmail.com>
2024-04-27 23:19:32 +0400

    update prep tauri

--- prep-tauri.sh
+++ prep-tauri.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
 # Run commands in the current working directory
-nix develop -i --command rainix-sol-prelude
-nix develop -i --command rainix-rs-prelude
-nix develop -i --command raindex-prelude
+nix develop -i -c rainix-sol-prelude
+nix develop -i -c rainix-rs-prelude
+nix develop -i -c raindex-prelude
 
 # Run commands in lib/rain.interpreter
-cd lib/rain.interpreter
-nix develop -i --command rainix-sol-prelude
-nix develop -i --command rainix-rs-prelude
-nix develop -i --command i9r-prelude
-cd -
+nix develop -i -c bash -c '(cd lib/rain.interpreter && rainix-sol-prelude)'
+nix develop -i -c bash -c '(cd lib/rain.interpreter && rainix-rs-prelude)'
+nix develop -i -c bash -c '(cd lib/rain.interpreter && i9r-prelude)'
 
 # Run commands in lib/rain.metadata
-cd lib/rain.metadata
-nix develop -i --command rainix-sol-prelude
-cd -
+nix develop -i -c bash -c '(cd lib/rain.metadata && rainix-sol-prelude)'
 
-nix develop -i .#tauri-shell --command ob-tauri-prelude
-nix develop -i .#tauri-shell --command ob-tauri-unit-test
+nix develop -i .#tauri-shell -c ob-tauri-prelude
+nix develop -i .#tauri-shell -c ob-tauri-unit-test
 
 # Run commands in tauri-app
-cd tauri-app
-nix develop -i .#tauri-shell --command cargo build --verbose
-cd -
+nix develop -i .#tauri-shell -c bash -c '(cd tauri-app && cargo build --verbose)'
