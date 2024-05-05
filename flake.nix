664f90fc75dee6bd9a71794041033725e084e541
thedavidmeister <thedavidmeister@gmail.com>
2024-05-05 12:17:42 +0400

    nix flake udpate

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    rainix.url = "github:rainlanguage/rainix/27352af51e6dc42a886cbe2f1e6ed995d5c70f69";
+    rainix.url = "github:rainlanguage/rainix";
     rain.url = "github:rainlanguage/rain.cli";
     flake-utils.url = "github:numtide/flake-utils";
   };
