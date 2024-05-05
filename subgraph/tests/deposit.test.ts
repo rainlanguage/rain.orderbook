373530f2464cf036e5617ac5b7c904ddf82d4023
thedavidmeister <thedavidmeister@gmail.com>
2024-05-05 12:07:38 +0400

    matchstick tests

--- subgraph/tests/deposit.test.ts
+++ subgraph/tests/deposit.test.ts
@@ -1,20 +1,20 @@
 import {
-    test,
-    assert,
-    createMockedFunction,
-    clearStore,
-    describe,
-    afterEach,
-    beforeAll,
-    afterAll,
-    newMockEvent,
-    clearInBlockStore
-  } from "matchstick-as";
+  test,
+  assert,
+  createMockedFunction,
+  clearStore,
+  describe,
+  afterEach,
+  beforeAll,
+  afterAll,
+  newMockEvent,
+  clearInBlockStore,
+} from "matchstick-as";
 import { Bytes, BigInt, ethereum, Address } from "@graphprotocol/graph-ts";
 
 describe("noop", () => {
-    test("noop", async () => {
-        let value = ethereum.Value.fromBytes(Bytes.fromHexString(""));
-        assert.equals(value, value);
-    });
+  test("noop", () => {
+    let value = ethereum.Value.fromBytes(Bytes.fromHexString(""));
+    assert.equals(value, value);
+  });
 });
