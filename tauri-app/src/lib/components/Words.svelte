1343a350d7a56ef26d83353ddf73c035333d1fff
highonhopium <jks.hardy@gmail.com>
2024-05-06 15:59:07 +0100

    styling

--- tauri-app/src/lib/components/Words.svelte
+++ tauri-app/src/lib/components/Words.svelte
@@ -23,20 +23,32 @@
                 pragma={scenario.result.data.deployer.address}
               />
             {:else if scenario.result.data.deployer.result.type === 'Error'}
-              <P>Error getting words for this deployer:</P>
-              <P>{scenario.result.data.deployer.result.data}</P>
+              <div
+                class="relative h-[500px] w-[450px] overflow-y-scroll rounded-lg border bg-white p-4 dark:border-none dark:bg-gray-800"
+              >
+                <p>Error getting words for this deployer:</p>
+                <p>{scenario.result.data.deployer.result.data}</p>
+              </div>
             {/if}
             {#each scenario.result.data.pragmas as pragma}
               {#if pragma.result.type === 'Success'}
                 <WordTable authoringMeta={pragma.result.data} pragma={pragma.address} />
               {:else if pragma.result.type === 'Error'}
-                <P>Error getting words for this pragma:</P>
-                <P>{pragma.result.data}</P>
+                <div
+                  class="relative h-[500px] w-[450px] overflow-y-scroll rounded-lg border bg-white p-4 dark:border-none dark:bg-gray-800"
+                >
+                  <p>Error getting words for the pragma {pragma.address}:</p>
+                  <p>{pragma.result.data}</p>
+                </div>
               {/if}
             {/each}
           {:else if scenario.result.type === 'Error'}
-            <P>Error getting words for this scenario:</P>
-            <P>{scenario.result.data}</P>
+            <div
+              class="relative h-[500px] w-[450px] overflow-y-scroll rounded-lg border bg-white p-4 dark:border-none dark:bg-gray-800"
+            >
+              <p>Error getting words for this scenario:</p>
+              <p>{scenario.result.data}</p>
+            </div>
           {/if}
         </div>
       </TabItem>
