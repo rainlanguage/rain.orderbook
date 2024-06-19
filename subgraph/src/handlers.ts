e925669970ccc8930e09bf3b21e44fb066a79656
highonhopium <jks.hardy@gmail.com>
2024-06-19 16:02:56 +0100

    handling vaults on deposit/withdraw

--- subgraph/src/handlers.ts
+++ subgraph/src/handlers.ts
@@ -1,12 +1,15 @@
 import { Deposit } from "../generated/OrderBook/OrderBook";
 import { Withdraw } from "../generated/OrderBook/OrderBook";
 import { createDepositEntity } from "./deposit";
+import { handleVaultDeposit, handleVaultWithdraw } from "./vault";
 import { createWithdrawalEntity } from "./withdraw";
 
 export function handleDeposit(event: Deposit): void {
   createDepositEntity(event);
+  handleVaultDeposit(event);
 }
 
 export function handleWithdraw(event: Withdraw): void {
   createWithdrawalEntity(event);
+  handleVaultWithdraw(event);
 }
