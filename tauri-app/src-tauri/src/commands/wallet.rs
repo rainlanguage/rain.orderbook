e416648ba5796d1d95430a990de24a36646ec62c
rouzwelt <rouzweltt@gmail.com>
2024-06-20 23:42:28 +0000

    init

--- tauri-app/src-tauri/src/commands/wallet.rs
+++ tauri-app/src-tauri/src/commands/wallet.rs
@@ -1,5 +1,5 @@
 use crate::error::CommandResult;
-use alloy_ethers_typecast::{client::LedgerClient, ethers_address_to_alloy};
+use alloy_ethers_typecast::{client::{LedgerClient, HDPath}, ethers_address_to_alloy};
 use alloy_primitives::Address;
 
 #[tauri::command]
@@ -9,7 +9,7 @@ pub async fn get_address_from_ledger(
     rpc_url: String,
 ) -> CommandResult<Address> {
     let ledger_client =
-        LedgerClient::new(derivation_index, chain_id, rpc_url.clone(), None).await?;
+        LedgerClient::new(derivation_index.map(HDPath::LedgerLive), chain_id, rpc_url.clone(), None).await?;
     let ledger_address = ethers_address_to_alloy(ledger_client.client.address());
 
     Ok(ledger_address)
