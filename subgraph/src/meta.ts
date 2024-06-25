80dbee4a9b2c1a2eaa4f7e80a66c2dac541dc6bb
highonhopium <jks.hardy@gmail.com>
2024-06-25 15:21:05 +0100

    order meta

---
+++ subgraph/src/meta.ts
@@ -0,0 +1,15 @@
+import { Bytes } from "@graphprotocol/graph-ts";
+import { MetaV1 } from "../generated/OrderBook/OrderBook";
+import { Order } from "../generated/schema";
+import { makeOrderId } from "./order";
+
+export function handleMeta(event: MetaV1): void {
+  // The order should already exist by the time the MetaV1 event is handled
+  let order = Order.load(
+    makeOrderId(Bytes.fromByteArray(Bytes.fromBigInt(event.params.subject)))
+  );
+  if (order != null) {
+    order.meta = event.params.meta;
+    order.save();
+  }
+}
---
+++ subgraph/tests/handlers/handle-meta.test.ts
@@ -0,0 +1,95 @@
+import {
+  test,
+  clearStore,
+  describe,
+  afterEach,
+  clearInBlockStore,
+  assert,
+  log,
+} from "matchstick-as";
+import { Bytes, BigInt, Address } from "@graphprotocol/graph-ts";
+import {
+  Evaluable,
+  IO,
+  createAddOrderEvent,
+  createMetaEvent,
+} from "../event-mocks.test";
+import { createOrderEntity, handleAddOrder } from "../../src/order";
+import { handleMeta } from "../../src/handlers";
+import { Order } from "../../generated/schema";
+
+describe("Add and remove orders", () => {
+  afterEach(() => {
+    clearStore();
+    clearInBlockStore();
+  });
+
+  test("handleMeta() should not error if there is no order", () => {
+    let event = createMetaEvent(
+      // sender
+      Address.fromString(
+        "0x1234567890abcdef1234567890abcdef12345678"
+      ) as Address,
+      // subject
+      BigInt.fromI32(1),
+      // meta
+      Bytes.fromHexString("0x1234567890abcdef1234567890abcdef12345678")
+    );
+
+    handleMeta(event);
+  });
+
+  test("handleMeta() should update the meta field of an order", () => {
+    let orderHash = Bytes.fromHexString(
+      "0x0987654321098765432109876543210987654321"
+    );
+    // first we need to create an order
+    let event = createAddOrderEvent(
+      Address.fromString("0x1234567890123456789012345678901234567890"),
+      orderHash,
+      [
+        new IO(
+          Address.fromString("0x1234567890123456789012345678901234567890"),
+          BigInt.fromI32(18),
+          BigInt.fromI32(1)
+        ),
+      ],
+      [
+        new IO(
+          Address.fromString("0x1234567890123456789012345678901234567890"),
+          BigInt.fromI32(18),
+          BigInt.fromI32(1)
+        ),
+      ],
+      Bytes.fromHexString("0x1234567890123456789012345678901234567890"),
+      new Evaluable(
+        Address.fromString("0x1234567890123456789012345678901234567890"),
+        Address.fromString("0x0987654321098765432109876543210987654321"),
+        Bytes.fromHexString("0x1234567890123456789012345678901234567890")
+      )
+    );
+
+    handleAddOrder(event);
+
+    let metaEvent = createMetaEvent(
+      // sender
+      Address.fromBytes(
+        Address.fromHexString("0x1234567890abcdef1234567890abcdef12345678")
+      ),
+      // subject
+      BigInt.fromByteArray(orderHash),
+      // meta
+      Bytes.fromHexString("0x1234567890abcdef1234567890abcdef12345678")
+    );
+
+    handleMeta(metaEvent);
+
+    // meta field on order should be updated
+    assert.fieldEquals(
+      "Order",
+      orderHash.toHexString(),
+      "meta",
+      "0x1234567890abcdef1234567890abcdef12345678"
+    );
+  });
+});
