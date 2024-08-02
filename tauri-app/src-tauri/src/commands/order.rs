96241511a230ac9f42def7f7555548c4b4202cd9
highonhopium <jks.hardy@gmail.com>
2024-08-02 19:26:32 +0100

    typo

--- tauri-app/src-tauri/src/commands/order.rs
+++ tauri-app/src-tauri/src/commands/order.rs
@@ -170,5 +170,5 @@ pub async fn order_remove_calldata(
 #[tauri::command]
 pub async fn compose_from_scenario(dotrain: String, scenario: Scenario) -> CommandResult<String> {
     let order = DotrainOrder::new(dotrain.clone(), None).await?;
-    Ok(order.compose_from_scenario(scenario).await?)
+    Ok(order.compose_scenario_to_rainlang(scenario).await?)
 }
