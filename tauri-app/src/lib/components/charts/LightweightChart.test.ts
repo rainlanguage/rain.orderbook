c254ffeec57d5b366a632ef79f1f610e521c4dee
highonhopium <jks.hardy@gmail.com>
2024-08-02 17:40:19 +0100

    wip

--- tauri-app/src/lib/components/charts/LightweightChart.test.ts
+++ tauri-app/src/lib/components/charts/LightweightChart.test.ts
@@ -7,5 +7,28 @@ import { mockIPC } from '@tauri-apps/api/mocks';
 import type { Vault } from '$lib/typeshare/vaultDetail';
 import { goto } from '$app/navigation';
 import { handleDepositModal, handleWithdrawModal } from '$lib/services/modal';
+import type { IChartApi } from 'lightweight-charts';
 
-test('shows query data in correct places', async () => {});
+vi.mock('lightweight-charts', async (importOriginal) => ({
+  ...((await importOriginal()) as object),
+  createChart: vi.fn(() => ({
+    addLineSeries: vi.fn(),
+    remove(): void {},
+    applyOptions: vi.fn(),
+  })),
+}));
+
+test('renders without data correctly', async () => {
+  const title = 'test title';
+  const emptyMessage = 'empty message';
+  let loading = true;
+  const priceSymbol = '$';
+  const createSeries = (chart: IChartApi) => chart.addLineSeries();
+
+  render(LightweightChart, { title, emptyMessage, loading, priceSymbol, createSeries });
+
+  await waitFor(() => {
+    expect(screen.getByTestId('lightweightChartTitle')).toHaveTextContent(title);
+    expect(screen.getByTestId('lightweightChartSpinner')).toBeInTheDocument();
+  });
+});
