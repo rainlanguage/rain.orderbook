63e6322fdf444645bf3a1d8b06c93a7dc381074f
rouzwelt <rouzweltt@gmail.com>
2024-08-12 17:28:20 +0000

    Update js_api.rs

--- crates/bindings/src/js_api.rs
+++ crates/bindings/src/js_api.rs
@@ -6,8 +6,8 @@ use crate::IOrderBookV4::{
 use alloy::sol_types::SolValue;
 use alloy::{
     primitives::{
-        hex::{encode_prefixed, FromHex},
-        keccak256, Address, U256,
+        hex::{decode, encode_prefixed, FromHex},
+        keccak256 as main_keccak256, Address, U256,
     },
     sol_types::SolCall,
 };
@@ -110,7 +110,9 @@ pub struct TakeOrdersConfigV3 {
 /// Get the order hash of an order
 #[wasm_bindgen(js_name = "getOrderHash")]
 pub fn get_order_hash(order: &OrderV3) -> String {
-    encode_prefixed(keccak256(MainOrderV3::from(order.clone()).abi_encode()))
+    encode_prefixed(main_keccak256(
+        MainOrderV3::from(order.clone()).abi_encode(),
+    ))
 }
 
 /// Get takeOrders2() calldata
@@ -122,6 +124,23 @@ pub fn get_take_orders2_calldata(take_orders_config: &TakeOrdersConfigV3) -> js_
     .abi_encode()
     .as_slice()
     .into()
+}
+
+/// calculates keccak256 of the given bytes
+#[wasm_bindgen]
+pub fn keccak256(bytes: &[u8]) -> String {
+    encode_prefixed(main_keccak256(bytes))
+}
+
+/// calculate keccak256 of a hex string
+#[wasm_bindgen(js_name = "keccak256HexString")]
+pub fn keccak256_hex_string(hex_string: &str) -> String {
+    let mut err = "".to_string();
+    encode_prefixed(main_keccak256(
+        decode(hex_string)
+            .inspect_err(|e| err.push_str(&e.to_string()))
+            .expect_throw(&err),
+    ))
 }
 
 impl From<EvaluableV3> for MainEvaluableV3 {
@@ -588,5 +607,23 @@ mod tests {
         }
         .abi_encode();
         assert_eq!(result.to_vec(), expected);
+    }
+
+    #[wasm_bindgen_test]
+    fn test_keccak256() {
+        let bytes = vec![1, 2];
+        let result = keccak256(&bytes);
+        let expected =
+            "0x22ae6da6b482f9b1b19b0b897c3fd43884180a1c5ee361e1107a1bc635649dda".to_string();
+        assert_eq!(result, expected);
+    }
+
+    #[wasm_bindgen_test]
+    fn test_keccak256_hex_string() {
+        let hex_string = "0x0102";
+        let result = keccak256_hex_string(&hex_string);
+        let expected =
+            "0x22ae6da6b482f9b1b19b0b897c3fd43884180a1c5ee361e1107a1bc635649dda".to_string();
+        assert_eq!(result, expected);
     }
 }
