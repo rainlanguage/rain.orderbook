ecbc4092b3dc7009d7a912b868124811e076cedd
findolor <anakisci@gmail.com>
2024-08-17 14:16:56 +0300

    fix: format order object before passing it into batch_order_quotes function

--- tauri-app/src/lib/services/orderQuote.ts
+++ tauri-app/src/lib/services/orderQuote.ts
@@ -1,12 +1,39 @@
 import { get } from 'svelte/store';
 import { invoke } from '@tauri-apps/api';
 import { rpcUrl, orderbookAddress, subgraphUrl } from '$lib/stores/settings';
+import type { Order } from '$lib/typeshare/orderDetail';
 
-export async function batchOrderQuotes(
-  orderHashes: string[],
-): Promise<{ maxOutput: string; ratio: string }[]> {
+export async function batchOrderQuotes(orders: Order[]): Promise<
+  {
+    pair_name: string;
+    success: boolean;
+    data: { maxOutput: string; ratio: string } | undefined;
+    error: string | undefined;
+  }[]
+> {
+  const formattedOrders = orders.map((order) => ({
+    ...order,
+    orderBytes: order.order_bytes,
+    orderHash: order.order_hash,
+    outputs: order.outputs.map((output) => ({
+      ...output,
+      vaultId: output.vault_id,
+    })),
+    inputs: order.inputs.map((input) => ({
+      ...input,
+      vaultId: input.vault_id,
+    })),
+    addEvents: order.add_events.map((event) => ({
+      ...event,
+      transaction: {
+        ...event.transaction,
+        blockNumber: event.transaction.block_number,
+      },
+    })),
+    timestampAdded: order.timestamp_added,
+  }));
   return invoke('batch_order_quotes', {
-    orderHashes,
+    orders: formattedOrders,
     subgraphUrl: get(subgraphUrl),
     rpcUrl: get(rpcUrl),
     orderbook: get(orderbookAddress),
