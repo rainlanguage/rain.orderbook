ee22297df88df446d54efa9bd698306c03889233
findolor <anakisci@gmail.com>
2024-08-17 14:27:24 +0300

    test: update UI tests for the new implementation

--- tauri-app/src/lib/components/detail/TanstackOrderQuote.test.ts
+++ tauri-app/src/lib/components/detail/TanstackOrderQuote.test.ts
@@ -4,6 +4,7 @@ import { QueryClient } from '@tanstack/svelte-query';
 import TanstackOrderQuote from './TanstackOrderQuote.svelte';
 import { expect } from '$lib/test/matchers';
 import { mockIPC } from '@tauri-apps/api/mocks';
+import { mockOrderDetailsExtended } from '$lib/queries/orderDetail';
 
 vi.mock('$lib/stores/settings', async (importOriginal) => {
   const { writable } = await import('svelte/store');
@@ -26,14 +27,28 @@ test('displays loading spinner while fetching data', async () => {
   mockIPC(() => {
     // Simulate a delay to trigger the loading state
     return new Promise((resolve) =>
-      setTimeout(() => resolve([{ maxOutput: '0x0', ratio: '0x0' }]), 100),
+      setTimeout(
+        () =>
+          resolve([
+            {
+              success: true,
+              pair_name: 'ETH/USDT',
+              data: { maxOutput: '0x0', ratio: '0x0' },
+              error: undefined,
+            },
+          ]),
+        100,
+      ),
     );
   });
 
   const queryClient = new QueryClient();
 
   render(TanstackOrderQuote, {
-    props: { orderHash: '0x123' },
+    props: {
+      id: '0x123',
+      order: mockOrderDetailsExtended.order,
+    },
     context: new Map([['$$_queryClient', queryClient]]),
   });
 
@@ -47,23 +62,32 @@ test('displays loading spinner while fetching data', async () => {
 test('displays order quote data when query is successful', async () => {
   mockIPC((cmd) => {
     if (cmd === 'batch_order_quotes') {
-      return [{ maxOutput: '0x158323e942e36d8c', ratio: '0x5b16799fcb6114f7' }];
+      return [
+        {
+          success: true,
+          pair_name: 'ETH/USDT',
+          data: { maxOutput: '0x158323e942e36d8c', ratio: '0x5b16799fcb6114f7' },
+          error: undefined,
+        },
+      ];
     }
   });
 
   const queryClient = new QueryClient();
 
   render(TanstackOrderQuote, {
-    props: { orderHash: '0x123' },
+    props: {
+      id: '0x123',
+      order: mockOrderDetailsExtended.order,
+    },
     context: new Map([['$$_queryClient', queryClient]]),
   });
 
   await waitFor(() => {
-    const orderQuoteComponent = screen.getByTestId('orderQuoteComponent');
+    const orderQuoteComponent = screen.getByTestId('bodyRow');
 
-    expect(orderQuoteComponent).toHaveTextContent('Maximum output');
+    expect(orderQuoteComponent).toHaveTextContent('ETH/USDT');
     expect(orderQuoteComponent).toHaveTextContent('1.550122181502135692');
-    expect(orderQuoteComponent).toHaveTextContent('Price');
     expect(orderQuoteComponent).toHaveTextContent('6.563567234157974775');
   });
 });
@@ -78,7 +102,10 @@ test('displays empty message when no data is returned', async () => {
   const queryClient = new QueryClient();
 
   render(TanstackOrderQuote, {
-    props: { orderHash: '0x123' },
+    props: {
+      id: '0x123',
+      order: mockOrderDetailsExtended.order,
+    },
     context: new Map([['$$_queryClient', queryClient]]),
   });
 
@@ -90,36 +117,108 @@ test('displays empty message when no data is returned', async () => {
 test('refreshes the quote when the refresh button is clicked', async () => {
   mockIPC((cmd) => {
     if (cmd === 'batch_order_quotes') {
-      return [{ maxOutput: '0x158323e942e36d8c', ratio: '0x5b16799fcb6114f7' }];
+      return [
+        {
+          success: true,
+          pair_name: 'ETH/USDT',
+          data: { maxOutput: '0x158323e942e36d8c', ratio: '0x5b16799fcb6114f7' },
+          error: undefined,
+        },
+        {
+          success: true,
+          pair_name: 'BTC/USDT',
+          data: { maxOutput: '0x54fa82f5c7001dad', ratio: '0x53e0089714d06709' },
+          error: undefined,
+        },
+      ];
     }
   });
 
   const queryClient = new QueryClient();
 
   render(TanstackOrderQuote, {
-    props: { orderHash: '0x123' },
+    props: {
+      id: '0x123',
+      order: mockOrderDetailsExtended.order,
+    },
     context: new Map([['$$_queryClient', queryClient]]),
   });
 
   await waitFor(() => {
-    const orderQuoteComponent = screen.getByTestId('orderQuoteComponent');
-    expect(orderQuoteComponent).toHaveTextContent('1.550122181502135692');
+    const orderQuoteRows = screen.getAllByTestId('bodyRow');
+
+    // Check ETH/USDT row
+    expect(orderQuoteRows[0]).toHaveTextContent('1.550122181502135692');
+
+    // Check BTC/USDT row
+    expect(orderQuoteRows[1]).toHaveTextContent('6.123350635480882605');
   });
 
   mockIPC((cmd) => {
     if (cmd === 'batch_order_quotes') {
-      return [{ maxOutput: '0x10ed6dd0a6e5d4cc', ratio: '0x5e68460c537594a0' }];
+      return [
+        {
+          success: true,
+          pair_name: 'ETH/USD',
+          data: { maxOutput: '0x5282713eceeccb5e', ratio: '0x577fe09a8775137c' },
+          error: undefined,
+        },
+        {
+          success: true,
+          pair_name: 'BTC/USDT',
+          data: { maxOutput: '0x5430775053da5e53', ratio: '0x5a01719c871bb83f' },
+          error: undefined,
+        },
+      ];
     }
   });
 
-  screen.getByText('Refresh Quote').click();
+  screen.getByText('Refresh Quotes').click();
 
   await waitFor(() => {
-    const orderQuoteComponent = screen.getByTestId('orderQuoteComponent');
+    const orderQuoteRows = screen.getAllByTestId('bodyRow');
 
-    expect(orderQuoteComponent).toHaveTextContent('Maximum output');
-    expect(orderQuoteComponent).toHaveTextContent('1.219751817007977676');
-    expect(orderQuoteComponent).toHaveTextContent('Price');
-    expect(orderQuoteComponent).toHaveTextContent('6.802764255896900768');
+    // Check ETH/USD row
+    expect(orderQuoteRows[0]).toHaveTextContent('ETH/USD');
+    expect(orderQuoteRows[0]).toHaveTextContent('5.945438972656012126');
+    expect(orderQuoteRows[0]).toHaveTextContent('6.305004957644166012');
+
+    // Check BTC/USDT row
+    expect(orderQuoteRows[1]).toHaveTextContent('BTC/USDT');
+    expect(orderQuoteRows[1]).toHaveTextContent('6.066479884955967059');
+    expect(orderQuoteRows[1]).toHaveTextContent('6.485589855485802559');
+  });
+});
+
+test('displays error message when query fails', async () => {
+  mockIPC((cmd) => {
+    if (cmd === 'batch_order_quotes') {
+      return [
+        {
+          success: false,
+          pair_name: 'ETH/USDT',
+          data: undefined,
+          error: 'Network error',
+        },
+      ];
+    }
+  });
+
+  const queryClient = new QueryClient();
+
+  render(TanstackOrderQuote, {
+    props: {
+      id: '0x123',
+      order: mockOrderDetailsExtended.order,
+    },
+    context: new Map([['$$_queryClient', queryClient]]),
+  });
+
+  await waitFor(() => {
+    const errorCell = screen.getByText(
+      (content, element) =>
+        content.includes('Error fetching pair quote:') && content.includes('Network error'),
+    );
+    expect(errorCell).toBeInTheDocument();
   });
 });
