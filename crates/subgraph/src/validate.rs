b5f51e8dac2e8834dbc23c6610c28a1cdd5f922e
rouzwelt <rouzweltt@gmail.com>
2024-08-27 03:19:26 +0000

    Update validate.rs

--- crates/subgraph/src/validate.rs
+++ crates/subgraph/src/validate.rs
@@ -7,71 +7,44 @@ use thiserror::Error;
 pub enum SchemaValidationError {
     #[error("undefined schema")]
     UndefinedSchema,
-    #[error("{0}")]
-    ErrorResponse(String),
+    #[error("undefined capabilities")]
+    UndefinedCapabilities,
     #[error(transparent)]
     ReadMainSchemaError(#[from] std::io::Error),
     #[error(transparent)]
     SchemaError(#[from] SchemaError),
     #[error(transparent)]
     ReqwestError(#[from] reqwest::Error),
-    #[error(transparent)]
-    SerdeJsonError(#[from] serde_json::Error),
 }
 
 /// Validates a subgraph's schema by fetching it from the given url and comparing
-/// against this repo schema
+/// it against this crate's schema
 pub async fn validate_subgraph_schema(subgraph_url: &str) -> Result<bool, SchemaValidationError> {
     let main_schema = read_to_string("./schema/orderbook.graphql")?;
 
     let client = reqwest::Client::new();
-
-    let capabilities_response = client
+    let capabilities = client
         .post(subgraph_url)
         .json(&CapabilitiesQuery::build(()))
         .send()
-        .await?;
-    let capabilities = if !capabilities_response.status().is_success() {
-        let body_string = capabilities_response
-            .text()
-            .await
-            .map_err(SchemaValidationError::ReqwestError)?;
+        .await?
+        .json::<GraphQlResponse<CapabilitiesQuery>>()
+        .await?
+        .data
+        .ok_or(SchemaValidationError::UndefinedCapabilities)?
+        .capabilities();
 
-        Ok::<cynic::GraphQlResponse<CapabilitiesQuery>, serde_json::Error>(serde_json::from_str::<
-            GraphQlResponse<CapabilitiesQuery>,
-        >(&body_string)?)
-    } else {
-        Ok(capabilities_response
-            .json::<GraphQlResponse<CapabilitiesQuery>>()
-            .await?)
-    }?
-    .data
-    .ok_or(SchemaValidationError::UndefinedSchema)?
-    .capabilities();
-
-    let schema_response = client
+    let schema = client
         .post(subgraph_url)
         .json(&IntrospectionQuery::with_capabilities(capabilities))
         .send()
-        .await?;
-    let schema = if !schema_response.status().is_success() {
-        let body_string = schema_response
-            .text()
-            .await
-            .map_err(SchemaValidationError::ReqwestError)?;
-
-        Ok::<cynic::GraphQlResponse<IntrospectionQuery>, serde_json::Error>(serde_json::from_str::<
-            GraphQlResponse<IntrospectionQuery>,
-        >(&body_string)?)
-    } else {
-        Ok(schema_response
-            .json::<GraphQlResponse<IntrospectionQuery>>()
-            .await?)
-    }?
-    .data
-    .ok_or(SchemaValidationError::UndefinedSchema)?
-    .into_schema()?
-    .to_sdl();
+        .await?
+        .json::<GraphQlResponse<IntrospectionQuery>>()
+        .await?
+        .data
+        .ok_or(SchemaValidationError::UndefinedSchema)?
+        .into_schema()?
+        .to_sdl();
 
     Ok(main_schema == schema)
 }
