a3dcb08e8a947097edd2d4fe471cd10a79022f47
rouzwelt <rouzweltt@gmail.com>
2024-08-27 18:04:14 +0000

    update

--- crates/subgraph/src/validate.rs
+++ crates/subgraph/src/validate.rs
@@ -17,11 +17,14 @@ pub enum SchemaValidationError {
     ReqwestError(#[from] reqwest::Error),
 }
 
-/// Validates a subgraph's schema by fetching it from the given url and comparing
-/// it against this crate's schema
-pub async fn validate_subgraph_schema(subgraph_url: &str) -> Result<bool, SchemaValidationError> {
+/// checks if a schema is equal to this crate's schema
+fn check_schema(schema: String) -> Result<bool, SchemaValidationError> {
     let main_schema = read_to_string("./schema/orderbook.graphql")?;
+    Ok(main_schema == schema)
+}
 
+/// Gets a subgraph schema given a url
+pub async fn get_schema(subgraph_url: &str) -> Result<String, SchemaValidationError> {
     let client = reqwest::Client::new();
     let capabilities = client
         .post(subgraph_url)
@@ -34,7 +37,7 @@ pub async fn validate_subgraph_schema(subgraph_url: &str) -> Result<bool, Schema
         .ok_or(SchemaValidationError::UndefinedCapabilities)?
         .capabilities();
 
-    let schema = client
+    Ok(client
         .post(subgraph_url)
         .json(&IntrospectionQuery::with_capabilities(capabilities))
         .send()
@@ -44,33 +47,131 @@ pub async fn validate_subgraph_schema(subgraph_url: &str) -> Result<bool, Schema
         .data
         .ok_or(SchemaValidationError::UndefinedSchema)?
         .into_schema()?
-        .to_sdl();
+        .to_sdl())
+}
 
-    Ok(main_schema == schema)
+/// Validates a subgraph's schema by fetching it from the
+/// given url and comparing it against this crate's schema
+pub async fn validate_subgraph_schema(subgraph_url: &str) -> Result<bool, SchemaValidationError> {
+    check_schema(get_schema(subgraph_url).await?)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use httpmock::MockServer;
+    use serde_json::json;
 
     #[tokio::test]
-    async fn test_validate_happy_true() {
-        let subgraph_url = "https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-test/0.7/gn";
-        let result = validate_subgraph_schema(subgraph_url).await.unwrap();
-        assert!(result)
+    async fn test_get_schema_happy() {
+        let sg_server = MockServer::start();
+        sg_server.mock(|_when, then| {
+            then.json_body_obj(&json!({
+                "data": {
+                    "__type": null,
+                    "__schema": {
+                        "queryType": { "name": "Query" },
+                        "mutationType": null,
+                        "subscriptionType": { "name": "Subscription" },
+                        "types": [{
+                            "kind": "ENUM",
+                            "name": "Aggregation_interval",
+                            "description": null,
+                            "fields": null,
+                            "inputFields": null,
+                            "interfaces": null,
+                            "enumValues": [
+                              {
+                                "name": "hour",
+                                "description": null,
+                                "isDeprecated": false,
+                                "deprecationReason": null
+                              },
+                              {
+                                "name": "day",
+                                "description": null,
+                                "isDeprecated": false,
+                                "deprecationReason": null
+                              }
+                            ],
+                            "possibleTypes": null
+                          }],
+                        "directives": []
+                    }
+                }
+            }));
+        });
+        let result = get_schema(&sg_server.url("/sg")).await.unwrap();
+        let expected = "enum Aggregation_interval {\n  hour\n  day\n}\n\n";
+        assert_eq!(result, expected);
     }
 
     #[tokio::test]
-    async fn test_validate_happy_false() {
-        let subgraph_url = "https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-base/0.4/gn";
-        let result = validate_subgraph_schema(subgraph_url).await.unwrap();
-        assert!(!result)
+    async fn test_get_schema_unhappy() {
+        let sg_server = MockServer::start();
+        sg_server.mock(|_when, then| {
+            then.json_body_obj(&json!({
+                "data": {
+                    "__type": null,
+                    "__schema": {
+                        "mutationType": null,
+                        "types": [],
+                        "directives": []
+                    }
+                }
+            }));
+        });
+        assert!(get_schema(&sg_server.url("/sg")).await.is_err());
+    }
+
+    #[test]
+    fn test_check_schema_happy() {
+        let schema = read_to_string("./schema/orderbook.graphql").unwrap();
+        let result = check_schema(schema).unwrap();
+        assert!(result);
     }
 
     #[tokio::test]
-    async fn test_validate_unhappy() {
-        let subgraph_url = "https://api.goldsky.com/api/public";
-        let result = validate_subgraph_schema(subgraph_url).await.is_err();
-        assert!(result)
+    async fn test_check_schema_unhappy() {
+        let sg_server = MockServer::start();
+        sg_server.mock(|_when, then| {
+            then.json_body_obj(&json!({
+                "data": {
+                    "__type": null,
+                    "__schema": {
+                        "queryType": { "name": "Query" },
+                        "mutationType": null,
+                        "subscriptionType": { "name": "Subscription" },
+                        "types": [{
+                            "kind": "ENUM",
+                            "name": "Aggregation_interval",
+                            "description": null,
+                            "fields": null,
+                            "inputFields": null,
+                            "interfaces": null,
+                            "enumValues": [
+                              {
+                                "name": "hour",
+                                "description": null,
+                                "isDeprecated": false,
+                                "deprecationReason": null
+                              },
+                              {
+                                "name": "day",
+                                "description": null,
+                                "isDeprecated": false,
+                                "deprecationReason": null
+                              }
+                            ],
+                            "possibleTypes": null
+                          }],
+                        "directives": []
+                    }
+                }
+            }));
+        });
+        let schema = get_schema(&sg_server.url("/sg")).await.unwrap();
+        let result = check_schema(schema).unwrap();
+        assert!(!result);
     }
 }
