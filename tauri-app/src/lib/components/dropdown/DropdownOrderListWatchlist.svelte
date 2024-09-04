593c00b18188c55c70edd2f47b3db8c3d620ad5f
findolor <anakisci@gmail.com>
2024-09-04 10:28:34 +0300

    feat: create a new component to display the watchlist items

---
+++ tauri-app/src/lib/components/dropdown/DropdownOrderListWatchlist.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { watchlist, activeWatchlistItems } from '$lib/stores/settings';
+  import DropdownCheckbox from './DropdownCheckbox.svelte';
+
+  $: options = $watchlist;
+  $: value = $activeWatchlistItems;
+
+  function handleChange(event: CustomEvent<string[]>) {
+    $activeWatchlistItems = event.detail;
+  }
+</script>
+
+<DropdownCheckbox
+  bind:options
+  bind:value
+  on:change={handleChange}
+  label="Watchlist"
+  allLabel="All addresses"
+  emptyMessage="No watchlist added"
+>
+  <svelte:fragment slot="item" let:item>
+    {item}
+  </svelte:fragment>
+</DropdownCheckbox>
