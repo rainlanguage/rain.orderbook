a2d355e9db5f6f381489c7338a1c6ca292bf4ffd
rouzwelt <rouzweltt@gmail.com>
2024-09-04 22:11:59 +0000

    fix

--- tauri-app/src/lib/components/ButtonVaultLink.svelte
+++ tauri-app/src/lib/components/ButtonVaultLink.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-  import type { Vault } from '$lib/typeshare/orderDetail';
+  import type { Vault } from '$lib/typeshare/subgraphTypes';
   import { bigintStringToHex } from '$lib/utils/hex';
 
   export let tokenVault: Vault;
@@ -9,7 +9,7 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-  class="cursor-pointer hover:text-primary-600"
+  class="hover:text-primary-600 cursor-pointer"
   on:click={() => goto(`/vaults/${tokenVault.id}`)}
 >
   <div class="grid grid-cols-2 gap-x-2 break-normal">
