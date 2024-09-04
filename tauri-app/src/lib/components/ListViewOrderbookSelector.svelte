b9393d461c89bb4832c07cc464536f1ed8502f86
findolor <anakisci@gmail.com>
2024-09-04 10:32:41 +0300

    feat: add the watchlist filter to selector component

--- tauri-app/src/lib/components/ListViewOrderbookSelector.svelte
+++ tauri-app/src/lib/components/ListViewOrderbookSelector.svelte
@@ -2,7 +2,7 @@
   import { isEmpty } from 'lodash';
   import DropdownActiveNetwork from './DropdownActiveNetwork.svelte';
   import DropdownActiveOrderbook from './DropdownActiveOrderbook.svelte';
-  import OrderListOwnerFilter from './dropdown/OrderListOwnerFilter.svelte';
+  import DropdownOrderListWatchlist from './dropdown/DropdownOrderListWatchlist.svelte';
   import { settings } from '$lib/stores/settings';
   import { Alert } from 'flowbite-svelte';
 </script>
@@ -13,7 +13,7 @@
       >No networks added to <a class="underline" href="/settings">settings</a></Alert
     >
   {:else}
-    <OrderListOwnerFilter />
+    <DropdownOrderListWatchlist />
     <DropdownActiveNetwork />
     <DropdownActiveOrderbook />
   {/if}
