4453759b8a0d00ee8efac3b20160071469f60509
findolor <anakisci@gmail.com>
2024-09-25 11:31:41 +0300

    feat: update scenario debug component to display the new deployment debug

--- tauri-app/src/lib/components/ScenarioDebugTable.svelte
+++ tauri-app/src/lib/components/ScenarioDebugTable.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import type { ChartData } from '$lib/typeshare/config';
   import { transformData } from '$lib/utils/chartData';
+  import { formatUnits, hexToNumber, isHex } from 'viem';
   import {
     Table,
     TableBody,
@@ -9,35 +9,118 @@
     TableHead,
     TableHeadCell,
   } from 'flowbite-svelte';
+  import { BugOutline, PauseSolid, PlaySolid } from 'flowbite-svelte-icons';
+  import { handleScenarioDebugModal } from '$lib/services/modal';
+  import Refresh from './icon/Refresh.svelte';
+  import EditableSpan from './EditableSpan.svelte';
+  import { useDebouncedFn } from '$lib/utils/asyncDebounce';
+  import { makeDeploymentDebugData } from '$lib/services/chart';
+  import { globalDotrainFile } from '$lib/storesGeneric/textFileStore';
+  import { settingsText } from '$lib/stores/settings';
 
-  export let chartData: ChartData;
+  let enabled = true;
+  let loading = false;
+  let blockNumber: number | undefined;
+
+  const fetchData = async () => {
+    loading = true;
+    const res = await makeDeploymentDebugData(
+      $globalDotrainFile.text,
+      $settingsText,
+      enabled ? undefined : blockNumber,
+    );
+
+    blockNumber = parseInt(res.block_number);
+
+    loading = false;
+    return res;
+  };
+  const { debouncedFn: debounceMakeDeploymentDebugData, result: data } = useDebouncedFn(
+    fetchData,
+    500,
+  );
+  $: debounceMakeDeploymentDebugData();
 </script>
 
-{#if chartData}
-  <div class="flex w-full gap-x-4 overflow-x-scroll">
-    {#each Object.values(chartData.scenarios_data) as scenario}
-      {@const data = transformData(scenario)}
-      <div class="flex flex-col gap-y-2">
-        <span>{scenario.scenario}</span>
-        <Table divClass="cursor-pointer rounded-lg overflow-hidden dark:border-none border">
-          <TableHead>
-            <TableHeadCell>Stack item</TableHeadCell>
-            <TableHeadCell>Value</TableHeadCell>
-            <TableHeadCell>Hex</TableHeadCell>
-          </TableHead>
-          <TableBody>
-            {#each Object.entries(data[0]) as [key, value]}
-              <TableBodyRow>
-                <TableBodyCell>{key}</TableBodyCell>
-                <TableBodyCell>{value[0]}</TableBodyCell>
-                <TableBodyCell>{value[1]}</TableBodyCell>
-              </TableBodyRow>
-            {/each}
-          </TableBody>
-        </Table>
-      </div>
-    {/each}
+<div class="mt-4">
+  <div class="flex items-center justify-end">
+    <div class="flex items-center gap-x-1">
+      {#if $data && isHex($data.block_number)}
+        <EditableSpan
+          displayValue={blockNumber?.toString() || hexToNumber($data.block_number).toString()}
+          on:focus={() => {
+            enabled = false;
+          }}
+          on:blur={({ detail: { value } }) => {
+            blockNumber = parseInt(value);
+            fetchData();
+          }}
+        />
+      {/if}
+      <span></span>
+      {#if data}
+        <Refresh
+          data-testid="refreshButton"
+          class="h-8 w-5 cursor-pointer text-gray-400 dark:text-gray-400"
+          on:click={fetchData}
+          spin={loading}
+        />
+        <PauseSolid
+          class={`ml-2 h-8 w-3 cursor-pointer text-gray-400 dark:text-gray-400 ${!enabled ? 'hidden' : ''}`}
+          on:click={() => {
+            enabled = false;
+          }}
+        />
+        <PlaySolid
+          on:click={() => {
+            enabled = true;
+            blockNumber = undefined;
+            fetchData();
+          }}
+          class={`ml-2 h-8 w-3 cursor-pointer text-gray-400 dark:text-gray-400 ${enabled ? 'hidden' : ''}`}
+        />
+      {/if}
+    </div>
   </div>
-{:else}
-  No scenario data
-{/if}
+
+  {#each Object.entries($data?.result ?? {}) as [deploymentName, results]}
+    <h2 class="my-4 text-lg">Deployment: <strong>{deploymentName}</strong></h2>
+    <Table divClass="rounded-lg overflow-hidden dark:border-none border">
+      <TableHead>
+        <TableHeadCell>Order</TableHeadCell>
+        <TableHeadCell>Scenario</TableHeadCell>
+        <TableHeadCell>Pair</TableHeadCell>
+        <TableHeadCell>Maximum Output</TableHeadCell>
+        <TableHeadCell>Ratio</TableHeadCell>
+        <TableHeadCell class="w-[50px]" />
+      </TableHead>
+
+      <TableBody>
+        {#each results as item}
+          {@const data = transformData(item.fuzz_result)[0]}
+          {@const ioRatio = Object.entries(data)[Object.entries(data).length - 1]}
+          {@const maxOutput = Object.entries(data)[Object.entries(data).length - 2]}
+          <TableBodyRow>
+            <TableBodyCell>{item.order_name}</TableBodyCell>
+            <TableBodyCell>{item.fuzz_result.scenario}</TableBodyCell>
+            <TableBodyCell>{item.pair}</TableBodyCell>
+            <TableBodyCell>
+              {maxOutput[1][0]}
+            </TableBodyCell>
+            <TableBodyCell>
+              {ioRatio[1][0]}
+              <span class="text-gray-400">
+                ({formatUnits(10n ** 36n / BigInt(ioRatio[1][1]), 18)})
+              </span>
+            </TableBodyCell>
+            <TableBodyCell>
+              <button on:click={() => handleScenarioDebugModal(item.pair, item.fuzz_result.data)}>
+                <BugOutline size="sm" color="grey" />
+              </button>
+            </TableBodyCell>
+          </TableBodyRow>
+        {/each}
+      </TableBody>
+    </Table>
+  {/each}
+</div>
