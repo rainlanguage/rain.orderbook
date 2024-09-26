275a8c0d12ff043a866fcacbf4bf54189bbdb5aa
findolor <anakisci@gmail.com>
2024-09-26 19:13:11 +0300

    refactor: add user alerts for parsing status

--- tauri-app/src/routes/settings/+page.svelte
+++ tauri-app/src/routes/settings/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Alert } from 'flowbite-svelte';
+  import { Alert, Spinner } from 'flowbite-svelte';
   import { hasRequiredSettings, settingsText, settings, settingsFile } from '$lib/stores/settings';
   import PageHeader from '$lib/components/PageHeader.svelte';
   import CodeMirrorConfigSource from '$lib/components/CodeMirrorConfigSource.svelte';
@@ -7,48 +7,93 @@
   import { useDebouncedFn } from '$lib/utils/asyncDebounce';
   import { parseConfigSource } from '$lib/services/config';
   import { reportErrorToSentry, SentrySeverityLevel } from '$lib/services/sentry';
-  import { toasts } from '$lib/stores/toasts';
-  import { ToastMessageType } from '$lib/typeshare/toast';
+  import { CheckOutline, CloseOutline } from 'flowbite-svelte-icons';
+  import { onMount } from 'svelte';
+
+  let settingsStatus: 'idle' | 'checking' | 'success' | 'error' = 'idle';
+  let errorMessage: string | undefined = undefined;
+  let height = 500;
+
+  function updateHeight() {
+    height = window.innerHeight - 370;
+  }
+
+  onMount(() => {
+    updateHeight();
+    window.addEventListener('resize', updateHeight);
+    return () => window.removeEventListener('resize', updateHeight);
+  });
 
   async function apply(settingsContent: string): Promise<void> {
+    settingsStatus = 'checking';
     try {
       settingsText.set(settingsContent);
       settings.set(await parseConfigSource(settingsContent));
+      settingsStatus = 'success';
     } catch (error) {
+      errorMessage = error as string;
       reportErrorToSentry(error, SentrySeverityLevel.Info);
-      toasts.error('Failed to apply settings', { message_type: ToastMessageType.Warning });
+      settingsStatus = 'error';
     }
   }
 
   const { debouncedFn: debouncedApply } = useDebouncedFn(apply, 1000);
-  $: debouncedApply($settingsFile.text);
+  $: {
+    debouncedApply($settingsFile.text);
+    errorMessage = undefined;
+    settingsStatus = 'checking';
+  }
 </script>
 
-<PageHeader title="Settings" />
+<div class="mb-4">
+  <PageHeader title="Settings" />
 
-<Alert color="blue" class="my-8 text-lg">
-  Looking for some settings to get started? Check out the <a
-    class="underline"
-    target="_blank"
-    href="https://docs.rainlang.xyz/raindex/getting-started">getting started guide</a
-  ></Alert
->
+  <Alert color="blue" class="mb-4 mt-8 text-lg">
+    Looking for some settings to get started? Check out the <a
+      class="underline"
+      target="_blank"
+      href="https://docs.rainlang.xyz/raindex/getting-started">getting started guide</a
+    >
+  </Alert>
 
-{#await hasRequiredSettings}
-  <!-- -->
-{:then val}
-  {#if !val}
-    <Alert color="red" class="my-8 text-lg">
-      Please fill in all the settings to use the Orderbook.
+  <!-- <div class="flex w-full text-lg"> -->
+  {#if settingsStatus === 'checking'}
+    <Alert color="blue" class="flex items-center text-blue-600 dark:text-blue-400">
+      <Spinner class="mr-2" size="4" />
+      <span>Checking settings...</span>
+    </Alert>
+  {:else if settingsStatus === 'success'}
+    <Alert color="green" class="flex items-center text-green-600 dark:text-green-400">
+      <CheckOutline class="mr-2" size="xs" />
+      <span>Settings applied successfully</span>
+    </Alert>
+  {:else if settingsStatus === 'error'}
+    <Alert color="red" class="flex flex-col text-red-600 dark:text-red-400">
+      <div class="mb-2 flex items-center">
+        <CloseOutline class="mr-2" size="xs" />
+        <span>Error applying settings</span>
+      </div>
+      <span>{errorMessage}</span>
     </Alert>
   {/if}
-{/await}
+  <!-- </div> -->
+
+  {#await hasRequiredSettings}
+    <!-- -->
+  {:then val}
+    {#if !val}
+      <Alert color="red" class="my-8 text-lg">
+        Please fill in all the settings to use the Orderbook.
+      </Alert>
+    {/if}
+  {/await}
+</div>
 
 <FileTextarea textFile={settingsFile} title="Settings">
   <svelte:fragment slot="textarea">
     <CodeMirrorConfigSource
       bind:value={$settingsFile.text}
-      styles={{ '&': { minHeight: '400px' } }}
+      styles={{ '&': { maxHeight: `${height - (errorMessage ? 35 : 0)}px`, height: '100%' } }}
     />
   </svelte:fragment>
 </FileTextarea>
