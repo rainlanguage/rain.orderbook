cbef2362b491cfaf687725fe947266b922851a7a
findolor <anakisci@gmail.com>
2024-09-27 14:49:57 +0300

    refactor: display function errors as super error

--- tauri-app/src/lib/components/ScenarioDebugTable.svelte
+++ tauri-app/src/lib/components/ScenarioDebugTable.svelte
@@ -21,15 +21,26 @@
   let enabled = true;
   let loading = false;
   let blockNumber: number | undefined;
+  let debugError: string | undefined;
 
   const fetchData = async (dotrain: string, settings: string, blockNumber?: number) => {
-    loading = true;
-    const res = await makeDeploymentDebugData(dotrain, settings, enabled ? undefined : blockNumber);
+    try {
+      debugError = undefined;
+      loading = true;
 
-    blockNumber = parseInt(res.block_number);
+      const res = await makeDeploymentDebugData(
+        dotrain,
+        settings,
+        enabled ? undefined : blockNumber,
+      );
 
-    loading = false;
-    return res;
+      blockNumber = parseInt(res.block_number);
+
+      loading = false;
+      return res;
+    } catch (error) {
+      debugError = error as string;
+    }
   };
   const { debouncedFn: debounceMakeDeploymentDebugData, result: data } = useDebouncedFn(
     fetchData,
@@ -42,94 +53,95 @@
   };
 </script>
 
-<div class="">
-  <div class="flex items-center justify-end">
-    <div class="flex items-center gap-x-1">
-      {#if $data && isHex($data.block_number)}
-        <EditableSpan
-          displayValue={blockNumber?.toString() || hexToNumber($data.block_number).toString()}
-          on:focus={() => {
-            enabled = false;
-          }}
-          on:blur={({ detail: { value } }) => {
-            blockNumber = parseInt(value);
-            handleRefresh();
-          }}
-        />
-      {/if}
-      <span></span>
-      {#if data}
-        <Refresh
-          data-testid="refreshButton"
-          class="h-8 w-5 cursor-pointer text-gray-400 dark:text-gray-400"
-          on:click={handleRefresh}
-          spin={loading}
-        />
-        <PauseSolid
-          class={`ml-2 h-8 w-3 cursor-pointer text-gray-400 dark:text-gray-400 ${!enabled ? 'hidden' : ''}`}
-          on:click={() => {
-            enabled = false;
-          }}
-        />
-        <PlaySolid
-          on:click={() => {
-            enabled = true;
-            blockNumber = undefined;
-            handleRefresh();
-          }}
-          class={`ml-2 h-8 w-3 cursor-pointer text-gray-400 dark:text-gray-400 ${enabled ? 'hidden' : ''}`}
-        />
-      {/if}
-    </div>
+<div class="flex items-center justify-end">
+  <div class="flex items-center gap-x-1">
+    {#if debugError}
+      <div class="text-red-500">{debugError}</div>
+    {/if}
+    {#if $data && isHex($data.block_number)}
+      <EditableSpan
+        displayValue={blockNumber?.toString() || hexToNumber($data.block_number).toString()}
+        on:focus={() => {
+          enabled = false;
+        }}
+        on:blur={({ detail: { value } }) => {
+          blockNumber = parseInt(value);
+          handleRefresh();
+        }}
+      />
+    {/if}
+    <span></span>
+    {#if data}
+      <Refresh
+        data-testid="refreshButton"
+        class="h-8 w-5 cursor-pointer text-gray-400 dark:text-gray-400"
+        on:click={handleRefresh}
+        spin={loading}
+      />
+      <PauseSolid
+        class={`ml-2 h-8 w-3 cursor-pointer text-gray-400 dark:text-gray-400 ${!enabled ? 'hidden' : ''}`}
+        on:click={() => {
+          enabled = false;
+        }}
+      />
+      <PlaySolid
+        on:click={() => {
+          enabled = true;
+          blockNumber = undefined;
+          handleRefresh();
+        }}
+        class={`ml-2 h-8 w-3 cursor-pointer text-gray-400 dark:text-gray-400 ${enabled ? 'hidden' : ''}`}
+      />
+    {/if}
   </div>
-
-  {#each Object.entries($data?.result ?? {}) as [deploymentName, results]}
-    <h2 class="my-4 text-lg">Deployment: <strong>{deploymentName}</strong></h2>
-    <Table divClass="rounded-lg overflow-hidden dark:border-none border">
-      <TableHead>
-        <TableHeadCell>Order</TableHeadCell>
-        <TableHeadCell>Scenario</TableHeadCell>
-        <TableHeadCell>Pair</TableHeadCell>
-        <TableHeadCell>Maximum Output</TableHeadCell>
-        <TableHeadCell>Ratio</TableHeadCell>
-        <TableHeadCell class="w-[50px]" />
-      </TableHead>
-
-      <TableBody>
-        {#each results as item}
-          {@const data = transformData(item.fuzz_result)[0]}
-          {@const dataEntries = Object.entries(data)}
-          <TableBodyRow>
-            <TableBodyCell>{item.order_name}</TableBodyCell>
-            <TableBodyCell>{item.fuzz_result.scenario}</TableBodyCell>
-            <TableBodyCell>{item.pair}</TableBodyCell>
-            {#if dataEntries.length === 1}
-              <TableBodyCell colspan="2" class="text-red-500"
-                >Missing stack data for max output and ratio</TableBodyCell
-              >
-            {:else}
-              {@const maxOutput = dataEntries[dataEntries.length - 2]}
-              {@const ioRatio = dataEntries[dataEntries.length - 1]}
-              <TableBodyCell>
-                {maxOutput[1][0]}
-              </TableBodyCell>
-              <TableBodyCell>
-                {ioRatio[1][0]}
-                <span class="text-gray-400">
-                  ({BigInt(ioRatio[1][1]) === 0n
-                    ? '0'
-                    : formatUnits(10n ** 36n / BigInt(ioRatio[1][1]), 18)})
-                </span>
-              </TableBodyCell>
-            {/if}
-            <TableBodyCell>
-              <button on:click={() => handleScenarioDebugModal(item.pair, item.fuzz_result.data)}>
-                <BugOutline size="sm" color="grey" />
-              </button>
-            </TableBodyCell>
-          </TableBodyRow>
-        {/each}
-      </TableBody>
-    </Table>
-  {/each}
 </div>
+
+{#each Object.entries($data?.result ?? {}) as [deploymentName, results]}
+  <h2 class="my-4 text-lg">Deployment: <strong>{deploymentName}</strong></h2>
+  <Table divClass="rounded-lg overflow-hidden dark:border-none border">
+    <TableHead>
+      <TableHeadCell>Order</TableHeadCell>
+      <TableHeadCell>Scenario</TableHeadCell>
+      <TableHeadCell>Pair</TableHeadCell>
+      <TableHeadCell>Maximum Output</TableHeadCell>
+      <TableHeadCell>Ratio</TableHeadCell>
+      <TableHeadCell class="w-[50px]" />
+    </TableHead>
+
+    <TableBody>
+      {#each results as item}
+        {@const data = transformData(item.fuzz_result)[0]}
+        {@const dataEntries = Object.entries(data)}
+        <TableBodyRow>
+          <TableBodyCell>{item.order_name}</TableBodyCell>
+          <TableBodyCell>{item.fuzz_result.scenario}</TableBodyCell>
+          <TableBodyCell>{item.pair}</TableBodyCell>
+          {#if dataEntries.length === 1}
+            <TableBodyCell colspan="2" class="text-red-500"
+              >Missing stack data for max output and ratio</TableBodyCell
+            >
+          {:else}
+            {@const maxOutput = dataEntries[dataEntries.length - 2]}
+            {@const ioRatio = dataEntries[dataEntries.length - 1]}
+            <TableBodyCell>
+              {maxOutput[1][0]}
+            </TableBodyCell>
+            <TableBodyCell>
+              {ioRatio[1][0]}
+              <span class="text-gray-400">
+                ({BigInt(ioRatio[1][1]) === 0n
+                  ? '0'
+                  : formatUnits(10n ** 36n / BigInt(ioRatio[1][1]), 18)})
+              </span>
+            </TableBodyCell>
+          {/if}
+          <TableBodyCell>
+            <button on:click={() => handleScenarioDebugModal(item.pair, item.fuzz_result.data)}>
+              <BugOutline size="sm" color="grey" />
+            </button>
+          </TableBodyCell>
+        </TableBodyRow>
+      {/each}
+    </TableBody>
+  </Table>
+{/each}
