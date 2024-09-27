fd564b457fa40b4fb21c002d1853a5521860f0d2
findolor <anakisci@gmail.com>
2024-09-27 13:31:26 +0300

    refactor: move alerts into the file textarea

--- tauri-app/src/routes/settings/+page.svelte
+++ tauri-app/src/routes/settings/+page.svelte
@@ -7,15 +7,15 @@
   import { useDebouncedFn } from '$lib/utils/asyncDebounce';
   import { parseConfigSource } from '$lib/services/config';
   import { reportErrorToSentry, SentrySeverityLevel } from '$lib/services/sentry';
-  import { CheckOutline, CloseOutline } from 'flowbite-svelte-icons';
   import { onMount } from 'svelte';
+  import { CheckOutline, CloseOutline } from 'flowbite-svelte-icons';
 
   let settingsStatus: 'idle' | 'checking' | 'success' | 'error' = 'idle';
   let errorMessage: string | undefined = undefined;
   let height = 500;
 
   function updateHeight() {
-    height = window.innerHeight - 370;
+    height = window.innerHeight - 320;
   }
 
   onMount(() => {
@@ -56,28 +56,6 @@
     >
   </Alert>
 
-  <!-- <div class="flex w-full text-lg"> -->
-  {#if settingsStatus === 'checking'}
-    <Alert color="blue" class="flex items-center text-blue-600 dark:text-blue-400">
-      <Spinner class="mr-2" size="4" />
-      <span>Checking settings...</span>
-    </Alert>
-  {:else if settingsStatus === 'success'}
-    <Alert color="green" class="flex items-center text-green-600 dark:text-green-400">
-      <CheckOutline class="mr-2" size="xs" />
-      <span>Settings applied successfully</span>
-    </Alert>
-  {:else if settingsStatus === 'error'}
-    <Alert color="red" class="flex flex-col text-red-600 dark:text-red-400">
-      <div class="mb-2 flex items-center">
-        <CloseOutline class="mr-2" size="xs" />
-        <span>Error applying settings</span>
-      </div>
-      <span>{errorMessage}</span>
-    </Alert>
-  {/if}
-  <!-- </div> -->
-
   {#await hasRequiredSettings}
     <!-- -->
   {:then val}
@@ -89,7 +67,29 @@
   {/await}
 </div>
 
-<FileTextarea textFile={settingsFile} title="Settings">
+<FileTextarea textFile={settingsFile}>
+  <svelte:fragment slot="alert">
+    {#if settingsStatus === 'checking'}
+      <Alert color="blue" class="flex h-10 items-center text-blue-600 dark:text-blue-400">
+        <Spinner class="mr-2" size="4" />
+        <span>Checking settings...</span>
+      </Alert>
+    {:else if settingsStatus === 'success'}
+      <Alert color="green" class="flex h-10 items-center text-green-600 dark:text-green-400">
+        <CheckOutline class="mr-2" size="xs" />
+        <span>Settings applied successfully</span>
+      </Alert>
+    {:else if settingsStatus === 'error'}
+      <Alert color="red" class="flex flex-col text-red-600 dark:text-red-400">
+        <div class="mb-2 flex items-center">
+          <CloseOutline class="mr-2" size="xs" />
+          <span>Error applying settings</span>
+        </div>
+        <span>{errorMessage}</span>
+      </Alert>
+    {/if}
+  </svelte:fragment>
+
   <svelte:fragment slot="textarea">
     <CodeMirrorConfigSource
       bind:value={$settingsFile.text}
