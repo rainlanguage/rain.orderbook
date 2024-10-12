64dee88e566072dee30b7c0cbb6552ab911d95ba
findolor <anakisci@gmail.com>
2024-10-12 21:35:10 +0300

    test: update tests

--- subgraph/src/clear.ts
+++ subgraph/src/clear.ts
@@ -1,163 +1,162 @@
-import { AfterClear, ClearV2 } from "../generated/OrderBook/OrderBook";
-import { ClearTemporaryData } from "../generated/schema";
-import { createTradeEntity } from "./trade";
-import { createTradeVaultBalanceChangeEntity } from "./tradevaultbalancechange";
-import { handleVaultBalanceChange, vaultEntityId } from "./vault";
-import { log } from "@graphprotocol/graph-ts";
-import {
-  BigInt,
-  Bytes,
-  crypto,
-  ethereum,
-  store,
-} from "@graphprotocol/graph-ts";
+import { AfterClear, ClearV2 } from "../generated/OrderBook/OrderBook"
+import { ClearTemporaryData } from "../generated/schema"
+import { createOrderbookEntity } from "./orderbook"
+import { createTradeEntity } from "./trade"
+import { createTradeVaultBalanceChangeEntity } from "./tradevaultbalancechange"
+import { handleVaultBalanceChange, vaultEntityId } from "./vault"
+import { log } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes, crypto, ethereum, store } from "@graphprotocol/graph-ts"
 
-export function orderHashFromClearEvent(
-  event: ClearV2,
-  isAlice: boolean
-): Bytes {
-  let orderHash = crypto.keccak256(
-    ethereum.encode(event.parameters[isAlice ? 1 : 2].value.toTuple()[0])!
-  );
-  return Bytes.fromByteArray(orderHash);
+export function orderHashFromClearEvent(order: ethereum.Value): Bytes {
+  let orderHash = crypto.keccak256(ethereum.encode(order)!)
+  return Bytes.fromByteArray(orderHash)
 }
 
 export function clearTemporaryDataEntityId(event: ethereum.Event): Bytes {
   return Bytes.fromByteArray(
     crypto.keccak256(event.transaction.hash.concat(event.address))
-  );
+  )
 }
 
 export function handleClear(event: ClearV2): void {
+  createOrderbookEntity(event)
+
   let clearTemporaryData = new ClearTemporaryData(
     clearTemporaryDataEntityId(event)
-  );
+  )
 
-  let aliceOrderHash = orderHashFromClearEvent(event, true);
-  let bobOrderHash = orderHashFromClearEvent(event, false);
+  let aliceOrderHash = orderHashFromClearEvent(
+    event.parameters[1].value.toTuple()[0]
+  )
+  let bobOrderHash = orderHashFromClearEvent(
+    event.parameters[2].value.toTuple()[0]
+  )
 
   let aliceInput =
     event.params.alice.validInputs[
       event.params.clearConfig.aliceInputIOIndex.toU32()
-    ];
+    ]
   let aliceOutput =
     event.params.alice.validOutputs[
       event.params.clearConfig.aliceOutputIOIndex.toU32()
-    ];
+    ]
   let bobInput =
     event.params.bob.validInputs[
       event.params.clearConfig.bobInputIOIndex.toU32()
-    ];
+    ]
   let bobOutput =
     event.params.bob.validOutputs[
       event.params.clearConfig.bobOutputIOIndex.toU32()
-    ];
+    ]
 
-  clearTemporaryData.aliceOrderHash = aliceOrderHash;
-  clearTemporaryData.bobOrderHash = bobOrderHash;
+  clearTemporaryData.aliceOrderHash = aliceOrderHash
+  clearTemporaryData.bobOrderHash = bobOrderHash
 
-  clearTemporaryData.aliceAddress = event.params.alice.owner;
-  clearTemporaryData.bobAddress = event.params.bob.owner;
+  clearTemporaryData.aliceAddress = event.params.alice.owner
+  clearTemporaryData.bobAddress = event.params.bob.owner
 
-  clearTemporaryData.aliceInputVaultId = aliceInput.vaultId;
-  clearTemporaryData.aliceOutputVaultId = aliceOutput.vaultId;
-  clearTemporaryData.bobInputVaultId = bobInput.vaultId;
-  clearTemporaryData.bobOutputVaultId = bobOutput.vaultId;
+  clearTemporaryData.aliceInputVaultId = aliceInput.vaultId
+  clearTemporaryData.aliceOutputVaultId = aliceOutput.vaultId
+  clearTemporaryData.bobInputVaultId = bobInput.vaultId
+  clearTemporaryData.bobOutputVaultId = bobOutput.vaultId
 
-  clearTemporaryData.aliceInputToken = aliceInput.token;
-  clearTemporaryData.aliceOutputToken = aliceOutput.token;
-  clearTemporaryData.bobInputToken = bobInput.token;
-  clearTemporaryData.bobOutputToken = bobOutput.token;
+  clearTemporaryData.aliceInputToken = aliceInput.token
+  clearTemporaryData.aliceOutputToken = aliceOutput.token
+  clearTemporaryData.bobInputToken = bobInput.token
+  clearTemporaryData.bobOutputToken = bobOutput.token
 
-  clearTemporaryData.aliceBounty = event.params.clearConfig.aliceBountyVaultId;
-  clearTemporaryData.bobBounty = event.params.clearConfig.bobBountyVaultId;
+  clearTemporaryData.aliceBounty = event.params.clearConfig.aliceBountyVaultId
+  clearTemporaryData.bobBounty = event.params.clearConfig.bobBountyVaultId
 
-  clearTemporaryData.save();
+  clearTemporaryData.save()
 }
 
 function createTrade(
-  event: AfterClear,
-  clearData: ClearTemporaryData,
-  isAlice: boolean
+  event: ethereum.Event,
+  orderHash: Bytes,
+  owner: Bytes,
+  inputVaultId: BigInt,
+  outputVaultId: BigInt,
+  inputToken: Bytes,
+  outputToken: Bytes,
+  inputAmount: BigInt,
+  outputAmount: BigInt
 ): void {
-  let owner = isAlice ? clearData.aliceAddress : clearData.bobAddress;
-
-  let orderHash = isAlice ? clearData.aliceOrderHash : clearData.bobOrderHash;
-
-  let input = isAlice
-    ? event.params.clearStateChange.aliceInput
-    : event.params.clearStateChange.bobInput;
-  let output = isAlice
-    ? event.params.clearStateChange.aliceOutput
-    : event.params.clearStateChange.bobOutput;
-
-  let inputVaultId = isAlice
-    ? clearData.aliceInputVaultId
-    : clearData.bobInputVaultId;
-  let inputToken = isAlice
-    ? clearData.aliceInputToken
-    : clearData.bobInputToken;
-
-  let outputVaultId = isAlice
-    ? clearData.aliceOutputVaultId
-    : clearData.bobOutputVaultId;
-  let outputToken = isAlice
-    ? clearData.aliceOutputToken
-    : clearData.bobOutputToken;
-
   let oldInputVaultBalance = handleVaultBalanceChange(
     event.address,
     inputVaultId,
     inputToken,
-    input,
+    inputAmount,
     owner
-  );
+  )
   let oldOutputVaultBalance = handleVaultBalanceChange(
     event.address,
     outputVaultId,
     outputToken,
-    output.neg(),
+    outputAmount.neg(),
     owner
-  );
+  )
 
   let inputVaultBalanceChange = createTradeVaultBalanceChangeEntity(
     event,
     orderHash,
     vaultEntityId(event.address, owner, inputVaultId, inputToken),
     oldInputVaultBalance,
-    input
-  );
+    inputAmount
+  )
   let outputVaultBalanceChange = createTradeVaultBalanceChangeEntity(
     event,
     orderHash,
     vaultEntityId(event.address, owner, outputVaultId, outputToken),
     oldOutputVaultBalance,
-    output.neg()
-  );
+    outputAmount.neg()
+  )
 
   createTradeEntity(
     event,
     orderHash,
     inputVaultBalanceChange,
     outputVaultBalanceChange
-  );
+  )
 }
 
 export function handleAfterClear(event: AfterClear): void {
+  createOrderbookEntity(event)
+
   let clearTemporaryData = ClearTemporaryData.load(
     clearTemporaryDataEntityId(event)
-  );
+  )
   if (clearTemporaryData) {
-    createTrade(event, clearTemporaryData, true);
-    createTrade(event, clearTemporaryData, false);
+    createTrade(
+      event,
+      clearTemporaryData.aliceOrderHash,
+      clearTemporaryData.aliceAddress,
+      clearTemporaryData.aliceInputVaultId,
+      clearTemporaryData.aliceOutputVaultId,
+      clearTemporaryData.aliceInputToken,
+      clearTemporaryData.aliceOutputToken,
+      event.params.clearStateChange.aliceInput,
+      event.params.clearStateChange.aliceOutput
+    )
+    createTrade(
+      event,
+      clearTemporaryData.bobOrderHash,
+      clearTemporaryData.bobAddress,
+      clearTemporaryData.bobInputVaultId,
+      clearTemporaryData.bobOutputVaultId,
+      clearTemporaryData.bobInputToken,
+      clearTemporaryData.bobOutputToken,
+      event.params.clearStateChange.bobInput,
+      event.params.clearStateChange.bobOutput
+    )
 
     // handle bounty vault changes
     const aliceBountyAmount = event.params.clearStateChange.aliceOutput.minus(
       event.params.clearStateChange.bobInput
-    );
+    )
     const bobBountyAmount = event.params.clearStateChange.bobOutput.minus(
       event.params.clearStateChange.aliceInput
-    );
+    )
     if (aliceBountyAmount.gt(BigInt.fromU32(0))) {
       handleVaultBalanceChange(
         event.address,
@@ -165,7 +164,7 @@ export function handleAfterClear(event: AfterClear): void {
         clearTemporaryData.aliceOutputToken,
         aliceBountyAmount,
         event.params.sender
-      );
+      )
     }
     if (bobBountyAmount.gt(BigInt.fromU32(0))) {
       handleVaultBalanceChange(
@@ -174,12 +173,13 @@ export function handleAfterClear(event: AfterClear): void {
         clearTemporaryData.bobOutputToken,
         bobBountyAmount,
         event.params.sender
-      );
+      )
     }
-    store.remove("ClearTemporaryData", clearTemporaryData.id.toHexString());
+
+    store.remove("ClearTemporaryData", clearTemporaryData.id.toHexString())
   } else {
     log.error("ClearTemporaryData not found for event {}", [
       event.transaction.hash.toHexString(),
-    ]);
+    ])
   }
 }
--- subgraph/tests/handlers/handle-clear.test.ts
+++ subgraph/tests/handlers/handle-clear.test.ts
@@ -5,83 +5,117 @@ import {
   describe,
   afterEach,
   clearInBlockStore,
-} from "matchstick-as";
-import { Bytes, BigInt, Address, log } from "@graphprotocol/graph-ts";
+} from "matchstick-as"
+import { Bytes, BigInt, Address } from "@graphprotocol/graph-ts"
 import {
   clearTemporaryDataEntityId,
   handleAfterClear,
   handleClear,
-} from "../../src/clear";
+} from "../../src/clear"
 import {
   AfterClearClearStateChangeStruct,
   ClearV2ClearConfigStruct,
   ClearV2Struct,
   createAfterClearEvent,
   createClearEvent,
+  createDepositEvent,
   Evaluable,
   IO,
-} from "../event-mocks.test";
-import { createMockERC20Functions } from "../erc20.test";
+} from "../event-mocks.test"
+import { createMockERC20Functions } from "../erc20.test"
+import { ClearTemporaryData, Vault } from "../../generated/schema"
+import { vaultEntityId } from "../../src/vault"
+import { handleDeposit } from "../../src/deposit"
+
+const alice = Address.fromString("0x850c40aBf6e325231ba2DeD1356d1f2c267e63Ce")
+const bob = Address.fromString("0x813aef302Ebad333EDdef619C6f8eD7FeF51BA7c")
+
+const token1 = Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2BB")
+const token2 = Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Bc")
+const token3 = Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Ba")
 
 describe("Clear", () => {
   afterEach(() => {
-    clearStore();
-    clearInBlockStore();
-  });
+    clearStore()
+    clearInBlockStore()
+  })
 
   test("clearEvent and afterClearEvent", () => {
-    let event = createClearEvent(
-      Address.fromString("0x850c40aBf6e325231ba2DeD1356d1f2c267e63Ce"),
+    createMockERC20Functions(token1)
+    createMockERC20Functions(token2)
+    createMockERC20Functions(token3)
+
+    // Alice input token
+    handleDeposit(
+      createDepositEvent(
+        alice,
+        token1,
+        BigInt.fromI32(1),
+        BigInt.fromString("10000")
+      )
+    )
+    // Alice output token
+    handleDeposit(
+      createDepositEvent(
+        alice,
+        token2,
+        BigInt.fromI32(1),
+        BigInt.fromString("10000")
+      )
+    )
+    // Bob input token
+    handleDeposit(
+      createDepositEvent(
+        bob,
+        token2,
+        BigInt.fromI32(2),
+        BigInt.fromString("10000")
+      )
+    )
+    // Bob output tokens
+    handleDeposit(
+      createDepositEvent(
+        bob,
+        token3,
+        BigInt.fromI32(2),
+        BigInt.fromString("10000")
+      )
+    )
+    handleDeposit(
+      createDepositEvent(
+        bob,
+        token1,
+        BigInt.fromI32(2),
+        BigInt.fromString("10000")
+      )
+    )
+
+    let clearEvent = createClearEvent(
+      alice,
       new ClearV2Struct(
-        Address.fromString("0x850c40aBf6e325231ba2DeD1356d1f2c267e63Ce"),
+        alice,
         new Evaluable(
           Address.fromString("0x5fB33D710F8B58DE4c9fDEC703B5c2487a5219d6"),
           Address.fromString("0x84c6e7F5A1e5dD89594Cc25BEf4722A1b8871aE6"),
           Bytes.fromHexString("0x1234567890123456789012345678901234567890")
         ),
-        [
-          new IO(
-            Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2BB"),
-            BigInt.fromI32(18),
-            BigInt.fromI32(1)
-          ),
-        ],
-        [
-          new IO(
-            Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Bc"),
-            BigInt.fromI32(18),
-            BigInt.fromI32(1)
-          ),
-        ],
+        [new IO(token1, BigInt.fromI32(18), BigInt.fromI32(1))],
+        [new IO(token2, BigInt.fromI32(18), BigInt.fromI32(1))],
         Bytes.fromHexString(
           "0xbce73059f54ada335f7283df99f81d42a3f2d09527eade865627e26cd756b748"
         )
       ),
       new ClearV2Struct(
-        Address.fromString("0x813aef302Ebad333EDdef619C6f8eD7FeF51BA7c"),
+        bob,
         new Evaluable(
           Address.fromString("0x5fB33D710F8B58DE4c9fDEC703B5c2487a5219d6"),
           Address.fromString("0x84c6e7F5A1e5dD89594Cc25BEf4722A1b8871aE6"),
           Bytes.fromHexString("0x1234567890123456789012345678901234567890")
         ),
+        [new IO(token2, BigInt.fromI32(18), BigInt.fromI32(2))],
         [
-          new IO(
-            Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Bc"),
-            BigInt.fromI32(18),
-            BigInt.fromI32(2)
-          ),
-        ],
-        [
-          new IO(
-            Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Ba"),
-            BigInt.fromI32(18),
-            BigInt.fromI32(2)
-          ),
-          new IO(
-            Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2BB"),
-            BigInt.fromI32(18),
-            BigInt.fromI32(2)
-          ),
+          new IO(token3, BigInt.fromI32(18), BigInt.fromI32(2)),
+          new IO(token1, BigInt.fromI32(18), BigInt.fromI32(2)),
         ],
         Bytes.fromHexString(
           "0x9c8176f8e6e02b5f02eee226ff7066d2474bdc50f89bd15dca539240e0cb1788"
@@ -95,71 +129,101 @@ describe("Clear", () => {
         BigInt.fromI32(1),
         BigInt.fromI32(1)
       )
-    );
+    )
 
-    let aliceInput = event.params.alice.validInputs[0];
-    assert.addressEquals(
-      aliceInput.token,
-      Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2BB")
-    );
-    let aliceOutput = event.params.alice.validOutputs[0];
-    assert.addressEquals(
-      aliceOutput.token,
-      Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Bc")
-    );
+    let aliceInput = clearEvent.params.alice.validInputs[0]
+    assert.addressEquals(aliceInput.token, token1)
+    let aliceOutput = clearEvent.params.alice.validOutputs[0]
+    assert.addressEquals(aliceOutput.token, token2)
 
-    let bobInput = event.params.bob.validInputs[0];
-    assert.addressEquals(
-      bobInput.token,
-      Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Bc")
-    );
-    let bobOutput = event.params.bob.validOutputs[1];
-    assert.addressEquals(
-      bobOutput.token,
-      Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2BB")
-    );
+    let bobInput = clearEvent.params.bob.validInputs[0]
+    assert.addressEquals(bobInput.token, token2)
+    let bobOutput = clearEvent.params.bob.validOutputs[1]
+    assert.addressEquals(bobOutput.token, token1)
 
-    let id = clearTemporaryDataEntityId(event);
-    handleClear(event);
+    let id = clearTemporaryDataEntityId(clearEvent)
+    handleClear(clearEvent)
 
-    assert.entityCount("ClearTemporaryData", 1);
-    assert.fieldEquals(
-      "ClearTemporaryData",
-      id.toHexString(),
-      "aliceAddress",
-      "0x850c40abf6e325231ba2ded1356d1f2c267e63ce"
-    );
-    assert.fieldEquals(
-      "ClearTemporaryData",
-      id.toHexString(),
-      "bobAddress",
-      "0x813aef302ebad333eddef619c6f8ed7fef51ba7c"
-    );
-
-    createMockERC20Functions(
-      Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2BB")
-    );
-    createMockERC20Functions(
-      Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Bc")
-    );
-    createMockERC20Functions(
-      Address.fromString("0x12e605bc104e93B45e1aD99F9e555f659051c2Ba")
-    );
+    assert.entityCount("ClearTemporaryData", 1)
+    let clearTemporaryData = ClearTemporaryData.load(id)
+    assert.assertNotNull(clearTemporaryData)
+    if (clearTemporaryData == null) {
+      return
+    }
+    assert.bytesEquals(clearTemporaryData.aliceAddress, alice)
+    assert.bytesEquals(clearTemporaryData.bobAddress, bob)
+    assert.bigIntEquals(clearTemporaryData.aliceInputVaultId, BigInt.fromI32(1))
+    assert.bigIntEquals(
+      clearTemporaryData.aliceOutputVaultId,
+      BigInt.fromI32(1)
+    )
+    assert.bigIntEquals(clearTemporaryData.bobInputVaultId, BigInt.fromI32(2))
+    assert.bigIntEquals(clearTemporaryData.bobOutputVaultId, BigInt.fromI32(2))
+    assert.bytesEquals(clearTemporaryData.aliceInputToken, token1)
+    assert.bytesEquals(clearTemporaryData.aliceOutputToken, token2)
+    assert.bytesEquals(clearTemporaryData.bobInputToken, token2)
+    assert.bytesEquals(clearTemporaryData.bobOutputToken, token1)
 
     let afterClearEvent = createAfterClearEvent(
-      Address.fromString("0x850c40aBf6e325231ba2DeD1356d1f2c267e63Ce"),
+      alice,
       new AfterClearClearStateChangeStruct(
-        BigInt.fromString("10000000000000000000"),
-        BigInt.fromString("12476769284020210880"),
-        BigInt.fromString("11308584431993808000"),
-        BigInt.fromString("10000000000000000000")
+        BigInt.fromString("1000"),
+        BigInt.fromString("1247"),
+        BigInt.fromString("1130"),
+        BigInt.fromString("1000")
       )
-    );
+    )
 
-    id = clearTemporaryDataEntityId(afterClearEvent);
-    handleAfterClear(afterClearEvent);
+    id = clearTemporaryDataEntityId(afterClearEvent)
+    handleAfterClear(afterClearEvent)
 
-    assert.entityCount("ClearTemporaryData", 0);
-    assert.entityCount("Trade", 2);
-  });
-});
+    assert.entityCount("ClearTemporaryData", 0)
+    assert.entityCount("Trade", 2)
+    assert.entityCount("TradeVaultBalanceChange", 4)
+
+    let aliceInputVault = Vault.load(
+      vaultEntityId(
+        afterClearEvent.address,
+        clearEvent.params.alice.owner,
+        aliceInput.vaultId,
+        aliceInput.token
+      )
+    )
+    assert.assertNotNull(aliceInputVault)
+    if (aliceInputVault == null) {
+      return
+    }
+    assert.bigIntEquals(aliceInputVault.balance, BigInt.fromString("11130"))
+
+    let aliceOutputVault = Vault.load(
+      vaultEntityId(
+        clearEvent.address,
+        clearEvent.params.alice.owner,
+        aliceOutput.vaultId,
+        aliceOutput.token
+      )
+    )
+    assert.assertNotNull(aliceOutputVault)
+    if (aliceOutputVault == null) {
+      return
+    }
+    assert.bigIntEquals(aliceOutputVault.balance, BigInt.fromString("9000"))
+
+    let bobVault = Vault.load(
+      vaultEntityId(
+        clearEvent.address,
+        clearEvent.params.bob.owner,
+        bobOutput.vaultId,
+        bobOutput.token
+      )
+    )
+    assert.assertNotNull(bobVault)
+    if (bobVault == null) {
+      return
+    }
+    assert.bigIntEquals(
+      bobVault.balance,
+      BigInt.fromString("1168184852026402880")
+    )
+  })
+})
