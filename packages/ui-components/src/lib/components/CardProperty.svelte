c922aa3450bdc81a9cad4ed6ce08e8ef56225f44
Jamie Harding <jharding142@icloud.com>
2024-11-07 13:59:13 +0100

    rm testid

--- packages/ui-components/src/lib/components/CardProperty.svelte
+++ packages/ui-components/src/lib/components/CardProperty.svelte
@@ -1,5 +1,8 @@
-<div {...$$props} data-testid="card-property">
-	<h5 class="text-md mb-1 w-full tracking-tight text-gray-400 dark:text-gray-400">
+<div {...$$props}>
+	<h5
+		data-testid="card-property-key"
+		class="text-md mb-1 w-full tracking-tight text-gray-400 dark:text-gray-400"
+	>
 		<slot name="key" />
 	</h5>
 	<p class="font-regular break-all leading-tight text-gray-800 dark:text-white">
