9610fce9e73fe6b7533384adc37cd4b6c9723b1c
Jamie Harding <jharding142@icloud.com>
2024-11-07 11:53:15 +0100

    add tests and yaml

--- packages/webapp/src/routes/+page.svelte
+++ packages/webapp/src/routes/+page.svelte
@@ -2,12 +2,14 @@
 	import { CardProperty } from '@rainlanguage/ui-components';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation...
-</p>
+<div data-testid="page-container">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation...
+	</p>
 
-<CardProperty data-testid="vaultDetailVaultId">
-	<svelte:fragment slot="key">Vault ID</svelte:fragment>
-	<svelte:fragment slot="value">{100000}</svelte:fragment>
-</CardProperty>
+	<CardProperty data-testid="card-property">
+		<svelte:fragment slot="key">Vault ID</svelte:fragment>
+		<svelte:fragment slot="value">{100000}</svelte:fragment>
+	</CardProperty>
+</div>
