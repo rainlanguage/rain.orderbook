0f75d7c4c3ecabb844dbe5243ea3c6ec55fd3a4c
rouzwelt <rouzweltt@gmail.com>
2024-11-08 18:13:14 +0000

    update

--- prep-tauri.sh
+++ prep-tauri.sh
@@ -18,7 +18,7 @@ keep=(
 nix develop -i ${keep[@]} -c rainix-sol-prelude
 nix develop -i ${keep[@]} -c rainix-rs-prelude
 nix develop -i ${keep[@]} -c raindex-prelude
-nix develop -i ${keep[@]} -c bash -c 'cd packages/orderbook && npm run build'
+nix develop -i ${keep[@]} -c bash -c '(cd packages/orderbook && npm run build)'
 nix develop -i ${keep[@]} -c npm run build:ui
 
 
