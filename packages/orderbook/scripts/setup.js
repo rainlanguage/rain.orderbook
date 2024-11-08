93caded3e28ffd4cadb37861ab95d94a30f1fbb6
rouzwelt <rouzweltt@gmail.com>
2024-11-08 16:16:38 +0000

    update

--- packages/orderbook/scripts/setup.js
+++ packages/orderbook/scripts/setup.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 
 // exit early if already built
 // for npm install from an already built and packed package
-if (fs.existsSync("./packages/orderbook/dist")) return;
+if (fs.existsSync("./dist")) return;
 
 execSync("npm run rm-temp");
 execSync("npm run rm-dist");
