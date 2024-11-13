bce5ec72192f4a2d94190514510c4a4de291a0ba
Jamie Harding <jharding142@icloud.com>
2024-11-13 20:08:51 +0100

    tests

--- packages/webapp/src/routes/+page.svelte
+++ packages/webapp/src/routes/+page.svelte
@@ -2,4 +2,6 @@
 	import { Hash, HashType } from '@rainlanguage/ui-components';
 </script>
 
-<Hash value="0x1234567890abcdef" type={HashType.Wallet} shorten={true} />
+<div data-testid="page-container">
+	<Hash value="0x1234567890abcdef" type={HashType.Wallet} shorten={true} />
+</div>
