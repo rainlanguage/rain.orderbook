c4c76400bf51ccbb2739973c2292a1e0d0ecc25b
Jamie Harding <jharding142@icloud.com>
2024-11-13 12:24:00 +0100

    remove

--- packages/webapp/src/routes/ordersList/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { OrdersListTable } from '@rainlanguage/ui-components';
-</script>
-
-<OrdersListTable connectedWalletAddress={'0xf08bCbce72f62c95Dcb7c07dCb5Ed26ACfCfBc11'}>
-	<svelte:fragment slot="filters"
-		><div class="border-4 border-red-500 p-2 text-xl">Place for filters</div></svelte:fragment
-	>
-</OrdersListTable>
