09770133b1a6677ba79c94ed5faae4e75f928e05
rouzwelt <rouzweltt@gmail.com>
2024-11-14 21:28:30 +0000

    Update wasm_traits.rs

--- crates/bindings/src/wasm_traits.rs
+++ crates/bindings/src/wasm_traits.rs
@@ -11,16 +11,16 @@ pub mod prelude {
 
 #[macro_export]
 macro_rules! impl_main_wasm_traits {
-    ($struct_name:path) => {{
+    ($type_name:path) => {{
         use $crate::wasm_traits::prelude::*;
 
-        impl WasmDescribe for $struct_name {
+        impl WasmDescribe for $type_name {
             #[inline]
             fn describe() {
                 <Self as Tsify>::JsType::describe()
             }
         }
-        impl IntoWasmAbi for $struct_name {
+        impl IntoWasmAbi for $type_name {
             type Abi = <<Self as Tsify>::JsType as IntoWasmAbi>::Abi;
 
             #[inline]
@@ -32,13 +32,13 @@ macro_rules! impl_main_wasm_traits {
                     .into_abi()
             }
         }
-        impl OptionIntoWasmAbi for $struct_name {
+        impl OptionIntoWasmAbi for $type_name {
             #[inline]
             fn none() -> Self::Abi {
                 <<Self as Tsify>::JsType as OptionIntoWasmAbi>::none()
             }
         }
-        impl FromWasmAbi for $struct_name {
+        impl FromWasmAbi for $type_name {
             type Abi = <<Self as Tsify>::JsType as FromWasmAbi>::Abi;
 
             #[inline]
@@ -49,7 +49,7 @@ macro_rules! impl_main_wasm_traits {
                     .expect_throw(&err)
             }
         }
-        impl OptionFromWasmAbi for $struct_name {
+        impl OptionFromWasmAbi for $type_name {
             #[inline]
             fn is_none(js: &Self::Abi) -> bool {
                 <<Self as Tsify>::JsType as OptionFromWasmAbi>::is_none(js)
@@ -60,50 +60,50 @@ macro_rules! impl_main_wasm_traits {
 
 #[macro_export]
 macro_rules! impl_complementary_wasm_traits {
-    ($struct_name:path) => {{
+    ($type_name:path) => {{
         use $crate::wasm_traits::prelude::*;
 
-        impl RefFromWasmAbi for $struct_name {
+        impl RefFromWasmAbi for $type_name {
             type Abi = <JsValue as RefFromWasmAbi>::Abi;
-            type Anchor = Box<$struct_name>;
+            type Anchor = Box<$type_name>;
             unsafe fn ref_from_abi(js: Self::Abi) -> Self::Anchor {
-                Box::new(<$struct_name>::from_abi(js))
+                Box::new(<$type_name>::from_abi(js))
             }
         }
-        impl LongRefFromWasmAbi for $struct_name {
+        impl LongRefFromWasmAbi for $type_name {
             type Abi = <JsValue as RefFromWasmAbi>::Abi;
-            type Anchor = Box<$struct_name>;
+            type Anchor = Box<$type_name>;
             unsafe fn long_ref_from_abi(js: Self::Abi) -> Self::Anchor {
-                Box::new(<$struct_name>::from_abi(js))
+                Box::new(<$type_name>::from_abi(js))
             }
         }
-        impl VectorIntoWasmAbi for $struct_name {
+        impl VectorIntoWasmAbi for $type_name {
             type Abi = <Box<[JsValue]> as IntoWasmAbi>::Abi;
             fn vector_into_abi(vector: Box<[Self]>) -> Self::Abi {
                 js_value_vector_into_abi(vector)
             }
         }
-        impl VectorFromWasmAbi for $struct_name {
+        impl VectorFromWasmAbi for $type_name {
             type Abi = <Box<[JsValue]> as IntoWasmAbi>::Abi;
             unsafe fn vector_from_abi(js: Self::Abi) -> Box<[Self]> {
                 js_value_vector_from_abi(js)
             }
         }
-        impl WasmDescribeVector for $struct_name {
+        impl WasmDescribeVector for $type_name {
             fn describe_vector() {
                 inform(VECTOR);
-                <$struct_name>::describe();
+                <$type_name>::describe();
             }
         }
-        impl From<$struct_name> for JsValue {
-            fn from(value: $struct_name) -> Self {
+        impl From<$type_name> for JsValue {
+            fn from(value: $type_name) -> Self {
                 let mut err = "".to_string();
                 to_value(&value)
                     .inspect_err(|e| err.push_str(&e.to_string()))
                     .expect_throw(&err)
             }
         }
-        impl TryFromJsValue for $struct_name {
+        impl TryFromJsValue for $type_name {
             type Error = serde_wasm_bindgen::Error;
             fn try_from_js_value(value: JsValue) -> Result<Self, Self::Error> {
                 from_value(value)
@@ -114,8 +114,8 @@ macro_rules! impl_complementary_wasm_traits {
 
 #[macro_export]
 macro_rules! impl_all_wasm_traits {
-    ($struct_name:path) => {
-        $crate::impl_main_wasm_traits!($struct_name);
-        $crate::impl_complementary_wasm_traits!($struct_name);
+    ($type_name:path) => {
+        $crate::impl_main_wasm_traits!($type_name);
+        $crate::impl_complementary_wasm_traits!($type_name);
     };
 }
