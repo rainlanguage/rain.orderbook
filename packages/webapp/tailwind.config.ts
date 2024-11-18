3c88b133d9e7f4519ce73aec759222fdc51509da
highonhopium <jks.hardy@gmail.com>
2024-11-18 11:37:36 +0000

    workflows and deps

--- packages/webapp/tailwind.config.ts
+++ packages/webapp/tailwind.config.ts
@@ -2,11 +2,11 @@ import type { Config } from 'tailwindcss';
 import { neutral, indigo } from 'tailwindcss/colors';
 
 export default {
-	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		'../../node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
-		'../../node_modules/@rainlanguage/ui-components/**/*.{html,js,svelte,ts}'
+		'../../node_modules/@rainlanguage/ui-components/**/*.{html,js,svelte,ts}',
+		'../ui-components/**/*.{html,js,svelte,ts}'
 	],
 
 	theme: {
