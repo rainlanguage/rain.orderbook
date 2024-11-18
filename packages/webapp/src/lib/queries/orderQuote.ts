924b534eade7a643bb6703e2d3c7702cb7e690e3
Jamie Harding <jharding142@icloud.com>
2024-11-18 13:39:05 +0100

    queries

---
+++ packages/webapp/src/lib/queries/orderQuote.ts
@@ -0,0 +1,78 @@
+import { get } from 'svelte/store';
+import { invoke } from '@tauri-apps/api';
+import { rpcUrl } from '$lib/stores/settings';
+import type { Order } from '$lib/typeshare/subgraphTypes';
+import type { BatchOrderQuotesResponse } from '$lib/typeshare/orderQuote';
+import type { Hex } from 'viem';
+import { mockIPC } from '@tauri-apps/api/mocks';
+import type { RainEvalResultsTable } from '$lib/typeshare/config';
+
+export async function batchOrderQuotes(
+  orders: Order[],
+  blockNumber?: number,
+): Promise<BatchOrderQuotesResponse[]> {
+  return invoke('batch_order_quotes', {
+    orders,
+    blockNumber,
+    rpcUrl: get(rpcUrl),
+  });
+}
+
+export async function debugOrderQuote(
+  order: Order,
+  inputIOIndex: number,
+  outputIOIndex: number,
+  orderbook: Hex,
+  rpcUrl: string,
+  blockNumber?: number,
+) {
+  return await invoke<[RainEvalResultsTable, string | undefined]>('debug_order_quote', {
+    order,
+    inputIoIndex: inputIOIndex,
+    outputIoIndex: outputIOIndex,
+    orderbook,
+    rpcUrl,
+    blockNumber,
+  });
+}
+
+export const mockQuoteDebug: [RainEvalResultsTable, string | undefined] = [
+  {
+    column_names: ['1', '2', '3'],
+    rows: [['0x01', '0x02', '0x03']],
+  },
+  'some error msg',
+];
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  it('uses the trade_debug command correctly', async () => {
+    mockIPC((cmd) => {
+      if (cmd === 'debug_order_quote') {
+        return mockQuoteDebug;
+      }
+    });
+
+    const result = await debugOrderQuote(
+      {
+        id: '1',
+        orderbook: { id: '0x00' },
+        orderBytes: '0x123',
+        orderHash: '0x123',
+        owner: '0x123',
+        outputs: [],
+        inputs: [],
+        active: true,
+        addEvents: [],
+        timestampAdded: '123',
+        trades: [],
+      },
+      0,
+      0,
+      '0x123',
+      'https://rpc-url.com',
+    );
+    expect(result).toEqual(mockQuoteDebug);
+  });
+}
---
+++ packages/webapp/src/lib/queries/orderTradesList.ts
@@ -0,0 +1,94 @@
+import type { Trade, VaultVolume } from '$lib/typeshare/subgraphTypes';
+import { invoke } from '@tauri-apps/api';
+import { DEFAULT_PAGE_SIZE } from './constants';
+import { prepareHistoricalOrderChartData } from '$lib/services/historicalOrderCharts';
+import { colorTheme } from '$lib/stores/darkMode';
+import { get } from 'svelte/store';
+
+export type OrderTradesListArgs = {
+  orderId: string;
+  subgraphArgs: {
+    url: string;
+  };
+  paginationArgs: {
+    page: number;
+    pageSize: number;
+  };
+  startTimestamp?: number;
+  endTimestamp?: number;
+};
+
+export const orderTradesList = async (
+  id: string,
+  url: string | undefined,
+  pageParam: number,
+  pageSize: number = DEFAULT_PAGE_SIZE,
+  startTimestamp?: number,
+  endTimestamp?: number,
+) => {
+  if (!url) {
+    return [];
+  }
+  return await invoke<Trade[]>('order_trades_list', {
+    orderId: id,
+    subgraphArgs: { url },
+    paginationArgs: { page: pageParam + 1, pageSize },
+    startTimestamp,
+    endTimestamp,
+  } as OrderTradesListArgs);
+};
+
+export const orderTradesListForChart = async (
+  id: string,
+  url: string | undefined,
+  pageParam: number,
+  pageSize: number = DEFAULT_PAGE_SIZE,
+  startTimestamp?: number,
+  endTimestamp?: number,
+) => {
+  if (!url) {
+    return [];
+  }
+  const data = await invoke<Trade[]>('order_trades_list', {
+    orderId: id,
+    subgraphArgs: { url },
+    paginationArgs: { page: pageParam + 1, pageSize },
+    startTimestamp,
+    endTimestamp,
+  } as OrderTradesListArgs);
+  return prepareHistoricalOrderChartData(data, get(colorTheme));
+};
+
+export const orderVaultsVolume = async (
+  id: string,
+  url: string | undefined,
+  startTimestamp?: number,
+  endTimestamp?: number,
+) => {
+  if (!url) {
+    return [];
+  }
+  return await invoke<VaultVolume[]>('order_vaults_volume', {
+    orderId: id,
+    subgraphArgs: { url },
+    startTimestamp,
+    endTimestamp,
+  } as OrderTradesListArgs);
+};
+
+export const orderTradesCount = async (
+  id: string,
+  url: string | undefined,
+  startTimestamp?: number,
+  endTimestamp?: number,
+) => {
+  if (!url) {
+    return [];
+  }
+  return await invoke<number>('order_trades_count', {
+    orderId: id,
+    subgraphArgs: { url },
+    startTimestamp,
+    endTimestamp,
+  } as OrderTradesListArgs);
+};
