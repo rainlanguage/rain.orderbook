05f57e5ce141a3d9b489bfb05f0460b0f935a68b
Jamie Harding <jharding142@icloud.com>
2024-11-19 17:00:18 +0100

    stores

---
+++ packages/webapp/src/lib/types/stores.ts
@@ -0,0 +1,8 @@
+import type { Writable } from 'svelte/store';
+
+export interface AppStoresInterface {
+	settings: Writable<Record<string, string>>;
+	activeSubgraphs: Writable<Record<string, string>>;
+	accounts: Writable<Record<string, string>>;
+	activeAccountsItems: Writable<Record<string, string>>;
+}
