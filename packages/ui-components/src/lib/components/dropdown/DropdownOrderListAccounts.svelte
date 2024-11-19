72c33ad5eaad833f0893092d53a187b66aef0c34
Jamie Harding <jharding142@icloud.com>
2024-11-19 16:11:19 +0100

    dropdown clone

---
+++ packages/ui-components/src/lib/components/dropdown/DropdownOrderListAccounts.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { DropdownCheckbox } from '@rainlanguage/ui-components';
+	export let accounts;
+	export let activeAccountsItems;
+	$: options = $accounts;
+</script>
+
+<DropdownCheckbox
+	{options}
+	bind:value={$activeAccountsItems}
+	label="Accounts"
+	allLabel="All accounts"
+	emptyMessage="No accounts added"
+/>
