86fbc248a9ba2b4a320b948f86ead44a4463d5e9
Jamie Harding <jharding142@icloud.com>
2024-11-19 13:56:41 +0100

    formatting

--- packages/webapp/src/routes/+layout.ts
+++ packages/webapp/src/routes/+layout.ts
@@ -1,19 +1,19 @@
 import type { AppStoresInterface } from '../types/stores';
 import { writable } from 'svelte/store';
-import settings from '$lib/settings-12-11-24.json'
+import settings from '$lib/settings-12-11-24.json';
 
 export interface LayoutData {
-  stores: AppStoresInterface;
+	stores: AppStoresInterface;
 }
 
 export const load = () => {
-  return {
-    stores: {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      settings: writable<any>(settings),
-      activeSubgraphs: writable<Record<string, string>>({})
-    }
-  };
+	return {
+		stores: {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			settings: writable<any>(settings),
+			activeSubgraphs: writable<Record<string, string>>({})
+		}
+	};
 };
 
 export const ssr = false;
--- packages/webapp/src/types/stores.ts
+++ packages/webapp/src/types/stores.ts
@@ -1,10 +1,10 @@
 export interface AppStoresInterface {
-  settings: {
-    subscribe: (callback: (value: Record<string, string>) => void) => void;
-    set: (value: Record<string, string>) => void;
-  };
-  activeSubgraphs: {
-    subscribe: (callback: (value: Record<string, string>) => void) => void;
-    set: (value: Record<string, string>) => void;
-  };
-} 
+	settings: {
+		subscribe: (callback: (value: Record<string, string>) => void) => void;
+		set: (value: Record<string, string>) => void;
+	};
+	activeSubgraphs: {
+		subscribe: (callback: (value: Record<string, string>) => void) => void;
+		set: (value: Record<string, string>) => void;
+	};
+}
