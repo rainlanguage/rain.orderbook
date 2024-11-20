7797e4ac029ca80ad929de4e16764577b8bd438f
ninokeldishvili <ninokeldishvili@gmail.com>
2024-11-20 12:04:26 +0400

    Add some test_get_balances_data_mainnet with hardcoded url

--- crates/cli/src/balance.rs
+++ crates/cli/src/balance.rs
@@ -53,7 +53,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_get_balances() {
-        let subgraph_url = "https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-flare/0.8/gn";
+        let subgraph_url = "https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-mainnet/2024-10-25-af6a/gn";
 
         let result = get_balances(subgraph_url).await;
 
@@ -65,7 +65,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_get_balances_data() {
+    async fn test_get_balances_data_mock() {
         // Define the mock response JSON
         let mock_response = serde_json::json!({
             "data": {
@@ -146,6 +146,46 @@ mod tests {
                 assert_eq!(
                     first_output.get("balance").unwrap(),
                     "1000",
+                    "Unexpected balance in output"
+                );
+            }
+
+            println!("Fetched balance data: {:?}", data);
+        }
+    }
+
+    #[tokio::test]
+    async fn C() {
+        let subgraph_url = "https://api.goldsky.com/api/public/project_clv14x04y9kzi01saerx7bxpg/subgraphs/ob4-mainnet/2024-10-25-af6a/gn";
+
+        // Call the function under test
+        let result = get_balances(subgraph_url).await;
+
+        // Assert the function call was successful
+        assert!(result.is_ok(), "Failed to fetch balances: {:?}", result);
+
+        // Validate the returned data structure and values
+        if let Ok(data) = result {
+            // Ensure "data" key exists
+            let orders = data.get("data").and_then(|d| d.get("orders"));
+            assert!(orders.is_some(), "Orders data missing in response");
+
+            if let Some(order_array) = orders {
+                let first_order = &order_array[0];
+                assert_eq!(
+                    first_order.get("owner").unwrap(),
+                    "0xcabcbea6e523274b269fb7b21665c0b4003bd456"
+                );
+                assert_eq!(first_order.get("active").unwrap(), false);
+
+                // Validate the `outputs` -> `balance`
+                let outputs = first_order.get("outputs").unwrap().as_array().unwrap();
+                assert_eq!(outputs.len(), 1, "Unexpected number of outputs");
+
+                let first_output = &outputs[0];
+                assert_eq!(
+                    first_output.get("balance").unwrap(),
+                    "0",
                     "Unexpected balance in output"
                 );
             }
