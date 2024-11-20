fd6ce431a13b11760e880a1788471692d0b9d41c
findolor <anakisci@gmail.com>
2024-11-20 15:49:32 +0300

    update saving by preset logic

--- crates/js_api/src/gui/field_values.rs
+++ crates/js_api/src/gui/field_values.rs
@@ -10,10 +10,10 @@ impl_wasm_traits!(FieldValuePair);
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
-#[serde(tag = "type", content = "value", rename_all = "lowercase")]
-pub enum PairValue {
-    Preset(GuiPreset),
-    Custom(String),
+#[serde(rename_all = "camelCase")]
+pub struct PairValue {
+    pub is_preset: bool,
+    pub value: String,
 }
 impl_wasm_traits!(PairValue);
 
@@ -21,19 +21,16 @@ impl_wasm_traits!(PairValue);
 impl DotrainOrderGui {
     #[wasm_bindgen(js_name = "saveFieldValue")]
     pub fn save_field_value(&mut self, binding: String, value: PairValue) -> Result<(), GuiError> {
-        let field_definition = self
-            .deployment
-            .fields
-            .iter()
-            .find(|field| field.binding == binding)
-            .ok_or(GuiError::FieldBindingNotFound(binding.clone()))?;
-        match value {
-            PairValue::Preset(ref v) => {
-                if !field_definition.presets.contains(&v) {
-                    return Err(GuiError::InvalidPreset);
-                }
+        let field_definition = self.get_field_definition(&binding)?;
+        if value.is_preset {
+            if !field_definition
+                .presets
+                .iter()
+                .find(|preset| preset.id == value.value)
+                .is_some()
+            {
+                return Err(GuiError::InvalidPreset);
             }
-            _ => {}
         }
         self.field_values.insert(binding, value);
         Ok(())
@@ -48,33 +45,47 @@ impl DotrainOrderGui {
     }
 
     #[wasm_bindgen(js_name = "getFieldValue")]
-    pub fn get_field_value(&self, binding: String) -> Result<PairValue, GuiError> {
+    pub fn get_field_value(&self, binding: String) -> Result<GuiPreset, GuiError> {
         let field_value = self
             .field_values
             .get(&binding)
-            .ok_or(GuiError::FieldBindingNotFound(binding))?;
-        Ok(field_value.clone())
+            .ok_or(GuiError::FieldBindingNotFound(binding.clone()))?;
+        let preset = match field_value.is_preset {
+            true => {
+                let field_definition = self.get_field_definition(&binding)?;
+                let preset = field_definition
+                    .presets
+                    .iter()
+                    .find(|preset| preset.id == field_value.value)
+                    .ok_or(GuiError::InvalidPreset)?;
+                preset.clone()
+            }
+            false => GuiPreset {
+                id: "".to_string(),
+                name: None,
+                value: field_value.value.clone(),
+            },
+        };
+        Ok(preset)
     }
 
     #[wasm_bindgen(js_name = "getAllFieldValues")]
-    pub fn get_all_field_values(&self) -> Vec<FieldValuePair> {
-        self.field_values
-            .iter()
-            .map(|(k, v)| FieldValuePair {
-                binding: k.clone(),
-                value: v.clone(),
-            })
-            .collect()
+    pub fn get_all_field_values(&self) -> Result<Vec<GuiPreset>, GuiError> {
+        let mut result = Vec::new();
+        for (binding, _) in self.field_values.iter() {
+            result.push(self.get_field_value(binding.clone())?);
+        }
+        Ok(result)
     }
 
     #[wasm_bindgen(js_name = "getFieldDefinition")]
-    pub fn get_field_definition(&self, binding: String) -> Result<GuiFieldDefinition, GuiError> {
+    pub fn get_field_definition(&self, binding: &str) -> Result<GuiFieldDefinition, GuiError> {
         let field_definition = self
             .deployment
             .fields
             .iter()
             .find(|field| field.binding == binding)
-            .ok_or(GuiError::FieldBindingNotFound(binding))?;
+            .ok_or(GuiError::FieldBindingNotFound(binding.to_string()))?;
         Ok(field_definition.clone())
     }
 
