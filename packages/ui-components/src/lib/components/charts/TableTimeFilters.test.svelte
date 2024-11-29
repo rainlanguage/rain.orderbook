7c39853dc835110267d7bc7c5bc8f1bd0ae5e083
Jamie Harding <jharding142@icloud.com>
2024-11-29 14:00:53 +0100

    tableTime filters

---
+++ packages/ui-components/src/lib/components/charts/TableTimeFilters.test.svelte
@@ -0,0 +1,9 @@
+<script>
+	import TableTimeFilters from './TableTimeFilters.svelte';
+	import { writable } from 'svelte/store';
+
+	export let startTimeStore = writable();
+	export let endTimeStore = writable();
+</script>
+
+<TableTimeFilters bind:startTimestamp={$startTimeStore} bind:endTimestamp={$endTimeStore} />
