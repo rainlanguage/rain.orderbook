53397580b67cef94664b2c66ab08efc1d427d069
Jamie Harding <jharding142@icloud.com>
2024-12-03 13:31:36 +0100

    import type from quote

--- packages/ui-components/src/lib/components/detail/TanstackOrderQuote.svelte
+++ packages/ui-components/src/lib/components/detail/TanstackOrderQuote.svelte
@@ -2,7 +2,7 @@
 	import Refresh from '../icon/Refresh.svelte';
 	import EditableSpan from '../EditableSpan.svelte';
 	import { getOrderQuote } from '@rainlanguage/orderbook/quote';
-	import type { Order } from '../../typeshare/subgraphTypes';
+	import type { Order } from '@rainlanguage/orderbook/quote';
 	import { QKEY_ORDER_QUOTE } from '../../queries/keys';
 	import { formatUnits, hexToNumber, isHex } from 'viem';
 	import { createQuery } from '@tanstack/svelte-query';
