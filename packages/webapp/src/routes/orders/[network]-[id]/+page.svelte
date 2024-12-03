e7e5788da34228004961b3657813638a7b2083b9
Jamie Harding <jharding142@icloud.com>
2024-12-03 14:54:49 +0100

    new query

--- packages/webapp/src/routes/orders/[network]-[id]/+page.svelte
+++ packages/webapp/src/routes/orders/[network]-[id]/+page.svelte
@@ -5,15 +5,30 @@
 		OrderTradesListTable,
 		colorTheme,
 		lightweightChartsTheme,
-		TanstackOrderQuote
+		TanstackOrderQuote,
+		QKEY_ORDER
 	} from '@rainlanguage/ui-components';
+	import { getOrder } from '@rainlanguage/orderbook/js_api';
+	import { createQuery } from '@tanstack/svelte-query';
 	const { id, network } = $page.params;
 	const { settings } = $page.data.stores;
 	const subgraphUrl = $settings.subgraphs[network];
 	const rpcUrl = $settings.networks[network]?.rpc;
+	$: console.log(rpcUrl);
+	$: orderDetailQuery = createQuery({
+		queryKey: [id, QKEY_ORDER + id],
+		queryFn: () => getOrder(subgraphUrl, id || ''),
+		enabled: !!subgraphUrl && !!id
+	});
+
+	$: order = $orderDetailQuery.data;
+	$: console.log(order);
 </script>
 
 <h1>Order Trades</h1>
-<TanstackOrderQuote {id} {colorTheme} {lightweightChartsTheme} />
-<OrderTradesChart {id} {subgraphUrl} {colorTheme} {lightweightChartsTheme} />
-<OrderTradesListTable {id} {subgraphUrl} {rpcUrl} />
+{#if $orderDetailQuery.data}
+	Hi!
+	<TanstackOrderQuote {id} {order} />
+	<OrderTradesChart {id} {subgraphUrl} {colorTheme} {lightweightChartsTheme} />
+	<OrderTradesListTable {id} {subgraphUrl} {rpcUrl} />
+{/if}
