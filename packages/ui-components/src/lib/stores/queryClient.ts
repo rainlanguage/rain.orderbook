a3eee1640dca5211b8abbfc95028a7cd5837e890
Jamie Harding <jharding142@icloud.com>
2024-12-04 12:23:50 +0100

    format

--- packages/ui-components/src/lib/stores/queryClient.ts
+++ packages/ui-components/src/lib/stores/queryClient.ts
@@ -2,9 +2,9 @@ import { browser } from '$app/environment';
 import { QueryClient } from '@tanstack/svelte-query';
 
 export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      enabled: browser,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			enabled: browser
+		}
+	}
 });
