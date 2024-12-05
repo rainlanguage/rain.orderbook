974df83135e88de899ea67cbe5688449be77b792
nino <Ninokeldishvili@gmail.com>
2024-12-05 16:53:27 +0400

    Lint

--- packages/ui-components/src/lib/components/Heading.svelte
+++ packages/ui-components/src/lib/components/Heading.svelte
@@ -1,19 +1,19 @@
 <script>
-  export let depth;
+	export let depth;
 </script>
 
 {#if depth === 1}
-  <div class="mb-4 text-4xl font-medium dark:text-white"><slot /></div>
+	<div class="mb-4 text-4xl font-medium dark:text-white"><slot /></div>
 {:else if depth === 2}
-  <div class="my-4 text-2xl font-medium dark:text-white"><slot /></div>
+	<div class="my-4 text-2xl font-medium dark:text-white"><slot /></div>
 {:else if depth === 3}
-  <div class="my-4 text-xl font-medium dark:text-white"><slot /></div>
+	<div class="my-4 text-xl font-medium dark:text-white"><slot /></div>
 {:else if depth === 4}
-  <div class="my-4 text-lg font-medium dark:text-white"><slot /></div>
+	<div class="my-4 text-lg font-medium dark:text-white"><slot /></div>
 {:else if depth === 5}
-  <h5><slot /></h5>
+	<h5><slot /></h5>
 {:else if depth === 6}
-  <h6><slot /></h6>
+	<h6><slot /></h6>
 {:else}
-  <slot />
+	<slot />
 {/if}
--- packages/ui-components/src/lib/components/Text.svelte
+++ packages/ui-components/src/lib/components/Text.svelte
@@ -1,12 +1,12 @@
 <script>
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  export let text;
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  text;
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  export let raw;
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  raw;
+	// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+	export let text;
+	// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+	text;
+	// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+	export let raw;
+	// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+	raw;
 </script>
 
 <div class="my-2 mb-3"><slot /></div>
