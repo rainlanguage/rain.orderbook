425b40133822accfd5820253be9db6a8b3967577
nino <Ninokeldishvili@gmail.com>
2024-12-09 13:05:44 +0400

    Add test testClear2ZeroRatioBug - show no changes to Alice's or Bob's balances due to zero ratio

--- test/concrete/ob/OrderBook.clear.mock.t.sol
+++ test/concrete/ob/OrderBook.clear.mock.t.sol
@@ -139,6 +139,101 @@ contract OrderBookClearTest is OrderBookExternalMockTest {
         assertEq(iOrderbook.vaultBalance(bountyBot, address(iToken0), bobBountyVaultId), 0.00495e18);
     }
 
+    function testClear2ZeroRatioBug(
+        address alice,
+        OrderConfigV3 memory aliceConfig,
+        address bob,
+        OrderConfigV3 memory bobConfig,
+        bytes memory expression,
+        address bountyBot,
+        uint256 aliceBountyVaultId,
+        uint256 bobBountyVaultId
+    ) public {
+        // Different accounts
+        vm.assume(alice != bob);
+        vm.assume(alice != bountyBot);
+        vm.assume(bob != bountyBot);
+        vm.assume(aliceBountyVaultId != bobBountyVaultId);
+        vm.assume(aliceConfig.validInputs.length > 0);
+        vm.assume(aliceConfig.validOutputs.length > 0);
+        vm.assume(bobConfig.validInputs.length > 0);
+        vm.assume(bobConfig.validOutputs.length > 0);
+
+        aliceConfig.evaluable.interpreter = iInterpreter;
+        aliceConfig.evaluable.store = iStore;
+
+        bobConfig.evaluable.interpreter = iInterpreter;
+        bobConfig.evaluable.store = iStore;
+
+        aliceConfig.validInputs[0].token = address(iToken0);
+        aliceConfig.validOutputs[0].token = address(iToken1);
+
+        bobConfig.validInputs[0].token = address(iToken1);
+        bobConfig.validOutputs[0].token = address(iToken0);
+
+        aliceConfig.validInputs[0].decimals = 18;
+        aliceConfig.validOutputs[0].decimals = 18;
+        bobConfig.validInputs[0].decimals = 18;
+        bobConfig.validOutputs[0].decimals = 18;
+
+        aliceConfig.meta = "";
+        bobConfig.meta = "";
+
+        uint256 amount = 2e18;
+
+        _depositInternal(alice, aliceConfig.validOutputs[0].token, aliceConfig.validOutputs[0].vaultId, amount);
+        _depositInternal(bob, bobConfig.validOutputs[0].token, bobConfig.validOutputs[0].vaultId, amount);
+        {
+            (OrderV3 memory aliceOrder, bytes32 aliceOrderHash) = addOrderWithChecks(alice, aliceConfig, expression);
+            (OrderV3 memory bobOrder, bytes32 bobOrderHash) = addOrderWithChecks(bob, bobConfig, expression);
+
+            ClearConfig memory configClear = ClearConfig({
+                aliceInputIOIndex: 0,
+                aliceOutputIOIndex: 0,
+                bobInputIOIndex: 0,
+                bobOutputIOIndex: 0,
+                aliceBountyVaultId: aliceBountyVaultId,
+                bobBountyVaultId: bobBountyVaultId
+            });
+
+            {
+                // Mock the interpreter.eval for Alice and Bob orders with zero ratio
+                uint256[] memory orderStackAlice = new uint256[](2);
+                orderStackAlice[0] = 0; // Zero orderIORatio
+                orderStackAlice[1] = 5e17; // orderOutputMax
+                vm.mockCall(
+                    address(iInterpreter),
+                    abi.encodeWithSelector(IInterpreterV3.eval3.selector),
+                    abi.encode(orderStackAlice, new uint256[](0))
+                );
+                uint256[] memory orderStackBob = new uint256[](2);
+
+                orderStackBob[0] = 0; // Zero orderIORatio
+                orderStackBob[1] = 5e17; // orderOutputMax
+                vm.mockCall(
+                    address(iInterpreter),
+                    abi.encodeWithSelector(IInterpreterV3.eval3.selector),
+                    abi.encode(orderStackBob, new uint256[](0))
+                );
+            }
+
+            vm.prank(bountyBot);
+            iOrderbook.clear2(aliceOrder, bobOrder, configClear, new SignedContextV1[](0), new SignedContextV1[](0));
+        }
+        // No changes to Alice's or Bob's balances due to zero ratio
+        assertEq(
+            iOrderbook.vaultBalance(alice, aliceConfig.validInputs[0].token, aliceConfig.validInputs[0].vaultId), 0
+        );
+        assertEq(
+            iOrderbook.vaultBalance(alice, aliceConfig.validOutputs[0].token, aliceConfig.validOutputs[0].vaultId),
+            amount
+        );
+        assertEq(iOrderbook.vaultBalance(bob, bobConfig.validInputs[0].token, bobConfig.validInputs[0].vaultId), 0);
+        assertEq(
+            iOrderbook.vaultBalance(bob, bobConfig.validOutputs[0].token, bobConfig.validOutputs[0].vaultId), amount
+        );
+    }
+
     /// Make a deposit to the OB mocking the internal transferFrom call.
     function _depositInternal(address depositor, address token, uint256 vaultId, uint256 amount) internal {
         vm.prank(depositor);
