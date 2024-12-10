0db8bcb9fd2a6e15a629dcda312c7b3863c83292
nino <Ninokeldishvili@gmail.com>
2024-12-10 14:56:22 +0400

    Format

--- packages/ui-components/src/lib/components/ButtonLoading.svelte
+++ packages/ui-components/src/lib/components/ButtonLoading.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import { Button, Spinner } from 'flowbite-svelte';
+	import { Button, Spinner } from 'flowbite-svelte';
 
-  export let loading = false;
-  export let disabled = false;
+	export let loading = false;
+	export let disabled = false;
 </script>
 
 <Button disabled={loading || disabled} {...$$props} on:click>
-  {#if loading}
-    <Spinner class="mr-2 h-4 w-4" color="white" />
-  {/if}
-  <slot />
+	{#if loading}
+		<Spinner class="mr-2 h-4 w-4" color="white" />
+	{/if}
+	<slot />
 </Button>
