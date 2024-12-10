9bdbba12500756d21f50a959fdd709c5bf0820cd
nino <Ninokeldishvili@gmail.com>
2024-12-10 19:24:42 +0400

    Move ButtonDarkMode component into ui-components

---
+++ packages/ui-components/src/lib/components/ButtonDarkMode.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { DarkMode } from 'flowbite-svelte';
+	import { writable } from 'svelte/store';
+	export let colorTheme = writable();
+	function toggle(e: MouseEvent) {
+		const isDark = (e.target as HTMLElement).ownerDocument.documentElement.classList.contains(
+			'dark'
+		);
+		colorTheme.set(isDark ? 'dark' : 'light');
+	}
+</script>
+
+<button type="button" on:click={toggle}>
+	<DarkMode />
+</button>
