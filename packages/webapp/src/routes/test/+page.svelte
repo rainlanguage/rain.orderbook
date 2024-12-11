41eee845e958c0c73f1d4ff2a88eefbb6f466fd7
nino <Ninokeldishvili@gmail.com>
2024-12-11 14:08:27 +0400

    Create test route for ButtonLoading component

---
+++ packages/webapp/src/routes/test/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" generics="T">
+	import { ButtonLoading } from '@rainlanguage/ui-components';
+	let isLoading: boolean = true;
+</script>
+
+<div>
+	<ButtonLoading color="primary" class="w-full px-2 py-1" size="lg" pill loading={isLoading}
+		>Loading button</ButtonLoading
+	>
+</div>
