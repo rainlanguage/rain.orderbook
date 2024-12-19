b2d6076548cb24eea1ff123ddc93bc83287f7dfa
Jamie Harding <jharding142@icloud.com>
2024-12-19 10:35:57 +0100

    type check

--- packages/ui-components/src/__tests__/ButtonVaultLink.test.ts
+++ packages/ui-components/src/__tests__/ButtonVaultLink.test.ts
@@ -1,11 +1,9 @@
-// @ts-nocheck
 import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 import ButtonVaultLink from '../lib/components/ButtonVaultLink.svelte';
 import * as navigation from '$app/navigation';
 import { userEvent } from '@testing-library/user-event';
 import type { Vault } from '@rainlanguage/orderbook/js_api';
-// TODO: Fox the inconsistent erc20 type for token
 // Mock the $app/navigation module
 vi.mock('$app/navigation', () => ({
 	goto: vi.fn()
