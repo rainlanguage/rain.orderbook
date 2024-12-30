ba29304df3b0f8f7c7ad57a2ef88b5f5b9e8f06e
nino <Ninokeldishvili@gmail.com>
2024-12-30 15:54:16 +0400

    Fmt

--- packages/ui-components/src/lib/components/debug/EvalResultsTable.svelte
+++ packages/ui-components/src/lib/components/debug/EvalResultsTable.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-  import {
-    Table,
-    TableBody,
-    TableBodyCell,
-    TableBodyRow,
-    TableHead,
-    TableHeadCell,
-  } from 'flowbite-svelte';
-  import { formatEther, hexToBigInt, isHex } from 'viem';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
+	import { formatEther, hexToBigInt, isHex } from 'viem';
 
-  export let table;
+	export let table;
 </script>
 
 <Table divClass="cursor-pointer rounded-lg overflow-hidden dark:border-none border">
-  <TableHead>
-    <TableHeadCell>Stack item</TableHeadCell>
-    <TableHeadCell>Value</TableHeadCell>
-    <TableHeadCell>Hex</TableHeadCell>
-  </TableHead>
-  <TableBody>
-    {#each table.rows[0] as value, i}
-      <TableBodyRow>
-        <TableBodyCell data-testid="debug-stack">{table.column_names[i]}</TableBodyCell>
-        <TableBodyCell data-testid="debug-value"
-          >{isHex(value) ? formatEther(hexToBigInt(value)) : ''}</TableBodyCell
-        >
-        <TableBodyCell data-testid="debug-value-hex">{value}</TableBodyCell>
-      </TableBodyRow>
-    {/each}
-  </TableBody>
+	<TableHead>
+		<TableHeadCell>Stack item</TableHeadCell>
+		<TableHeadCell>Value</TableHeadCell>
+		<TableHeadCell>Hex</TableHeadCell>
+	</TableHead>
+	<TableBody>
+		{#each table.rows[0] as value, i}
+			<TableBodyRow>
+				<TableBodyCell data-testid="debug-stack">{table.column_names[i]}</TableBodyCell>
+				<TableBodyCell data-testid="debug-value"
+					>{isHex(value) ? formatEther(hexToBigInt(value)) : ''}</TableBodyCell
+				>
+				<TableBodyCell data-testid="debug-value-hex">{value}</TableBodyCell>
+			</TableBodyRow>
+		{/each}
+	</TableBody>
 </Table>
