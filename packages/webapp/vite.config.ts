ac175c79fbca1924e31a482f9a903dc69a570637
Jamie Harding <jharding142@icloud.com>
2025-01-13 14:32:36 +0100

    vite config

--- packages/webapp/vite.config.ts
+++ packages/webapp/vite.config.ts
@@ -3,7 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	define: {
+		'process.env': {}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
