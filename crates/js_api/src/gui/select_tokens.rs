97e22a26d5dc1a55a48cb9f5e79eed5abebeb598
findolor <anakisci@gmail.com>
2025-01-13 14:53:54 +0100

    refator select tokens functionality

--- crates/js_api/src/gui/select_tokens.rs
+++ crates/js_api/src/gui/select_tokens.rs
@@ -1,56 +1,52 @@
-use std::str::FromStr;
-
 use super::*;
-
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Tsify)]
-pub struct SelectTokens(#[tsify(type = "Map<string, string>")] BTreeMap<String, Address>);
-impl_all_wasm_traits!(SelectTokens);
+use rain_orderbook_app_settings::token::Token;
+use std::str::FromStr;
 
 #[wasm_bindgen]
 impl DotrainOrderGui {
-    pub fn check_token_addresses(&self) -> Result<(), GuiError> {
-        if let Some(select_tokens) = &self.select_tokens {
-            for (token, address) in select_tokens.iter() {
-                if address == &Address::ZERO {
-                    return Err(GuiError::TokenMustBeSelected(token.clone()));
-                }
+    #[wasm_bindgen(js_name = "getSelectTokens")]
+    pub fn get_select_tokens(&self) -> Result<Vec<String>, GuiError> {
+        let deployment = self.get_current_deployment()?;
+        Ok(deployment.select_tokens.unwrap_or(vec![]))
+    }
+
+    #[wasm_bindgen(js_name = "isSelectTokenSet")]
+    pub fn is_select_token_set(&self, key: String) -> Result<bool, GuiError> {
+        Ok(self.dotrain_order.orderbook_yaml().get_token(&key).is_ok())
+    }
+
+    #[wasm_bindgen(js_name = "checkSelectTokens")]
+    pub fn check_select_tokens(&self) -> Result<(), GuiError> {
+        let deployment = self.get_current_deployment()?;
+
+        if let Some(select_tokens) = deployment.select_tokens {
+            for key in select_tokens {
+                self.dotrain_order
+                    .orderbook_yaml()
+                    .get_token(&key)
+                    .map_err(|_| GuiError::SelectTokensNotSet)?;
             }
         }
+
         Ok(())
     }
 
-    /// Get all selected tokens and their addresses
-    ///
-    /// Returns a map of token key to address
-    #[wasm_bindgen(js_name = "getSelectTokens")]
-    pub fn get_select_tokens(&self) -> Result<SelectTokens, GuiError> {
-        let gui = self.get_current_deployment()?;
-
-        if gui.select_tokens.is_none() || self.select_tokens.is_none() {
-            return Ok(SelectTokens(BTreeMap::new()));
-        }
-
-        Ok(SelectTokens(self.select_tokens.clone().unwrap()))
-    }
-
-    #[wasm_bindgen(js_name = "saveSelectTokenAddress")]
-    pub async fn save_select_token_address(
+    #[wasm_bindgen(js_name = "saveSelectToken")]
+    pub async fn save_select_token(
         &mut self,
         key: String,
         address: String,
     ) -> Result<(), GuiError> {
         let deployment = self.get_current_deployment()?;
-        let mut select_tokens = self
-            .select_tokens
-            .clone()
-            .ok_or(GuiError::SelectTokensNotSet)?;
-        if !select_tokens.contains_key(&key) {
+        if deployment.select_tokens.is_none() {
+            return Err(GuiError::SelectTokensNotSet);
+        }
+        let select_tokens = deployment.select_tokens.unwrap();
+        if !select_tokens.contains(&key) {
             return Err(GuiError::TokenNotFound(key.clone()));
         }
 
         let address = Address::from_str(&address)?;
-        select_tokens.insert(key.clone(), address);
-        self.select_tokens = Some(select_tokens);
 
         let rpc_url = deployment
             .deployment
@@ -63,23 +59,31 @@ impl DotrainOrderGui {
             .clone();
         let erc20 = ERC20::new(rpc_url.clone(), address);
         let token_info = erc20.token_info(None).await?;
-        self.onchain_token_info.insert(address, token_info);
 
-        self.dotrain_order
-            .orderbook_yaml()
-            .get_token(&key)?
-            .update_address(&address.to_string())?;
+        Token::add_record_to_yaml(
+            self.dotrain_order.orderbook_yaml().documents,
+            &key,
+            &deployment.deployment.scenario.deployer.network.key,
+            &address.to_string(),
+            Some(&token_info.decimals.to_string()),
+            Some(&token_info.name),
+            Some(&token_info.symbol),
+        )?;
         Ok(())
     }
 
-    #[wasm_bindgen(js_name = "clearSelectTokenAddress")]
-    pub fn clear_select_token_address(&mut self, key: String) -> Result<(), GuiError> {
-        let mut select_tokens = self
-            .select_tokens
-            .clone()
-            .ok_or(GuiError::SelectTokensNotSet)?;
-        select_tokens.insert(key.clone(), Address::ZERO);
-        self.select_tokens = Some(select_tokens);
+    #[wasm_bindgen(js_name = "removeSelectToken")]
+    pub fn remove_select_token(&mut self, key: String) -> Result<(), GuiError> {
+        let deployment = self.get_current_deployment()?;
+        if deployment.select_tokens.is_none() {
+            return Err(GuiError::SelectTokensNotSet);
+        }
+        let select_tokens = deployment.select_tokens.unwrap();
+        if !select_tokens.contains(&key) {
+            return Err(GuiError::TokenNotFound(key.clone()));
+        }
+
+        Token::remove_record_from_yaml(self.dotrain_order.orderbook_yaml().documents, &key)?;
         Ok(())
     }
 }
