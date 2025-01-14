13da99f508b2c0849eec22dacb42ad7e8640963e
Jamie Harding <jharding142@icloud.com>
2025-01-14 07:53:50 +0100

    rm console

--- packages/webapp/src/routes/+page.svelte
+++ packages/webapp/src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { Homepage } from '@rainlanguage/ui-components';
 	import { colorTheme } from '$lib/darkMode';
-
-	$: console.log($colorTheme);
 </script>
 
 <Homepage {colorTheme} />
