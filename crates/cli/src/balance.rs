1f2c48e3b4d3e14d0a6b0c195130d54615faa939
nino <Ninokeldishvili@gmail.com>
2025-01-20 15:08:21 +0400

    Test minor updates

--- crates/cli/src/balance.rs
+++ crates/cli/src/balance.rs
@@ -24,14 +24,18 @@ pub async fn get_balances_single_order(subgraph_client: &OrderbookSubgraphClient
 mod tests {
     use super::*;
     use rain_orderbook_subgraph_client::OrderbookSubgraphClient;
+    use reqwest::Url;
 
     #[tokio::test]
     async fn test_get_balances_single_order() {
         let subgraph_url = std::env::var("ORDERBOOK_SUBGRAPH_URL")
             .expect("Environment variable ORDERBOOK_SUBGRAPH_URL must be set.");
+
+        let subgraph_url = Url::parse(&subgraph_url).expect("Invalid URL format.");
+
         let subgraph_client = OrderbookSubgraphClient::new(subgraph_url);
 
-        let order_id = "0x12863c37d7dd314984b237619f569f6f6f645383bb39aec4cb219abd52f8eff2"; // Example order_id
+        let order_id: String = "0x12863c37d7dd314984b237619f569f6f6f645383bb39aec4cb219abd52f8eff2".to_string();
 
         // Call the function to get balances
         let result = get_balances_single_order(&subgraph_client, order_id).await;
