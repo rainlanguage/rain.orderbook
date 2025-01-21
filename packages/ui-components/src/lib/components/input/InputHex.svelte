96555e8219bb862bfae597cbd1fa3c7efed529fb
nino <Ninokeldishvili@gmail.com>
2025-01-21 12:46:38 +0400

    Remove console

--- packages/ui-components/src/lib/components/input/InputHex.svelte
+++ packages/ui-components/src/lib/components/input/InputHex.svelte
@@ -33,9 +33,7 @@
 			try {
 				value = fromHex(valuePrefixed as `0x${string}`, 'bigint');
 				// eslint-disable-next-line no-empty
-			} catch (e) {
-				console.error(e);
-			}
+			} catch (e) {}
 		}
 	}
 </script>
