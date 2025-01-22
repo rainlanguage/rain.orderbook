7eb82b162d6f0908c025f7991cf4270f5d388090
Jamie Harding <jharding142@icloud.com>
2025-01-22 14:47:20 +0100

    add

--- packages/webapp/src/routes/deploy/+page.svelte
+++ packages/webapp/src/routes/deploy/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import { DeploymentSteps } from '@rainlanguage/ui-components';
+	import { page } from '$app/stores';
+	const { files } = $page.data;
+	$: console.log(files);
 </script>
 
 <div class="flex flex-col justify-center pb-12">
---
+++ packages/webapp/src/routes/deploy/+page.ts
@@ -0,0 +1,16 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+	try {
+		const response = await fetch(`https://raw.githubusercontent.com/rainlanguage/rain.strategies/refs/heads/main/strategies/dev/registry.json`);
+		const data = await response.json();
+
+		return {
+			files: data
+		};
+	} catch {
+		return {
+			files: []
+		};
+	}
+};
