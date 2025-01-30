f9d31e0b6454c0f98810c73169403a6adaaea259
nino <Ninokeldishvili@gmail.com>
2025-01-30 16:56:38 +0400

    Add load strategy button

--- packages/webapp/src/routes/deploy/+page.svelte
+++ packages/webapp/src/routes/deploy/+page.svelte
@@ -7,7 +7,7 @@
 	let _registryUrl = '';
 	let _files = files;
 	let _dotrain = '';
-
+	let inputDotrain = '';
 	let error = '';
 	let errorDetails = '';
 	let loading = false;
@@ -32,22 +32,36 @@
 		loading = false;
 		return;
 	};
+
+	const loadStrategy = () => {
+		_dotrain = inputDotrain;
+	};
 </script>
 
 <div class="flex w-full flex-col">
 	<div class="flex items-start justify-end gap-4">
 		{#if advancedMode}
-			<div class="mb-12 flex w-2/3 items-start gap-4">
-				<div class="flex w-full flex-col gap-4">
+			<div class="mb-12 flex w-2/3 flex-col items-start gap-4">
+				<div class="flex w-full items-start gap-4">
 					<Input
 						id="strategy-url"
 						type="url"
 						placeholder="Enter URL to raw strategy registry file"
 						bind:value={_registryUrl}
 					/>
-					<Textarea id="textarea-id" placeholder="Raw strategy" rows="8" bind:value={_dotrain} />
+					<Button class="text-nowrap" on:click={() => getFileRegistry(_registryUrl)}
+						>Load URL</Button
+					>
 				</div>
-				<Button on:click={() => getFileRegistry(_registryUrl)}>Load</Button>
+				<div class="flex w-full items-start gap-4">
+					<Textarea
+						id="textarea-id"
+						placeholder="Raw strategy"
+						rows="8"
+						bind:value={inputDotrain}
+					/>
+					<Button class="text-nowrap" on:click={loadStrategy}>Load Strategy</Button>
+				</div>
 			</div>
 		{/if}
 		<Toggle on:change={() => (advancedMode = !advancedMode)}>
@@ -60,7 +74,13 @@
 	{:else if error}
 		<p>{error}</p>
 		<p>{errorDetails}</p>
-	{:else if _files.length > 0}
+	{:else if _dotrain}
+		<div class="mb-36">
+			<StrategySection rawDotrain={_dotrain} />
+		</div>
+	{/if}
+
+	{#if _files.length > 0}
 		<div class="flex flex-col gap-36">
 			{#each _files as { name, url }}
 				<StrategySection strategyUrl={url} strategyName={name} />
