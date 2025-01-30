dcabbd84f300c194261475ce10bccaea0fc7dbf6
nino <Ninokeldishvili@gmail.com>
2025-01-30 13:57:42 +0400

    Add textbox for raw strat

--- packages/webapp/src/routes/deploy/+page.svelte
+++ packages/webapp/src/routes/deploy/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { StrategySection } from '@rainlanguage/ui-components';
 	import { page } from '$app/stores';
-	import { Button, Input, Spinner, Toggle } from 'flowbite-svelte';
+	import { Button, Input, Spinner, Toggle, Textarea } from 'flowbite-svelte';
 	const { files } = $page.data;
 
 	let _registryUrl = '';
 	let _files = files;
+	let _dotrain = '';
 
 	let error = '';
 	let errorDetails = '';
@@ -33,17 +34,21 @@
 	};
 </script>
 
-<div class="flex flex-col">
-	<div class="flex h-12 w-full items-center justify-end gap-4">
+<div class="flex w-full flex-col">
+	<div class="flex items-start justify-end gap-4">
 		{#if advancedMode}
-			<Input
-				id="strategy-url"
-				type="url"
-				placeholder="Enter URL to raw strategy registry file"
-				bind:value={_registryUrl}
-				class="max-w-lg"
-			/>
-			<Button on:click={() => getFileRegistry(_registryUrl)}>Load</Button>
+			<div class="mb-12 flex w-2/3 items-start gap-4">
+				<div class="flex w-full flex-col gap-4">
+					<Input
+						id="strategy-url"
+						type="url"
+						placeholder="Enter URL to raw strategy registry file"
+						bind:value={_registryUrl}
+					/>
+					<Textarea id="textarea-id" placeholder="Raw strategy" rows="8" bind:value={_dotrain} />
+				</div>
+				<Button on:click={() => getFileRegistry(_registryUrl)}>Load</Button>
+			</div>
 		{/if}
 		<Toggle on:change={() => (advancedMode = !advancedMode)}>
 			{'Advanced Mode'}
