b9dbacaf46301193ee78e119a87955a0af6096b2
nino <Ninokeldishvili@gmail.com>
2025-02-07 16:45:06 +0400

    Add test for CheckboxMyItemsOnly

---
+++ packages/ui-components/src/__tests__/CheckboxMyItemsOnly.test.ts
@@ -0,0 +1,32 @@
+import { render, fireEvent, screen } from '@testing-library/svelte';
+import { get, writable, type Writable } from 'svelte/store';
+import { beforeEach, expect, test, describe } from 'vitest';
+import CheckboxMyItemsOnly from '../lib/components/CheckboxMyItemsOnly.svelte';
+
+describe('CheckboxMyItemsOnly', () => {
+	let showMyItemsOnly: Writable<boolean>;
+	let context: 'orders' | 'vaults';
+	beforeEach(() => {
+		showMyItemsOnly = writable(true);
+		context = 'orders';
+	});
+
+	test('renders correctly on orders page', () => {
+		render(CheckboxMyItemsOnly, {
+			props: {
+				showMyItemsOnly,
+				context
+			}
+		});
+		expect(screen.getByText('Only show my orders')).toBeInTheDocument();
+	});
+	test('renders correctly on vaults page', () => {
+		render(CheckboxMyItemsOnly, {
+			props: {
+				showMyItemsOnly,
+				context: 'vaults'
+			}
+		});
+		expect(screen.getByText('Only show my vaults')).toBeInTheDocument();
+	});
+});
