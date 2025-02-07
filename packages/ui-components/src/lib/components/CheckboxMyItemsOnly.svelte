4241bffef1041f2cad0eff4692e9728f4df419ff
nino <Ninokeldishvili@gmail.com>
2025-02-07 11:55:33 +0400

    Create CheckboxMyItemsOnly.svelte

---
+++ packages/ui-components/src/lib/components/CheckboxMyItemsOnly.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Checkbox, Label } from 'flowbite-svelte';
+	import type { Writable } from 'svelte/store';
+
+	export let showMyItems: Writable<boolean>;
+	export let context: 'orders' | 'vaults';
+
+	function handleShowMyItemsChange() {
+		$showMyItems = !$showMyItems;
+	}
+</script>
+
+<div data-testid="show-my-items-checkbox" class="flex items-center gap-x-2">
+	<Label
+		for="show-my-items"
+		class="cursor-pointer whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-300"
+	>
+		Only show my {context}
+	</Label>
+	<Checkbox id="show-my-items" checked={$showMyItems} on:change={handleShowMyItemsChange} />
+</div>
