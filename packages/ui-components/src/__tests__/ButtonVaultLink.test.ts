1920520f26f6ee6f29c27b98f37ec8a8d20d87dc
Jamie Harding <jharding142@icloud.com>
2025-02-10 13:00:36 +0100

    format test

--- packages/ui-components/src/__tests__/ButtonVaultLink.test.ts
+++ packages/ui-components/src/__tests__/ButtonVaultLink.test.ts
@@ -4,28 +4,28 @@ import ButtonVaultLink from '../lib/components/ButtonVaultLink.svelte';
 import type { Vault } from '@rainlanguage/orderbook/js_api';
 
 describe('ButtonVaultLink', () => {
-    const mockVault = {
-        id: '123',
-        vaultId: '1000',
-        balance: '1000000000000000000',
-        token: {
-            name: 'Test Token',
-            symbol: 'TEST',
-            decimals: '18'
-        }
-    } as unknown as Vault;
+	const mockVault = {
+		id: '123',
+		vaultId: '1000',
+		balance: '1000000000000000000',
+		token: {
+			name: 'Test Token',
+			symbol: 'TEST',
+			decimals: '18'
+		}
+	} as unknown as Vault;
 
-    it('should render vault information correctly', () => {
-        render(ButtonVaultLink, {
-            props: {
-                tokenVault: mockVault,
-                subgraphName: 'test'
-            }
-        });
+	it('should render vault information correctly', () => {
+		render(ButtonVaultLink, {
+			props: {
+				tokenVault: mockVault,
+				subgraphName: 'test'
+			}
+		});
 
-        const vaultLink = screen.getByTestId('vault-link');
-        expect(vaultLink).toBeTruthy();
-        expect(vaultLink).toHaveTextContent('Test Token');
-        expect(vaultLink).toHaveTextContent('TEST');
-    });
+		const vaultLink = screen.getByTestId('vault-link');
+		expect(vaultLink).toBeTruthy();
+		expect(vaultLink).toHaveTextContent('Test Token');
+		expect(vaultLink).toHaveTextContent('TEST');
+	});
 });
