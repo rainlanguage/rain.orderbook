ec3d7aab8153b3ef1f263b29a59e540f6d476fa0
findolor <anakisci@gmail.com>
2025-02-10 15:56:28 +0100

    update registry link

--- packages/webapp/src/lib/stores/registry.ts
+++ packages/webapp/src/lib/stores/registry.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const registryUrl = writable<string>(
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/refs/heads/main/strategies/dev/registry'
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/74096cc20b6ff6ca907d3591658b47ca279f4637/ports/registry'
 );
