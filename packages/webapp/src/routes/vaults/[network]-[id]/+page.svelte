391df8e2a078a5883cc4d6d16464a14d69b82537
Jamie Harding <jharding142@icloud.com>
2025-02-13 11:10:21 +0100

    invalidate queries on tx success

--- packages/webapp/src/routes/vaults/[network]-[id]/+page.svelte
+++ packages/webapp/src/routes/vaults/[network]-[id]/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-	import { PageHeader } from '@rainlanguage/ui-components';
+	import { PageHeader, TransactionStatus, transactionStore } from '@rainlanguage/ui-components';
 	import { page } from '$app/stores';
 	import { VaultDetail } from '@rainlanguage/ui-components';
 	import { wagmiConfig, signerAddress } from '$lib/stores/wagmi';
 	import { handleDepositOrWithdrawModal } from '$lib/services/modal';
+	import { useQueryClient } from '@tanstack/svelte-query';
+	const queryClient = useQueryClient();
+
 	const { settings, activeOrderbookRef, activeNetworkRef, lightweightChartsTheme } =
 		$page.data.stores;
+
+	$: if ($transactionStore.status === TransactionStatus.SUCCESS) {
+		console.log('invalidating vault detail', $page.params.id);
+		queryClient.invalidateQueries({
+			queryKey: [$page.params.id]
+		});
+	}
 </script>
 
 <PageHeader title="Vault" pathname={$page.url.pathname} />
