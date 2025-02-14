5f34657a40c721dd996004a49f1bcf0cbe3c298e
Jamie Harding <jharding142@icloud.com>
2025-02-14 20:57:02 +0100

    rm only

--- packages/ui-components/src/__tests__/transactionStore.test.ts
+++ packages/ui-components/src/__tests__/transactionStore.test.ts
@@ -229,7 +229,7 @@ describe('transactionStore', () => {
 		expect(get(transactionStore).status).toBe(TransactionStatus.PENDING_SUBGRAPH);
 	});
 
-	it.only('should handle waiting for subgraph indexing', async () => {
+	it('should handle waiting for subgraph indexing', async () => {
 		const mockSubgraphUrl = 'test.com';
 		const mockTxHash = 'mockHash';
 		const mockSuccessMessage = 'Success! Transaction confirmed';
