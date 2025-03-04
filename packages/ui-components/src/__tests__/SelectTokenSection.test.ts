ae17356359e1f6fc21ddeabec9757a075c7625c1
Jamie Harding <jharding142@icloud.com>
2025-03-04 14:45:17 +0100

    format

--- packages/ui-components/src/__tests__/SelectTokenSection.test.ts
+++ packages/ui-components/src/__tests__/SelectTokenSection.test.ts
@@ -5,42 +5,41 @@ import { getViemChain } from '../lib/services/getViemChain';
 import type { ComponentProps } from 'svelte';
 import type { Chain } from 'viem/chains';
 
-type SelectTokenSectionProps = ComponentProps<SelectTokensSection> 
+type SelectTokenSectionProps = ComponentProps<SelectTokensSection>;
 
 vi.mock('../lib/services/getViemChain', () => ({
-    getViemChain: vi.fn().mockReturnValue({ id: 1 } as Chain)
+	getViemChain: vi.fn().mockReturnValue({ id: 1 } as Chain)
 }));
 
 describe('SelectTokensSection', () => {
-    const mockGui = {};
-    const mockSelectTokens = [{ id: 1 }, { id: 2 }];
-    const mockOnSelectTokenSelect = vi.fn();
-    const mockNetworkKey = 'mainnet';
+	const mockGui = {};
+	const mockSelectTokens = [{ id: 1 }, { id: 2 }];
+	const mockOnSelectTokenSelect = vi.fn();
+	const mockNetworkKey = 'mainnet';
 
+	const defaultProps: SelectTokenSectionProps = {
+		gui: mockGui,
+		selectTokens: mockSelectTokens,
+		onSelectTokenSelect: mockOnSelectTokenSelect,
+		tokenList: [{ chainId: 1, address: '0x1', decimals: 18, name: 'test', symbol: 'TEST' }],
+		networkKey: mockNetworkKey
+	} as unknown as SelectTokenSectionProps;
 
-    const defaultProps: SelectTokenSectionProps = {
-        gui: mockGui,
-        selectTokens: mockSelectTokens,
-        onSelectTokenSelect: mockOnSelectTokenSelect,
-        tokenList: [{chainId: 1, address: '0x1', decimals: 18, name: 'test', symbol: 'TEST'}],
-        networkKey: mockNetworkKey
-    } as unknown as SelectTokenSectionProps;
+	beforeEach(() => {
+		vi.clearAllMocks();
+	});
 
-    beforeEach(() => {
-        vi.clearAllMocks();
-    });
+	it('should make a call to getViem get the chain for a given chainId if there are tokens in the list', () => {
+		render(SelectTokensSection, {
+			props: defaultProps
+		});
+		expect(getViemChain).toHaveBeenCalledWith(mockNetworkKey);
+	});
 
-    it('should make a call to getViem get the chain for a given chainId if there are tokens in the list', () => {
-        render(SelectTokensSection, {
-            props: defaultProps
-        });
-        expect(getViemChain).toHaveBeenCalledWith(mockNetworkKey);
-    });
-
-    it('should not make a call to getViem get the chain for a given chainId if there are no tokens in the list', () => {
-        render(SelectTokensSection, {
-            props: { ...defaultProps, tokenList: [] }
-        });
-        expect(getViemChain).not.toHaveBeenCalled();
-    });
+	it('should not make a call to getViem get the chain for a given chainId if there are no tokens in the list', () => {
+		render(SelectTokensSection, {
+			props: { ...defaultProps, tokenList: [] }
+		});
+		expect(getViemChain).not.toHaveBeenCalled();
+	});
 });
--- packages/ui-components/src/__tests__/getViemChain.test.ts
+++ packages/ui-components/src/__tests__/getViemChain.test.ts
@@ -2,13 +2,13 @@ import { getViemChain } from '../lib/services/getViemChain';
 import { describe, it, expect } from 'vitest';
 
 describe('getViemChain', () => {
-    it('should return the chain for a given chainId', () => {
-        const chain = getViemChain('mainnet');
-        expect(chain).toBeDefined();
-    });
+	it('should return the chain for a given chainId', () => {
+		const chain = getViemChain('mainnet');
+		expect(chain).toBeDefined();
+	});
 
-    it('should return the chain for a given chainId', () => {
-        const chain = getViemChain('fakeChainThatDoesNotExist');
-        expect(chain).not.toBeDefined();
-    });
+	it('should return the chain for a given chainId', () => {
+		const chain = getViemChain('fakeChainThatDoesNotExist');
+		expect(chain).not.toBeDefined();
+	});
 });
