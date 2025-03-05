70fc36a91fb834fa4b82c78634dfd9fbadd884a6
thedavidmeister <thedavidmeister@gmail.com>
2025-03-05 12:45:45 +0400

    fix reg

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const REGISTRY_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/eb719a1c480752ce912c7f0c3dd50ddf91c2d8cf/ports/registry';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/eb719a1c480752ce912c7f0c3dd50ddf91c2d8cf/registry';
