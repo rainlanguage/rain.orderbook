8f4d646150dbe8bd70ffdb727c6dd603f0acda2c
rouzwelt <rouzweltt@gmail.com>
2025-03-06 23:23:49 +0000

    Update tests.rs

--- crates/macros/tests/tests.rs
+++ crates/macros/tests/tests.rs
@@ -1,10 +1,14 @@
 #[test]
 pub fn pass() {
+    // to update the tests "*.expanded.rs" delete them
+    // and run cargo test to regenerate them
     macrotest::expand("tests/pass/*.rs");
 }
 
 #[test]
 fn fail() {
+    // to update the tests "*.stderr" set env TRYBUILD=overwrite
+    // and run cargo test
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/fail/*.rs");
 }
