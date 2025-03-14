11f8269579d168dfe6352372a6264c02d946ac71
Jamie Harding <jharding142@icloud.com>
2025-03-14 14:14:31 +0100

    rm setcontext

--- packages/ui-components/src/lib/context/wagmi.ts
+++ packages/ui-components/src/lib/context/wagmi.ts
@@ -1,11 +1,7 @@
 import type { WagmiContext } from '$lib/types/wagmi';
-import { getContext, setContext } from 'svelte';
+import { getContext } from 'svelte';
 
 export const WAGMI_CONTEXT_KEY = 'wagmi-stores';
-
-export function setWagmiContext(stores: WagmiContext) {
-  setContext(WAGMI_CONTEXT_KEY, stores);
-}
 
 export function getWagmiContext() {
   return getContext<WagmiContext>(WAGMI_CONTEXT_KEY);
