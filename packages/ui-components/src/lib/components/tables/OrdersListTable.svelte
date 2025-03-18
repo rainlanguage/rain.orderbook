43b9217c96184a1de946d45109294ef4404aa943
Jamie Harding <jharding142@icloud.com>
2025-03-18 11:44:52 +0100

    orders list

--- packages/ui-components/src/lib/components/tables/OrdersListTable.svelte
+++ packages/ui-components/src/lib/components/tables/OrdersListTable.svelte
@@ -1,6 +1,4 @@
 <script lang="ts" generics="T">
-	import type { Writable } from 'svelte/store';
-
 	import { goto } from '$app/navigation';
 	import { DotsVerticalOutline } from 'flowbite-svelte-icons';
 	import { type SgOrderWithSubgraphName } from '@rainlanguage/orderbook/js_api';
@@ -21,6 +19,7 @@
 		TableBodyCell,
 		TableHeadCell
 	} from 'flowbite-svelte';
+	import type { Writable } from 'svelte/store';
 
 	// Optional props only used in tauri-app
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -38,9 +37,10 @@
 	export let hideZeroBalanceVaults: AppStoresInterface['hideZeroBalanceVaults'];
 	export let showMyItemsOnly: AppStoresInterface['showMyItemsOnly'];
 	export let currentRoute: string;
+	export let signerAddress: Writable<string | null> | undefined;
 	export let activeNetworkRef: AppStoresInterface['activeNetworkRef'];
 	export let activeOrderbookRef: AppStoresInterface['activeOrderbookRef'];
-	export let signerAddress: Writable<string | null> | undefined = undefined;
+
 	$: multiSubgraphArgs = Object.entries(
 		Object.keys($activeSubgraphs ?? {}).length ? $activeSubgraphs : ($settings?.subgraphs ?? {})
 	).map(([name, url]) => ({
