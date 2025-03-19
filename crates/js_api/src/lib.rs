323445947595b9dff11a9e5e9c0a277cd57d5522
findolor <anakisci@gmail.com>
2025-03-19 09:26:15 +0300

    change settings naming to be orderbook yaml

--- crates/js_api/src/lib.rs
+++ crates/js_api/src/lib.rs
@@ -1,6 +1,6 @@
 #[cfg(target_family = "wasm")]
 pub mod gui;
 #[cfg(target_family = "wasm")]
-pub mod settings;
-#[cfg(target_family = "wasm")]
 pub mod subgraph;
+#[cfg(target_family = "wasm")]
+pub mod yaml;
