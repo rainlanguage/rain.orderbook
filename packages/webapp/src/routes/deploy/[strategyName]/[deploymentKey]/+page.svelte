07fb1ba4d87a2f2a7b79559eb647d6660418a2ad
Jamie Harding <jharding142@icloud.com>
2025-03-21 23:53:08 +0100

    rm unused

--- packages/webapp/src/routes/deploy/[strategyName]/[deploymentKey]/+page.svelte
+++ packages/webapp/src/routes/deploy/[strategyName]/[deploymentKey]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
-	import { DeploymentSteps, PageHeader, useAccount } from '@rainlanguage/ui-components';
+	import { DeploymentSteps, PageHeader } from '@rainlanguage/ui-components';
 	import { wagmiConfig, connected, appKitModal } from '$lib/stores/wagmi';
 	import { handleDeployModal, handleDisclaimerModal } from '$lib/services/modal';
 	import { DotrainOrderGui } from '@rainlanguage/orderbook/js_api';
