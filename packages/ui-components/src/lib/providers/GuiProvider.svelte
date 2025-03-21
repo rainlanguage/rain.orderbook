3b7fced139c2969a4b3f2c8cf11261c6cc35ad2f
Jamie Harding <jharding142@icloud.com>
2025-03-21 16:53:03 +0100

    Update packages/ui-components/src/lib/providers/GuiProvider.svelte
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/ui-components/src/lib/providers/GuiProvider.svelte
+++ packages/ui-components/src/lib/providers/GuiProvider.svelte
@@ -1,12 +1,6 @@
-<script lang="ts">
-	import { setContext } from 'svelte';
-	import { DotrainOrderGui } from '@rainlanguage/orderbook/js_api';
+import { getContext } from 'svelte';
+import { DotrainOrderGui } from '@rainlanguage/orderbook/js_api';
 
-	export let gui: DotrainOrderGui;
-
-	const GUI_CONTEXT_KEY = 'gui-context';
-
-	setContext(GUI_CONTEXT_KEY, gui);
-</script>
+export const GUI_CONTEXT_KEY = 'gui-context';
 
 <slot />
