7cadfa89d669dea2b9f1aa19169231b991bafc54
rouzwelt <rouzweltt@gmail.com>
2025-03-23 17:47:55 +0000

    typo

--- crates/js_api/src/lib.rs
+++ crates/js_api/src/lib.rs
@@ -3,7 +3,7 @@ pub mod gui;
 #[cfg(target_family = "wasm")]
 pub mod subgraph;
 
-// re-export other crates to inculde their wasm bindings as single export point
+// re-export other crates to include their wasm bindings as single export point
 #[cfg(target_family = "wasm")]
 pub use rain_orderbook_app_settings;
 #[cfg(target_family = "wasm")]
