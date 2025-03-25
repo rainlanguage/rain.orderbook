392172368299e3774928d4bbad38c08acd254b6d
Jamie Harding <jharding142@icloud.com>
2025-03-25 16:55:31 +0100

    show no strategies found error

--- packages/webapp/src/routes/deploy/+page.svelte
+++ packages/webapp/src/routes/deploy/+page.svelte
@@ -13,7 +13,7 @@
 			Error loading registry:<span class="text-red-500">{error}</span>
 		</div>
 	{:else}
-		<div class="flex flex-col rounded-3xl bg-primary-100 p-12 dark:bg-primary-900">
+		<div class="bg-primary-100 dark:bg-primary-900 flex flex-col rounded-3xl p-12">
 			<h1 class="text-xl font-semibold text-gray-900 dark:text-white">
 				Raindex empowers you to take full control of your trading strategies. All the strategies
 				here are non-custodial, perpetual, and automated strategies built with our open-source,
@@ -22,11 +22,15 @@
 				>
 			</h1>
 		</div>
-		{#if validStrategies.length > 0}
-			<ValidStrategiesSection strategies={validStrategies} />
+		{#if validStrategies.length === 0 && invalidStrategies.length === 0}
+			<div class="text-center text-lg">No strategies found</div>
+		{:else}
+			{#if validStrategies.length > 0}
+				<ValidStrategiesSection strategies={validStrategies} />
+			{/if}
+			{#if invalidStrategies.length > 0}
+				<InvalidStrategiesSection strategiesWithErrors={invalidStrategies} />
+			{/if}
 		{/if}
-		{#if invalidStrategies.length > 0}
-			<InvalidStrategiesSection strategiesWithErrors={invalidStrategies} />
-		{/if}
-	{/if}
+	</div>
 </div>
