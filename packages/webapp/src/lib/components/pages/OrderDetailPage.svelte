31aeda12bd6cb0a5cd788337ad26d3716fac5573
Jamie Harding <jharding142@icloud.com>
2025-03-26 19:21:44 +0100

    delete

--- packages/webapp/src/lib/components/pages/OrderDetailPage.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { OrderDetail } from '@rainlanguage/ui-components';
-</script>
-
-<OrderDetail />
-
-<!-- TODO: Wrap OrderDetail with the modal handling here -->
-<!-- No optional props -->
