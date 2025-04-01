ff84611c3e665398692134fd4acc97d8c5486e7f
Jamie Harding <jharding142@icloud.com>
2025-04-01 13:53:35 +0200

    walletconnect prop

--- packages/ui-components/src/__tests__/WalletConnect.test.ts
+++ packages/ui-components/src/__tests__/WalletConnect.test.ts
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/svelte';
 import WalletConnect from '../lib/components/wallet/WalletConnect.svelte';
 import { describe, it, vi, beforeEach, expect } from 'vitest';
-import { writable, type Writable } from 'svelte/store';
+import { readable, writable, type Writable } from 'svelte/store';
 import type { AppKit } from '@reown/appkit';
 import truncateEthAddress from 'truncate-eth-address';
-const { mockSignerAddressStore, mockConnectedStore } = await vi.hoisted(
+import type { Hex } from 'viem';
+const {mockConnectedStore } = await vi.hoisted(
 	() => import('$lib/__mocks__/stores')
 );
 
@@ -14,7 +15,6 @@ vi.mock('$lib/stores/wagmi', async (importOriginal) => {
 		...original,
 		appKitModal: writable({} as AppKit),
 		connected: mockConnectedStore,
-		signerAddress: mockSignerAddressStore
 	};
 });
 
@@ -25,7 +25,6 @@ describe('WalletConnect component', () => {
 	});
 
 	it('displays "Connect" with red icon when wallet is not connected or wrong network', () => {
-		mockSignerAddressStore.mockSetSubscribeValue('');
 		mockConnectedStore.mockSetSubscribeValue(false);
 
 		render(WalletConnect);
@@ -35,14 +34,13 @@ describe('WalletConnect component', () => {
 	});
 
 	it('displays truncated address when wallet is connected', () => {
-		mockSignerAddressStore.mockSetSubscribeValue('0x123');
 		mockConnectedStore.mockSetSubscribeValue(true);
 
 		render(WalletConnect, {
 			props: {
 				connected: mockConnectedStore as Writable<boolean>,
 				appKitModal: writable({} as AppKit),
-				signerAddress: mockSignerAddressStore
+				signerAddress: readable('0x123' as Hex)
 			}
 		});
 
