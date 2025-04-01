cfd5097ca65c64392b9b20a368ac4daf51a8781d
highonhopium <jks.hardy@gmail.com>
2025-04-01 14:36:08 +0100

    fixes

--- packages/ui-components/src/lib/components/detail/OrderDetail.svelte
+++ packages/ui-components/src/lib/components/detail/OrderDetail.svelte
@@ -30,6 +30,7 @@
 	import Refresh from '../icon/Refresh.svelte';
 	import { invalidateIdQuery } from '$lib/queries/queryClient';
 	import { InfoCircleOutline } from 'flowbite-svelte-icons';
+	import { isAddressEqual, isAddress } from 'viem';
 
 	export let handleDepositOrWithdrawModal:
 		| ((props: DepositOrWithdrawModalProps) => void)
@@ -87,7 +88,7 @@
 			</div>
 
 			<div class="flex items-center gap-2">
-				{#if data && $signerAddress === data.order.owner && data.order.active && handleOrderRemoveModal && $wagmiConfig && chainId && orderbookAddress}
+				{#if data && $signerAddress && isAddress($signerAddress) && isAddressEqual($signerAddress, data.order.owner) && data.order.active && handleOrderRemoveModal && $wagmiConfig && chainId && orderbookAddress}
 					<Button
 						data-testid="remove-button"
 						color="dark"
@@ -154,7 +155,7 @@
 								{#each data.vaults.get(type) || [] as vault}
 									<ButtonVaultLink tokenVault={vault} {subgraphName}>
 										<svelte:fragment slot="buttons">
-											{#if handleDepositOrWithdrawModal && $signerAddress === vault.owner && chainId}
+											{#if handleDepositOrWithdrawModal && $signerAddress && isAddress($signerAddress) && isAddressEqual($signerAddress, vault.owner) && chainId}
 												<DepositOrWithdrawButtons
 													{vault}
 													{chainId}
