04b3af208e7ad395982df108f6c3f6b6648f96e1
Jamie Harding <jharding142@icloud.com>
2025-04-01 19:48:19 +0200

    Update buttonVaultLink tests to test address matching correctly

--- packages/ui-components/src/__tests__/ButtonVaultLinkWrapper.test.svelte
+++ packages/ui-components/src/__tests__/ButtonVaultLinkWrapper.test.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import type { DepositOrWithdrawModalProps } from '$lib/types/modal';
+	import type { CreateQueryResult } from '@tanstack/svelte-query';
 	import ButtonVaultLink from '../lib/components/ButtonVaultLink.svelte';
 	import DepositOrWithdrawButtons from '../lib/components/detail/DepositOrWithdrawButtons.svelte';
 	import type { SgVault } from '@rainlanguage/orderbook/js_api';
@@ -6,12 +8,14 @@
 
 	export let tokenVault: SgVault;
 	export let subgraphName: string;
-	export let handleDepositOrWithdrawModal: any = undefined;
-	export let signerAddress: string = '';
-	export let chainId: number | undefined = undefined;
-	export let rpcUrl: string = '';
-	export let orderDetailQuery: any = undefined;
-	export let subgraphUrl: string = '';
+	export let handleDepositOrWithdrawModal:
+		| ((props: DepositOrWithdrawModalProps) => void)
+		| undefined = undefined;
+	export let signerAddress: string;
+	export let chainId: number;
+	export let rpcUrl: string;
+	export let orderDetailQuery: CreateQueryResult;
+	export let subgraphUrl: string;
 </script>
 
 <ButtonVaultLink {tokenVault} {subgraphName}>
