f9e736b619cbef172d8079b8e2d52f500232455e
Jamie Harding <jharding142@icloud.com>
2025-04-02 14:57:40 +0200

    deduplicate data

--- packages/ui-components/src/lib/components/charts/TanstackLightweightChartLine.svelte
+++ packages/ui-components/src/lib/components/charts/TanstackLightweightChartLine.svelte
@@ -1,30 +1,19 @@
 <script lang="ts" generics="T">
 	import type { Readable } from 'svelte/store';
-
 	import LightweightChart from './LightweightChart.svelte';
 	import type { CreateQueryResult } from '@tanstack/svelte-query';
 	import type { IChartApi, UTCTimestamp } from 'lightweight-charts';
-	import { sortBy } from 'lodash';
+	import { transformAndSortData } from './transformAndSortData';
 
-	// eslint-disable-next-line no-undef
 	export let query: CreateQueryResult<T[]>;
-	// eslint-disable-next-line no-undef
 	export let timeTransform: (data: T) => UTCTimestamp;
-	// eslint-disable-next-line no-undef
 	export let valueTransform: (data: T) => number;
 	export let lightweightChartsTheme: Readable<Record<string, unknown>>;
 
-	// eslint-disable-next-line no-undef
-	const transformAndSortData = (data: T[]) => {
-		const transformedData = data.map((d) => ({
-			value: valueTransform(d),
-			time: timeTransform(d)
-		}));
-
-		return sortBy(transformedData, (d) => d.time);
-	};
-
-	$: data = transformAndSortData($query.data ?? []);
+	$: data = transformAndSortData($query.data ?? [], {
+		valueTransform,
+		timeTransform
+	});
 
 	const createSeries = (chart: IChartApi) => chart.addLineSeries({ lineWidth: 1 });
 </script>
