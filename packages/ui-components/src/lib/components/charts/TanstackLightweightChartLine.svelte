34047463319d38c2bc2782ce6e09623086f9104a
Jamie Harding <jharding142@icloud.com>
2025-04-02 14:58:14 +0200

    remove no undef

--- packages/ui-components/src/lib/components/charts/TanstackLightweightChartLine.svelte
+++ packages/ui-components/src/lib/components/charts/TanstackLightweightChartLine.svelte
@@ -5,8 +5,11 @@
 	import type { IChartApi, UTCTimestamp } from 'lightweight-charts';
 	import { transformAndSortData } from './transformAndSortData';
 
+	// eslint-disable-next-line no-undef
 	export let query: CreateQueryResult<T[]>;
+	// eslint-disable-next-line no-undef
 	export let timeTransform: (data: T) => UTCTimestamp;
+	// eslint-disable-next-line no-undef
 	export let valueTransform: (data: T) => number;
 	export let lightweightChartsTheme: Readable<Record<string, unknown>>;
 
