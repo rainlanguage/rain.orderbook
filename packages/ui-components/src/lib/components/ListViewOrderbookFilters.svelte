284ae094e045869556b30ddbb7d5d827aad1c64c
Jamie Harding <jharding142@icloud.com>
2025-04-05 17:15:47 +0200

    format

--- packages/ui-components/src/lib/components/ListViewOrderbookFilters.svelte
+++ packages/ui-components/src/lib/components/ListViewOrderbookFilters.svelte
@@ -39,9 +39,7 @@
 			<div class="mt-4 w-full lg:w-auto" data-testid="my-items-only">
 				<CheckboxMyItemsOnly context={isVaultsPage ? 'vaults' : 'orders'} {showMyItemsOnly} />
 				{#if !$account}
-					<Tooltip class="z-50"
-						>Connect a wallet to filter by {isVaultsPage ? 'vault' : 'order'} owner</Tooltip
-					>
+					<Tooltip>Connect a wallet to filter by {isVaultsPage ? 'vault' : 'order'} owner</Tooltip>
 				{/if}
 			</div>
 		{/if}
