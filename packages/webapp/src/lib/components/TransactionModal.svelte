592ee2bb52571a81974e5eb1fc4586fbf43b2068
findolor <anakisci@gmail.com>
2025-04-10 11:00:44 +0300

    update transaction indexing [skip ci]

--- packages/webapp/src/lib/components/TransactionModal.svelte
+++ packages/webapp/src/lib/components/TransactionModal.svelte
@@ -7,7 +7,7 @@
 	export let open: boolean;
 	export let messages: {
 		success: string;
-		error: string;
+		error?: string;
 		pending: string;
 	};
 
@@ -38,14 +38,14 @@
 				<p
 					class="w-full whitespace-pre-wrap break-words text-center text-lg font-semibold text-gray-900 dark:text-white"
 				>
-					{messages.error}
+					{$transactionStore.status}
 				</p>
 				<p
 					class="w-full whitespace-pre-wrap break-words text-center font-normal text-gray-900 dark:text-white"
 				>
 					{$transactionStore.error}
 				</p>
-				<Button on:click={handleClose} class="mt-4">Dismiss</Button>
+				<Button on:click={handleClose}>Dismiss</Button>
 			{:else if $transactionStore.status === TransactionStatus.SUCCESS}
 				<div
 					class="mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-green-100 dark:bg-green-900"
@@ -80,7 +80,7 @@
 				</div>
 			{:else}
 				<div
-					class="mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-primary-100 dark:bg-primary-900"
+					class="bg-primary-100 dark:bg-primary-900 mb-4 flex h-16 w-16 items-center justify-center rounded-full"
 				>
 					<Spinner color="blue" size={10} />
 				</div>
@@ -94,16 +94,17 @@
 				>
 					{$transactionStore.message}
 				</p>
-				{#if $transactionStore.explorerLink}
-					<p>
-						<a
-							class="cursor-pointer text-blue-500 hover:underline"
-							rel="noopener noreferrer"
-							href={$transactionStore.explorerLink}
-							target="_blank">View transaction on block explorer</a
-						>
-					</p>
-				{/if}
+			{/if}
+			<!-- TODO: Add a test for this -->
+			{#if $transactionStore.explorerLink}
+				<p>
+					<a
+						class="cursor-pointer text-blue-500 hover:underline"
+						rel="noopener noreferrer"
+						href={$transactionStore.explorerLink}
+						target="_blank">View transaction on block explorer</a
+					>
+				</p>
 			{/if}
 		</div>
 	{/if}
