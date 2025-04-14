4265206ec4c2fcd16f76a11f96269fb67fca5212
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-04-14 14:16:06 +0530

    update registry commit to main

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const REGISTRY_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/17363acaa2cc123cbc2b24c2f3597285e16c37ee/registry';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/21df90d7cbd8bb61663253bad32ff3a26599a48b/registry';
