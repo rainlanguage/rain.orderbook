157a963bf25fbd757fffa39486eb36fb81dd32ea
Jamie Harding <jharding142@icloud.com>
2025-04-25 14:26:26 +0200

    Move cachedWritableStore to shared lib, test hideZeroBalanceVaults

--- packages/webapp/src/lib/stores/settings.ts
+++ packages/webapp/src/lib/stores/settings.ts
@@ -1,44 +1,14 @@
-import { cachedWritableStore, type ConfigSource } from '@rainlanguage/ui-components';
-import { derived } from 'svelte/store';
-
-export const settings = cachedWritableStore<ConfigSource | undefined>(
-  'settings',
-  undefined,
-  (value) => JSON.stringify(value),
-  (str) => {
-    try {
-      return JSON.parse(str) as ConfigSource;
-    } catch {
-      return undefined;
-    }
-  },
-);
+import { cachedWritableStore } from '@rainlanguage/ui-components';
 
 export const hideZeroBalanceVaults = cachedWritableStore<boolean>(
-  'settings.hideZeroBalanceVaults',
-  true, // default value is true
-  (value) => JSON.stringify(value),
-  (str) => {
-    try {
-      return JSON.parse(str) as boolean;
-    } catch {
-      return true;
-    }
-  },
-);
-
-export const subgraph = derived(settings, ($settings) =>
-  $settings?.subgraphs !== undefined ? Object.entries($settings.subgraphs) : [],
-);
-export const activeSubgraphs = cachedWritableStore<Record<string, string>>(
-  'settings.activeSubgraphs',
-  {},
-  JSON.stringify,
-  (s) => {
-    try {
-      return JSON.parse(s);
-    } catch {
-      return {};
-    }
-  },
+	'settings.hideZeroBalanceVaults',
+	true, // default value is true
+	(value) => JSON.stringify(value),
+	(str) => {
+		try {
+			return JSON.parse(str) as boolean;
+		} catch {
+			return true;
+		}
+	}
 );
