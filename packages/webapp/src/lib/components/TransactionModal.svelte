a88134d98c89791d45450c9dadcda3d747bd13af
Jamie Harding <jharding142@icloud.com>
2025-05-06 13:10:07 +0200

    Do not reset transaction store when modals are closed

--- packages/webapp/src/lib/components/TransactionModal.svelte
+++ packages/webapp/src/lib/components/TransactionModal.svelte
@@ -19,10 +19,6 @@
 	$: if ($transactionStore.status === TransactionStatus.SUCCESS) {
 		dispatch('success');
 	}
-
-	$: if (!open) {
-		transactionStore.reset();
-	}
 </script>
 
 <Modal size="sm" class="bg-opacity-90 backdrop-blur-sm" bind:open data-testid="transaction-modal">
@@ -80,7 +76,7 @@
 				</div>
 			{:else}
 				<div
-					class="mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-primary-100 dark:bg-primary-900"
+					class="bg-primary-100 dark:bg-primary-900 mb-4 flex h-16 w-16 items-center justify-center rounded-full"
 				>
 					<Spinner color="blue" size={10} />
 				</div>
