b9cb7a8f1650eb0abb5645405bb8ad0dcad17717
findolor <anakisci@gmail.com>
2025-05-06 10:05:44 +0300

    add test

--- crates/js_api/src/subgraph/transaction.rs
+++ crates/js_api/src/subgraph/transaction.rs
@@ -11,3 +11,41 @@ pub async fn get_transaction(url: &str, tx_hash: &str) -> Result<SgTransaction, 
     let client = OrderbookSubgraphClient::new(Url::parse(url)?);
     Ok(client.transaction_detail(Id::new(tx_hash)).await?)
 }
+
+#[cfg(test)]
+mod test_helpers {
+    use super::*;
+
+    #[cfg(not(target_family = "wasm"))]
+    mod non_wasm {
+        use super::*;
+        use httpmock::MockServer;
+        use rain_orderbook_subgraph_client::types::common::{SgBigInt, SgBytes};
+        use serde_json::{json, Value};
+
+        #[tokio::test]
+        async fn test_get_transaction() {
+            let sg_server = MockServer::start_async().await;
+            sg_server.mock(|when, then| {
+                when.path("/sg");
+                then.status(200).json_body_obj(&json!({
+                    "data": {
+                        "transaction": {
+                            "id": "tx1",
+                            "from": "0x1",
+                            "blockNumber": "1",
+                            "timestamp": "1"
+                        }
+                    }
+                }));
+            });
+
+            let url = sg_server.url("/sg");
+            let tx = get_transaction(&url, "hash").await.unwrap();
+            assert_eq!(tx.id.0, "tx1".to_string());
+            assert_eq!(tx.from.0, "0x1".to_string());
+            assert_eq!(tx.block_number.0, "1".to_string());
+            assert_eq!(tx.timestamp.0, "1".to_string());
+        }
+    }
+}
