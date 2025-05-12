67e76d951468c09c49e1c79cc9212a6e9e858eaa
Jamie Harding <jharding142@icloud.com>
2025-05-12 11:30:10 +0200

    reorder index [skip ci]

--- packages/ui-components/src/lib/index.ts
+++ packages/ui-components/src/lib/index.ts
@@ -132,13 +132,14 @@ export { default as logoDark } from './assets/logo-dark.svg';
 // Providers
 export { default as GuiProvider } from './providers/GuiProvider.svelte';
 export { default as WalletProvider } from './providers/wallet/WalletProvider.svelte';
+export { default as RegistryProvider } from './providers/registry/RegistryProvider.svelte';
 export { default as ToastProvider } from './providers/toasts/ToastProvider.svelte';
 
 // Hooks
 export { useGui } from './hooks/useGui';
 export { useAccount } from './providers/wallet/useAccount';
+export { useRegistry } from './providers/registry/useRegistry';
 export { useToasts } from './providers/toasts/useToasts';
-export { default as RegistryProvider } from './providers/registry/RegistryProvider.svelte';
 
 // Classes
 export { RegistryManager } from './providers/registry/RegistryManager';
