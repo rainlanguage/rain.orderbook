da6ea51af406bce24bf0b25c6dc35682ab534852
Jamie Harding <jharding142@icloud.com>
2025-05-15 11:27:39 +0200

    return value

--- packages/ui-components/src/lib/providers/transactions/TransactionManager.ts
+++ packages/ui-components/src/lib/providers/transactions/TransactionManager.ts
@@ -74,6 +74,7 @@ export class TransactionManager {
         removeOrderInstance.execute();
         console.log('TransactionManager: Adding transaction to store');
         this.transactions.update(currentTransactions => [...currentTransactions, removeOrderInstance]); 
+        return removeOrderInstance;
     }
 
     /**
