0b8d48e2c962a8cced092c5111527eeed99e812d
Jamie Harding <jharding142@icloud.com>
2025-05-16 15:52:37 +0200

    Remove reactive props store form mock component

--- packages/webapp/src/routes/+layout.svelte
+++ packages/webapp/src/routes/+layout.svelte
@@ -77,6 +77,7 @@
 						class="flex min-h-screen w-full justify-start bg-white dark:bg-gray-900 dark:text-gray-400"
 					>
 						<Sidebar {colorTheme} page={$page} />
+
 						<main class="mx-auto h-full w-full grow overflow-x-auto px-4 pt-14 lg:ml-64 lg:p-8">
 							<slot />
 						</main>
