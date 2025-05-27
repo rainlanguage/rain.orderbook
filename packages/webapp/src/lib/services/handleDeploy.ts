0628c8596aaefe2a2aa2969e9052dbbdac8c9358
findolor <anakisci@gmail.com>
2025-05-27 14:58:04 +0300

    small change

--- packages/webapp/src/lib/services/handleDeploy.ts
+++ packages/webapp/src/lib/services/handleDeploy.ts
@@ -3,7 +3,7 @@ import { handleDeployModal } from './modal';
 import type { DeploymentArgs } from '@rainlanguage/ui-components';
 import { handleDisclaimerModal } from './modal';
 
-export async function handleDeploy(deploymentArgs: DeploymentArgs) {
+export function handleDeploy(deploymentArgs: DeploymentArgs) {
 	handleDisclaimerModal({
 		open: true,
 		onAccept: () => {
