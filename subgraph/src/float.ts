a04ceff4bfb54c05856062ceb557f3f7f9525d75
0xgleb <gleb.dianov@protonmail.com>
2025-06-23 19:57:48 +0400

    deploy DecimalFloat

--- subgraph/src/float.ts
+++ subgraph/src/float.ts
@@ -12,18 +12,19 @@ function getDecimalFloatAddress(): Address {
   if (network == "flare") {
     return Address.fromString("0x0000000000000000000000000000000000000001");
   } else if (network == "base") {
-    return Address.fromString("0x0000000000000000000000000000000000000002");
+    return Address.fromString("0x83e4c7732e715b5E7310796A4A2a21d89f3FB59A");
   } else if (network == "bsc") {
-    return Address.fromString("0x0000000000000000000000000000000000000003");
+    return Address.fromString("0xDbcb964760d021e18A31C9A731d8589c361E0E20");
   } else if (network == "arbitrum-one") {
     return Address.fromString("0x3ae05C7A18e003299D3db30fB3b2caA67a35a4dE");
   } else if (network == "matic") {
-    return Address.fromString("0x0000000000000000000000000000000000000005");
+    return Address.fromString("0x83e4c7732e715b5E7310796A4A2a21d89f3FB59A");
   } else if (network == "linea") {
-    return Address.fromString("0x0000000000000000000000000000000000000006");
+    return Address.fromString("0x83e4c7732e715b5E7310796A4A2a21d89f3FB59A");
   } else if (network == "mainnet") {
-    return Address.fromString("0x0000000000000000000000000000000000000007");
+    return Address.fromString("0x83e4c7732e715b5E7310796A4A2a21d89f3FB59A");
   }
 
+  // Fallback
   return Address.fromString("0x0000000000000000000000000000000000000001");
 }
