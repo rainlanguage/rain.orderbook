71f78ae6e3614fdc2fecd1292ca613f662b6da3b
findolor <anakisci@gmail.com>
2025-07-11 09:51:37 +0300

    update link

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const REGISTRY_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/888f4875a5f35ac82c7c676ee62a1c5328975808/registry';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/7082c14a48eaa35c49f0d4fdb721c7263f27d661/registry';
