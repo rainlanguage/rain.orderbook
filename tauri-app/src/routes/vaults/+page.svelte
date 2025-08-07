29da0e7270e03dee989b695b0d3a069a837e16e4
brusher_ru <brusher.designer@gmail.com>
2025-08-07 04:52:52 -0300

    refactor: apply CodeRabbit's suggestions

--- tauri-app/src/routes/vaults/+page.svelte
+++ tauri-app/src/routes/vaults/+page.svelte
@@ -2,19 +2,9 @@
   import { PageHeader, VaultsListTable } from '@rainlanguage/ui-components';
   import { page } from '$app/stores';
 
-  import { orderHash, activeAccountsItems, showInactiveOrders } from '$lib/stores/settings';
-
   import { handleDepositModal, handleWithdrawModal } from '$lib/services/modal';
-  import { writable } from 'svelte/store';
 </script>
 
 <PageHeader title="Vaults" pathname={$page.url.pathname} />
 
-<VaultsListTable
-  {orderHash}
-  {activeAccountsItems}
-  {showInactiveOrders}
-  {handleDepositModal}
-  {handleWithdrawModal}
-  showMyItemsOnly={writable(false)}
-/>
+<VaultsListTable {handleDepositModal} {handleWithdrawModal} />
