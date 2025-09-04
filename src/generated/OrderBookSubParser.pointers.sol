346804895ea7f82d1fd6f9805a3ea75cd1378a20
thedavidmeister <thedavidmeister@gmail.com>
2025-09-04 13:25:27 +0400

    pointers

--- src/generated/OrderBookSubParser.pointers.sol
+++ src/generated/OrderBookSubParser.pointers.sol
@@ -10,10 +10,10 @@
 pragma solidity =0.8.25;
 
 /// @dev Hash of the known bytecode.
-bytes32 constant BYTECODE_HASH = bytes32(0x14fcea81300c2900d35b488e9a0f7b9e1de913764ae6e26810568ea39e6ca651);
+bytes32 constant BYTECODE_HASH = bytes32(0x7857e267f9c11d1afd8b0e467b4b951d3723ab4d9ef330544da3e9d0c954c2c6);
 
 /// @dev The hash of the meta that describes the contract.
-bytes32 constant DESCRIBED_BY_META_HASH = bytes32(0xdfd338fdf179bf832d2bc3222c6b2ac399d0e341fe38960e1bbd59aa6ccfbf9b);
+bytes32 constant DESCRIBED_BY_META_HASH = bytes32(0x3457291434660c589b0e6c287e12b513e5194a5129c75fa47d01ce03fbc52178);
 
 /// @dev The parse meta that is used to lookup word definitions.
 /// The structure of the parse meta is:
@@ -50,7 +50,7 @@ bytes constant SUB_PARSER_WORD_PARSERS =
 /// These positional indexes all map to the same indexes looked up in the parse
 /// meta.
 bytes constant OPERAND_HANDLER_FUNCTION_POINTERS =
-    hex"1d191d191d191d191d191d191d191d191d191d191d191d191d191d191d191d191d191d5e1e5d1d191d191d191d191d191d191d191d191d191d191d19";
+    hex"1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1d401e3f1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb1cfb";
 
 /// @dev Every two bytes is a function pointer for a literal parser.
 /// Literal dispatches are determined by the first byte(s) of the literal
