6abbed07c6419d509673dca16c8180b5d1b7130f
0xgleb <gleb.dianov@protonmail.com>
2025-10-10 17:59:16 -0300

    try libsoup 3

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for development workflows.";
 
   inputs = {
-    rainix.url = "github:rainlanguage/rainix?rev=f2a4edac01a17368ffdbae9fb42a110139acebde";
+    rainix.url = "github:rainlanguage/rainix";
     rain.url = "github:rainlanguage/rain.cli";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -289,7 +289,7 @@
           ];
           shellHook = rainix.devShells.${system}.tauri-shell.shellHook;
           buildInputs = rainix.devShells.${system}.tauri-shell.buildInputs
-            ++ [ pkgs.clang-tools pkgs.libsoup_2_4 ];
+            ++ [ pkgs.clang-tools pkgs.libsoup_3 ];
           nativeBuildInputs =
             rainix.devShells.${system}.tauri-shell.nativeBuildInputs;
         };
@@ -300,5 +300,4 @@
             rainix.devShells.${system}.default.nativeBuildInputs;
         };
       });
-
 }
