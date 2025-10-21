e1d1cc87be6d06ca00abc79b48f5e790bd3105a5
Arda Nakisci <anakisci@gmail.com>
2025-10-21 11:19:14 +0300

    move topics to bindings crate

--- crates/bindings/src/lib.rs
+++ crates/bindings/src/lib.rs
@@ -34,3 +34,31 @@ pub mod js_api;
 
 #[cfg(target_family = "wasm")]
 pub mod wasm_traits;
+
+pub mod topics {
+    use crate::{
+        IInterpreterStoreV3::Set,
+        IOrderBookV5::{
+            AddOrderV3, AfterClearV2, ClearV3, DepositV2, RemoveOrderV3, TakeOrderV3, WithdrawV2,
+        },
+        OrderBook::MetaV1_2,
+    };
+    use alloy::{primitives::B256, sol_types::SolEvent};
+
+    pub fn orderbook_event_topics() -> Vec<B256> {
+        vec![
+            AddOrderV3::SIGNATURE_HASH,
+            TakeOrderV3::SIGNATURE_HASH,
+            WithdrawV2::SIGNATURE_HASH,
+            DepositV2::SIGNATURE_HASH,
+            RemoveOrderV3::SIGNATURE_HASH,
+            ClearV3::SIGNATURE_HASH,
+            AfterClearV2::SIGNATURE_HASH,
+            MetaV1_2::SIGNATURE_HASH,
+        ]
+    }
+
+    pub fn store_set_topics() -> Vec<B256> {
+        vec![Set::SIGNATURE_HASH]
+    }
+}
