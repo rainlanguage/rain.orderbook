1aed8f93242fdb69bf750a74d1c86875829a0895
rouzwelt <rouzweltt@gmail.com>
2025-10-26 02:16:10 +0000

    update

--- packages/orderbook/scripts/build.js
+++ packages/orderbook/scripts/build.js
@@ -10,9 +10,6 @@ fs.writeFileSync("./cjs.d.ts", 'export * from "./dist/cjs/index";\n');
 fs.writeFileSync("./esm.js", 'export * from "./dist/esm/index";\n');
 fs.writeFileSync("./esm.d.ts", 'export * from "./dist/esm/index";\n');
 
-// build for wasm32 target
-execSync("npm run build-wasm");
-
 // build specified packages and include them in final index file
 // list of packages to build can be extended by adding new package
 // names to the list below
