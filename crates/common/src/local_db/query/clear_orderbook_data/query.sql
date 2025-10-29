8962ae937dc7fb5b5c8c18c9dfd673dc9e672fc3
Arda Nakisci <anakisci@gmail.com>
2025-10-29 16:51:40 +0300

    add query to remove all orderbook data

---
+++ crates/common/src/local_db/query/clear_orderbook_data/query.sql
@@ -0,0 +1,48 @@
+BEGIN TRANSACTION;
+
+DELETE FROM context_values
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM take_order_contexts
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM take_orders
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM order_ios
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM clear_v3_events
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM after_clear_v2_events
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM meta_events
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM interpreter_store_sets
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM raw_events
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM deposits
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM withdrawals
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM order_events
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM erc20_tokens
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM sync_status
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+DELETE FROM target_watermarks
+WHERE chain_id = ?1 AND lower(orderbook_address) = lower(?2);
+
+COMMIT;
