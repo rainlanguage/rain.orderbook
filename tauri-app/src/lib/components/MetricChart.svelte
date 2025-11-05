6c1d4026a003779a761e727231c3bc587c4be637
Arda Nakisci <anakisci@gmail.com>
2025-11-05 08:43:00 +0100

    Update components to use new logic

--- tauri-app/src/lib/components/MetricChart.svelte
+++ tauri-app/src/lib/components/MetricChart.svelte
@@ -4,16 +4,20 @@
   export let metric: MetricCfg;
   export let data: TransformedPlotData[];
 
-  $: metricData = metric?.precision
-    ? parseFloat(data[0]?.[metric.value].toPrecision(metric.precision)).toString()
-    : data[0]?.[metric.value];
+  $: metricDatum = data?.[0]?.[metric.value];
+  $: metricValueText = metricDatum
+    ? metric?.precision != null
+      ? Number(metricDatum.value.toPrecision(metric.precision)).toString()
+      : metricDatum.formatted
+    : undefined;
 </script>
 
 <div class="flex h-full w-full flex-col items-center justify-between border p-4">
   <span>{metric.label}</span>
-  <span class="text-2xl"
-    >{metric?.['unit-prefix'] || ''}{metricData}{metric?.['unit-suffix'] || ''}</span
-  >
+  <span class="text-2xl">
+    {(metric?.['unit-prefix'] ?? '') + (metricValueText ?? '') + (metric?.['unit-suffix'] ?? '')}
+  </span>
+
   {#if metric?.description}
     <span class="text-sm">{metric.description}</span>
   {/if}
