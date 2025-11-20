84cec286d10159daba606270e5297fb55c011fb2
Arda Nakisci <anakisci@gmail.com>
2025-11-20 13:45:33 +0300

    update tests

--- packages/ui-components/src/__tests__/TanstackAppTable.test.svelte
+++ packages/ui-components/src/__tests__/TanstackAppTable.test.svelte
@@ -5,9 +5,20 @@
 	export let title: string;
 	export let head: string;
 	export let queryKey: string;
+	export let enableVirtualization = true;
+	export let estimatedRowHeight = 56;
+	export let virtualizationOverscan = 8;
 </script>
 
-<TanstackAppTable {query} {emptyMessage} rowHoverable {queryKey}>
+<TanstackAppTable
+	{query}
+	{emptyMessage}
+	rowHoverable
+	{queryKey}
+	{enableVirtualization}
+	{estimatedRowHeight}
+	{virtualizationOverscan}
+>
 	<svelte:fragment slot="title">
 		<h2>{title}</h2>
 	</svelte:fragment>
