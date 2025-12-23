c0c4008346d3040e92c1c02efa5cf758f6941f32
Arda Nakisci <anakisci@gmail.com>
2025-12-23 16:17:08 +0300

    add balance change type filter component

---
+++ packages/ui-components/src/lib/components/VaultBalanceChangeTypeFilter.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { type VaultBalanceChangeFilter } from '@rainlanguage/orderbook';
+	import DropdownCheckbox from './dropdown/DropdownCheckbox.svelte';
+
+	export let value: VaultBalanceChangeFilter[] | undefined = undefined;
+
+	const filterOptions: Record<VaultBalanceChangeFilter, string> = {
+		deposit: 'Deposit',
+		withdrawal: 'Withdrawal',
+		trade: 'Trade',
+		clearBounty: 'Clear Bounty'
+	};
+
+	let typeFilter: Partial<Record<VaultBalanceChangeFilter, string>> = {};
+
+	$: {
+		const keys = Object.keys(typeFilter) as VaultBalanceChangeFilter[];
+		if (keys.length > 0 && keys.length < Object.keys(filterOptions).length) {
+			value = keys.sort();
+		} else {
+			value = undefined;
+		}
+	}
+</script>
+
+<DropdownCheckbox
+	options={filterOptions}
+	bind:value={typeFilter}
+	label="Trade Type"
+	allLabel="All types"
+	onlyTitle={true}
+/>
