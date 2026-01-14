1411d502cab600ef7bf8acafca7c998e01f92617
Arda Nakisci <anakisci@gmail.com>
2026-01-14 12:58:27 +0300

    update settings

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const REGISTRY_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/160d2c7119d6423c22737db1eb9ca71190120775/registry';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/7b2a499474ba7ea3d798909e93021fb51aa3b3b9/registry';
 export const REMOTE_SETTINGS_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/160d2c7119d6423c22737db1eb9ca71190120775/settings.yaml';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/7b2a499474ba7ea3d798909e93021fb51aa3b3b9/settings.yaml';
