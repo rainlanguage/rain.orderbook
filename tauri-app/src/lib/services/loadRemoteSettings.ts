b8ceffb5149b93f46e98c3d4d86f6ea2d7758b14
Arda Nakisci <anakisci@gmail.com>
2026-01-15 17:35:42 +0300

    update rain.strategies settings hash to 7b2a499

--- tauri-app/src/lib/services/loadRemoteSettings.ts
+++ tauri-app/src/lib/services/loadRemoteSettings.ts
@@ -1,5 +1,5 @@
 const REMOTE_SETTINGS_URL =
-  'https://raw.githubusercontent.com/rainlanguage/rain.strategies/160d2c7119d6423c22737db1eb9ca71190120775/settings.yaml';
+  'https://raw.githubusercontent.com/rainlanguage/rain.strategies/7b2a499474ba7ea3d798909e93021fb51aa3b3b9/settings.yaml';
 
 export async function loadRemoteSettings() {
   const response = await fetch(REMOTE_SETTINGS_URL);
