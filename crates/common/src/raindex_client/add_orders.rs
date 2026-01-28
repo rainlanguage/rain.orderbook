31fe473264a6d03e47e39bdbc894d8f711013550
Arda Nakisci <anakisci@gmail.com>
2026-01-28 17:16:42 +0300

    Add missing wipe_and_recreate to test mocks

--- crates/common/src/raindex_client/add_orders.rs
+++ crates/common/src/raindex_client/add_orders.rs
@@ -237,6 +237,10 @@ mod tests {
                     "query_text not supported in CountingJsonExec",
                 ))
             }
+
+            async fn wipe_and_recreate(&self) -> Result<(), LocalDbQueryError> {
+                Err(LocalDbQueryError::not_implemented("wipe_and_recreate"))
+            }
         }
 
         fn empty_add_orders_response() -> Value {
