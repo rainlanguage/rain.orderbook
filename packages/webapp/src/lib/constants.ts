f7bbbe2084c968d19417944d2e7e98329dd37b86
Arda Nakisci <anakisci@gmail.com>
2026-01-30 08:46:06 +0300

    Update constants.ts [skip ci]

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const REGISTRY_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/7745ace683dd2ac8566119ccbb77e76e9dae2b2b/registry';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/b1c696cb081acc23a2842ea2c7826369a068109d/registry';
