a1bde8b40bc4d39b2599b907ebd469d2e370bd79
Arda Nakisci <anakisci@gmail.com>
2026-02-03 11:20:17 +0300

    update registry to polygon batch-size 10
    rain.strategies PR: https://github.com/rainlanguage/rain.strategies/pull/80

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const REGISTRY_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/f0978c57ee997a4331fa1052c913c34f48873142/registry';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/5665a9852085336966aefa8916f7ecb73bcd7365/registry';
