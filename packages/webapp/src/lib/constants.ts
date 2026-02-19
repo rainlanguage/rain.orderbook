ac0d923d3d7a170061f6dd826b20e2cef91ad75c
Arda Nakisci <anakisci@gmail.com>
2026-02-19 11:59:19 +0100

    Update strategies registry URL to latest commit

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const REGISTRY_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/0a11baffb6d63488a326cd51932bcdb06bc3e18e/registry';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/57d3d07e5e37fac8309bb2f6da3fb3b2c9df7b3d/registry';
