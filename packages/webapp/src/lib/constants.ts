e02cf81519e26311203b0565ff4e5546cddad3ea
Arda Nakisci <anakisci@gmail.com>
2026-02-19 12:35:30 +0100

    update registry hash

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const REGISTRY_URL =
-	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/57d3d07e5e37fac8309bb2f6da3fb3b2c9df7b3d/registry';
+	'https://raw.githubusercontent.com/rainlanguage/rain.strategies/57d3d07b2df08fb4e6b88c77f69bf495f86057f7/registry';
